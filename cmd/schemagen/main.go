81a80ece6cf439288326fcdf4b86921b26e15afc
Elliot Courant <me@elliotcourant.dev>
2021-02-06 12:44:23 -0600

    Fixing syntax error.

--- cmd/schemagen/main.go
+++ cmd/schemagen/main.go
@@ -22,7 +22,7 @@ var (
 var (
 	createExtensions = []string{
 		`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`,
-		`CREATE EXTENSION IF NOT EXISTS "citext":`,
+		`CREATE EXTENSION IF NOT EXISTS "citext";`,
 	}
 )
 
