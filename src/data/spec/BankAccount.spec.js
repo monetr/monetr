888efedc876d6bfd7b70c774ab72c3baa082a727
Elliot Courant <me@elliotcourant.dev>
2021-02-18 20:11:00 -0600

    I wrote a test :tada:

---
+++ src/data/spec/BankAccount.spec.js
@@ -0,0 +1,12 @@
+import BankAccount from "data/BankAccount";
+
+
+describe('bank accounts', () => {
+  it('will return available balance', () => {
+    const account = new BankAccount({
+      availableBalance: 1000,
+    });
+
+    expect(account.getAvailableBalanceString()).toBe('$10.00');
+  });
+});
