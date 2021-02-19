8c56fdedeff6e474fce273dc4cf154c45ba5da66
Elliot Courant <me@elliotcourant.dev>
2021-02-18 21:02:24 -0600

    Silence warning.

--- pkg/models/all.go
+++ pkg/models/all.go
@@ -15,4 +15,19 @@ var (
 		&Expense{},
 		&Transaction{},
 	}
+
+	// This silences any warnings about the tableName field not being used. It's used via reflection in our ORM to
+	// query and generate schemas/SQL.
+	_ = Account{}.tableName
+	_ = BankAccount{}.tableName
+	_ = EmailVerification{}.tableName
+	_ = Expense{}.tableName
+	_ = FundingSchedule{}.tableName
+	_ = Link{}.tableName
+	_ = Login{}.tableName
+	_ = PhoneVerification{}.tableName
+	_ = PlaidLink{}.tableName
+	_ = Registration{}.tableName
+	_ = Transaction{}.tableName
+	_ = User{}.tableName
 )
