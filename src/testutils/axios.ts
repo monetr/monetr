c5bce267999d3e30ac5a5cdb369f301aded342bc
Elliot Courant <me@elliotcourant.dev>
2021-03-25 22:32:54 -0500

    Improving testing.

--- src/testutils/axios.ts
+++ src/testutils/axios.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+export function mockAxiosGetOnce(result: any) {
+  // @ts-ignore
+  axios.get.mockImplementationOnce(() => result);
+  mockAxios();
+}
+
 export function mockAxios() {
   Object.defineProperty(window, 'API', {
     value: axios
