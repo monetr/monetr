53d3650feeafbfdd3c9bb678d24168a83b3a8807
Elliot Courant <me@elliotcourant.dev>
2021-04-01 22:14:31 -0500

    Pg tap testing

--- tests/pg/tables.sql
+++ tests/pg/tables.sql
@@ -1,1 +1,11 @@
-SELECT has_table('test');
+BEGIN;
+SELECT plan(4);
+
+SELECT has_table('logins');
+SELECT has_table('registrations');
+SELECT has_table('email_verifications');
+SELECT has_table('phone_verifications');
+
+SELECT *
+FROM finish();
+ROLLBACK;
