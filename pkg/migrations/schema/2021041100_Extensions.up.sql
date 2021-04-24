f667db45dea746161ca4934ea0bbc169a7219218
Elliot Courant <me@elliotcourant.dev>
2021-04-24 16:40:40 -0500

    Skip extension migrations

--- pkg/migrations/schema/2021041100_Extensions.up.sql
+++ pkg/migrations/schema/2021041100_Extensions.up.sql
@@ -1,3 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- TODO Creating extensions requires a superuser. Find a way to manage that here.
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE EXTENSION IF NOT EXISTS "citext";
+-- CREATE EXTENSION IF NOT EXISTS "citext";
