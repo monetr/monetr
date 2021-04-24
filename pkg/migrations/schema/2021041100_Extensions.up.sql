adb8ff3a2566ac8ef32737368b3b6a46bff4bf7f
Elliot Courant <me@elliotcourant.dev>
2021-04-24 16:44:16 -0500

    Query cannot be empty.

--- pkg/migrations/schema/2021041100_Extensions.up.sql
+++ pkg/migrations/schema/2021041100_Extensions.up.sql
@@ -1,4 +1,4 @@
 -- TODO Creating extensions requires a superuser. Find a way to manage that here.
 -- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- CREATE EXTENSION IF NOT EXISTS "citext";
+SELECT 1;
