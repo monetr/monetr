aaf3932dad881ff4d4bcc18e575fb3b49dd8cd71
Elliot Courant <me@elliotcourant.dev>
2021-04-24 18:15:39 -0500

    Fixing pg tests

--- tests/pg/tables.sql
+++ tests/pg/tables.sql
@@ -1,10 +1,16 @@
 BEGIN;
 SELECT plan(4);
 
+SELECT has_table('accounts');
+SELECT has_table('bank_accounts');
+SELECT has_table('funding_schedules');
+SELECT has_table('jobs');
+SELECT has_table('links');
 SELECT has_table('logins');
-SELECT has_table('registrations');
-SELECT has_table('email_verifications');
-SELECT has_table('phone_verifications');
+SELECT has_table('plaid_links');
+SELECT has_table('spending');
+SELECT has_table('transactions');
+SELECT has_table('users');
 
 SELECT *
 FROM finish();
