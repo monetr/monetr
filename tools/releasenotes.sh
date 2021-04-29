b55e2980c2363abf6d96815cbdf7dea5f7e38f4c
Elliot Courant <me@elliotcourant.dev>
2021-04-29 16:38:58 -0500

    Working on release notes generator

---
+++ tools/releasenotes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+releaseTag=$1
+
+gpgBadge() {
+    commitSha=$1
+    gpgVerified=$(git --no-pager show -s --pretty=tformat:"%G?" $gitCommit)
+    if [ $gpgVerified = "G" ]
+    then
+        echo "[![Verified](https://badgen.net/badge/GPG/Verified/green?scale=0.9)]()"
+    elif [ $gpgVerified = "N" ]
+    then
+        echo "[![No GPG](https://badgen.net/badge/GPG/Not%2Verified/red)]()"
+    else
+        signature=$(git --no-pager show -s --pretty=tformat:"%GK" $gitCommit)
+        echo "[![Not Verified](https://badgen.net/badge/GPG/Not%20Verified%20$1/red)]()"
+    fi
+}
+
+git_commits=$(git --no-pager log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%H");
+commitCount=$(echo "$git_commits" | wc -l | xargs);
+echo "# Today's Release"
+echo "**New Version**: $releaseTag"
+echo "## Last Release Tag: **$(git describe --tags --abbrev=0)**";
+echo "## Number Of Commits Since Last Release: $commitCount";
+for gitCommit in $git_commits
+do
+    commitSha=$gitCommit
+    authorName=$(git --no-pager show -s --pretty=tformat:"%aN" $gitCommit)
+    commitMessage=$(git --no-pager show -s --pretty=tformat:"%s" $gitCommit)
+    commitBody=$(git --no-pager show -s --pretty=tformat:"%b" $gitCommit | tr '\n' ' ' | xargs)
+    gpgVerified=$(git --no-pager show -s --pretty=tformat:"%G?" $gitCommit)
+    badge=$(gpgBadge $commitSha)
+    echo "- $commitSha **$authorName** - _${commitMessage}_"
+    if [ ! -z "$commitBody" ]
+    then
+      echo "  > $commitBody"
+    fi
+done
