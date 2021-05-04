1e4ed1f4970749af0d9a672b3b932d7b3b72c2f8
Elliot Courant <me@elliotcourant.dev>
2021-05-03 19:52:04 -0500

    Adding some basic stuff to the account view page.
    A list of linked bank accounts for now. Even though it is not editable
    its a small start for the page.

--- src/views/AccountView/index.tsx
+++ src/views/AccountView/index.tsx
@@ -1,15 +1,42 @@
-import { Card, Typography } from "@material-ui/core";
+import { Card, List, ListItem, ListItemText, Typography } from "@material-ui/core";
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { getLinks } from "shared/links/selectors/getLinks";
+import { Map } from 'immutable';
+import Link from "data/Link";
 
-export class AccountView extends Component<any, any> {
+interface WithConnectionPropTypes {
+  links: Map<number, Link>;
+}
+
+export class AccountView extends Component<WithConnectionPropTypes, any> {
 
   render() {
     return (
       <div className="minus-nav">
         <div className="flex flex-col h-full p-10 max-h-full">
           <div className="grid grid-cols-3 gap-4 flex-grow">
-            <div className="col-span-3">
+            <div className="col-span-1">
+              <Card elevation={ 4 } className="w-full goals-list ">
+                <div className="w-full text-center pt-5">
+                  <Typography
+                    variant="h5"
+                  >
+                    Linked Bank Accounts
+                  </Typography>
+                </div>
+                <List className="w-full">
+                  { this.props.links.map(link => (
+                    <ListItem key={ link.linkId } button>
+                      <ListItemText>
+                        { link.getName() }
+                      </ListItemText>
+                    </ListItem>
+                  )).valueSeq().toArray() }
+                </List>
+              </Card>
+            </div>
+            <div className="col-span-2">
               <Card elevation={ 4 } className="w-full goals-list ">
                 <div className="h-full flex justify-center items-center">
                   <div className="grid grid-cols-1 grid-rows-2 grid-flow-col gap-2">
@@ -31,6 +58,8 @@ export class AccountView extends Component<any, any> {
 }
 
 export default connect(
-  (state) => ({}),
+  (state) => ({
+    links: getLinks(state),
+  }),
   {}
 )(AccountView);
