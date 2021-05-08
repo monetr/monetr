d6c73b3d7eec6c3894e6c871889cfd624fb3b7e9
Elliot Courant <me@elliotcourant.dev>
2021-05-07 23:43:41 -0500

    Fixing logout not properly deleting cookie.

--- src/shared/authentication/actions/logout.js
+++ src/shared/authentication/actions/logout.js
@@ -4,7 +4,10 @@ import Cookies from "js-cookie";
 
 export default function logout() {
   return dispatch => {
-    Cookies.remove('M-Token');
+    Cookies.remove('M-Token', {
+      // eslint-disable-next-line no-undef
+      domain: CONFIG.COOKIE_DOMAIN,
+    });
     window.localStorage.removeItem('M-Token');
 
     return dispatch({
