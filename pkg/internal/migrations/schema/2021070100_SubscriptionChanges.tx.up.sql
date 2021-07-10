9246f5af4bbf83984683b6694e1b14e804160e76
Elliot Courant <me@elliotcourant.dev>
2021-07-09 21:08:07 -0500

    Fixing migration in staging.

--- pkg/internal/migrations/schema/2021070100_SubscriptionChanges.tx.up.sql
+++ pkg/internal/migrations/schema/2021070100_SubscriptionChanges.tx.up.sql
@@ -9,6 +9,6 @@ ALTER TABLE "accounts" ADD COLUMN "subscription_active_until" TIMESTAMPTZ NULL;
 DROP TABLE "subscriptions";
 
 -- Just clean up from an old idea.
-DELETE FROM "users" WHERE user_id = -1;
 DELETE FROM "logins" WHERE login_id = -1;
+DELETE FROM "users" WHERE user_id = -1;
 DELETE FROM "accounts" WHERE account_id = -1;
