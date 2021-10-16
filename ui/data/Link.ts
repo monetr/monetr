e397a053a517780ce77d7963dc2697915fb04f13
Elliot Courant <me@elliotcourant.dev>
2021-10-15 22:54:43 -0500

    Experimenting with documentation inside typescript.

--- ui/data/Link.ts
+++ ui/data/Link.ts
@@ -18,7 +18,16 @@ export const errorMessages = {
   'ITEM_LOGIN_REQUIRED': `This link's authentication has expired and needs to be re-authenticated.`
 };
 
+/**
+  * A Link is used to represent an institution and it's bank accounts. An account can have multiple links and can have
+  * multiple links for the same institution. But typically links will represent a unique group of bank accounts for a
+  * an institution. A group of bank accounts within a single "login" for that institution.
+  */
 export default class Link {
+  /**
+   * Represents the global unique identifier for a group of bank accounts in monetr.
+   * This value is generated automatically by the API upon creation, and cannot be changed.
+   */
   linkId: number;
   linkType: LinkType;
   linkStatus: LinkStatus;
