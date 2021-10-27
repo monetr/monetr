f1c3b94a1588b38bf0fe961f63bb42776f8babfb
Elliot Courant <me@elliotcourant.dev>
2021-10-27 17:06:30 -0500

    chore: Improving github action flows, added version command.
    Adding stuff to the github action flows to also build windows and
    darwin binaries. Also trying something out to automatically attach
    binaries to the release.

--- pkg/internal/cmd/root.go
+++ pkg/internal/cmd/root.go
@@ -1,13 +1,31 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/monetr/monetr/pkg/build"
+	"github.com/spf13/cobra"
+)
 
 var (
 	RootCommand = &cobra.Command{
 		Use:   "monetr",
-		Short: "REST-API for monetr's budgeting application",
+		Short: "monetr's budgeting application",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return cmd.Help()
 		},
 	}
+
+	VersionCommand = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version of monetr.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println(build.Release)
+			return nil
+		},
+	}
 )
+
+func init() {
+	RootCommand.AddCommand(VersionCommand)
+}
