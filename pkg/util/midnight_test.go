bb40ab49adf74f9978393bd5fa1e4dac03b4f8c4
Elliot Courant <me@elliotcourant.dev>
2021-11-07 20:00:57 -0600

    fix: Added DST test to make sure I'm not going insane.

---
+++ pkg/util/midnight_test.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInLocal(t *testing.T) {
+	t.Run("central time daylight savings", func(t *testing.T) {
+		central, err := time.LoadLocation("America/Chicago")
+		assert.NoError(t, err, "must be able to load the central time location")
+		start := time.Date(2021, 11, 06, 0, 0, 0, 0, time.UTC)
+
+		assert.False(t, start.IsDST(), "November 6th 2021 at midnight UTC should not be considered DST, UTC does not vibe with DST")
+
+		inCentral := InLocal(start, central)
+		assert.True(t, inCentral.IsDST(), "November 6th 2021 at midnight CDT should be considered DST")
+
+		theNextDay := InLocal(start.Add(24*time.Hour), central)
+		// This is because DST happens at 2AM.
+		assert.True(t, theNextDay.IsDST(), "November 7th 2021 at midnight CDT should be considered DST")
+
+		finallyDST := InLocal(start.Add(48*time.Hour), central)
+		// This is because DST happens at 2AM.
+		assert.False(t, finallyDST.IsDST(), "November 8th 2021 at midnight CST should not be considered DST")
+	})
+}
