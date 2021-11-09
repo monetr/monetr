6b7dd54e829379c302ed545114a188961098def9
Elliot Courant <me@elliotcourant.dev>
2021-11-08 21:36:01 -0600

    fix: Converting more components to functional components.

--- ui/components/Balance/BalanceNavDisplay.tsx
+++ ui/components/Balance/BalanceNavDisplay.tsx
@@ -1,42 +1,31 @@
 import { Typography } from '@mui/material';
-import Balance from 'models/Balance';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { getBalance } from 'shared/balances/selectors/getBalance';
 
-interface WithConnectionPropTypes {
-  balance: Balance;
-}
+const BalanceNavDisplay = (): JSX.Element => {
+  const balance = useSelector(getBalance);
 
-export class BalanceNavDisplay extends Component<WithConnectionPropTypes, any> {
-
-  render() {
-    if (!this.props.balance) {
-      return null;
-    }
-
-    return (
-      <div className="flex-1 flex justify-center gap-2">
-        <Typography>
-          <b>Safe-To-Spend:</b> { this.props.balance.getSafeToSpendString() }
-        </Typography>
-        <Typography variant="body2">
-          <b>Expenses:</b> { this.props.balance.getExpensesString() }
-        </Typography>
-        <Typography variant="body2">
-          <b>Goals:</b> { this.props.balance.getGoalsString() }
-        </Typography>
-        <Typography variant="body2">
-          <b>Available:</b> { this.props.balance.getAvailableString() }
-        </Typography>
-      </div>
-    )
+  if (!balance) {
+    return null;
   }
+
+  return (
+    <div className="flex-1 flex justify-center gap-2">
+      <Typography>
+        <b>Safe-To-Spend:</b> { balance.getSafeToSpendString() }
+      </Typography>
+      <Typography variant="body2">
+        <b>Expenses:</b> { balance.getExpensesString() }
+      </Typography>
+      <Typography variant="body2">
+        <b>Goals:</b> { balance.getGoalsString() }
+      </Typography>
+      <Typography variant="body2">
+        <b>Available:</b> { balance.getAvailableString() }
+      </Typography>
+    </div>
+  )
 }
 
-export default connect(
-  (state) => ({
-    balance: getBalance(state),
-  }),
-  {}
-)(BalanceNavDisplay);
+export default BalanceNavDisplay;
--- ui/components/Expenses/ExpenseItem.tsx
+++ ui/components/Expenses/ExpenseItem.tsx
@@ -1,110 +1,83 @@
 import { Checkbox, Chip, LinearProgress, ListItem, ListItemIcon, Typography } from '@mui/material';
-import FundingSchedule from 'models/FundingSchedule';
-import Spending from 'models/Spending';
-import { getActiveElement } from 'formik';
-import moment from 'moment';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getFundingScheduleById } from 'shared/fundingSchedules/selectors/getFundingScheduleById';
 import selectExpense from 'shared/spending/actions/selectExpense';
 import { getExpenseIsSelected } from 'shared/spending/selectors/getExpenseIsSelected';
 import { getSpendingById } from 'shared/spending/selectors/getSpendingById';
-import { AppState } from 'store';
 
 export interface PropTypes {
   expenseId: number;
 }
 
-interface WithConnectionPropTypes extends PropTypes {
-  expense: Spending;
-  fundingSchedule: FundingSchedule;
-  isSelected: boolean;
-  selectExpense: { (expenseId: number): void };
-}
+const ExpenseItem = (props: PropTypes): JSX.Element => {
+  const expense = useSelector(getSpendingById(props.expenseId));
+  const isSelected = useSelector(getExpenseIsSelected(props.expenseId));
+  const fundingSchedule = useSelector(getFundingScheduleById(expense.fundingScheduleId));
 
-export class ExpenseItem extends Component<WithConnectionPropTypes> {
+  const dispatch = useDispatch();
 
-  onClick = () => {
-    return this.props.selectExpense(this.props.expenseId);
-  };
+  const onClick = () => dispatch(selectExpense(expense.spendingId));
 
-  render() {
-    const { expense, isSelected, fundingSchedule } = this.props;
-
-    return (
-      <ListItem button onClick={ this.onClick }>
-        <ListItemIcon>
-          <Checkbox
-            edge="start"
-            checked={ isSelected }
-            tabIndex={ -1 }
-            color="primary"
-          />
-        </ListItemIcon>
-        <div className="w-full grid grid-cols-6 grid-rows-4 grid-flow-col">
-          <div className="col-span-4">
-            <Typography>
-              <b>{ expense.name }</b>
-            </Typography>
-          </div>
-          <div className="col-span-4">
-            <Typography
-              variant="body1"
-            >
-              { expense.getCurrentAmountString() } <span
-              className="opacity-80">of</span> { expense.getTargetAmountString() }
-            </Typography>
-          </div>
-          <div className="col-span-4">
-            <Typography
-              variant="body1"
-            >
-              { expense.getNextOccurrenceString() }
-              { expense.description && ` - ${ expense.description }` }
-            </Typography>
-          </div>
-          <div className="col-span-4">
-            <Typography
-              variant="body1"
-            >
-              { expense.getNextContributionAmountString() }/{ fundingSchedule.name }
-            </Typography>
-          </div>
-          <div className="flex justify-end p-5 align-middle col-span-1 row-span-4">
-            { expense.isBehind &&
-            <Chip
-              className="self-center"
-              label="Behind"
-              color="secondary"
-            />
-            }
-          </div>
-          <div className="flex justify-end align-middle col-span-1 row-span-4">
-            <LinearProgress
-              variant="determinate"
-              color="primary"
-              className="self-center w-full"
-              value={ Math.min((expense.currentAmount / expense.targetAmount) * 100, 100) }
-            />
-          </div>
+  return (
+    <ListItem button onClick={ onClick }>
+      <ListItemIcon>
+        <Checkbox
+          edge="start"
+          checked={ isSelected }
+          tabIndex={ -1 }
+          color="primary"
+        />
+      </ListItemIcon>
+      <div className="w-full grid grid-cols-6 grid-rows-4 grid-flow-col">
+        <div className="col-span-4">
+          <Typography>
+            <b>{ expense.name }</b>
+          </Typography>
         </div>
-      </ListItem>
-    )
-  }
+        <div className="col-span-4">
+          <Typography
+            variant="body1"
+          >
+            { expense.getCurrentAmountString() } <span
+            className="opacity-80">of</span> { expense.getTargetAmountString() }
+          </Typography>
+        </div>
+        <div className="col-span-4">
+          <Typography
+            variant="body1"
+          >
+            { expense.getNextOccurrenceString() }
+            { expense.description && ` - ${ expense.description }` }
+          </Typography>
+        </div>
+        <div className="col-span-4">
+          <Typography
+            variant="body1"
+          >
+            { expense.getNextContributionAmountString() }/{ fundingSchedule.name }
+          </Typography>
+        </div>
+        <div className="flex justify-end p-5 align-middle col-span-1 row-span-4">
+          { expense.isBehind &&
+          <Chip
+            className="self-center"
+            label="Behind"
+            color="secondary"
+          />
+          }
+        </div>
+        <div className="flex justify-end align-middle col-span-1 row-span-4">
+          <LinearProgress
+            variant="determinate"
+            color="primary"
+            className="self-center w-full"
+            value={ Math.min((expense.currentAmount / expense.targetAmount) * 100, 100) }
+          />
+        </div>
+      </div>
+    </ListItem>
+  )
 }
 
-export default connect(
-  (state: AppState, props: PropTypes) => {
-    const expense = getSpendingById(props.expenseId)(state);
-    const isSelected = getExpenseIsSelected(props.expenseId)(state);
-
-    return {
-      expense,
-      isSelected,
-      fundingSchedule: getFundingScheduleById(expense.fundingScheduleId)(state),
-    };
-  },
-  {
-    selectExpense,
-  },
-)(ExpenseItem);
+export default ExpenseItem;
