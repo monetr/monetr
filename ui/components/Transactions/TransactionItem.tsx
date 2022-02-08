391b6f4dc4d9ec61ddecf33c0fd04c5209218918
Elliot Courant <me@elliotcourant.dev>
2022-02-08 16:24:42 -0600

    feat: Improve spent from dropdown, converted to react-select.
    The drop down to select a spending object from which a transaction is
    allocated is now a react-select based component.

--- ui/components/Transactions/TransactionItem.tsx
+++ ui/components/Transactions/TransactionItem.tsx
@@ -1,166 +1,46 @@
-import { Chip, Divider, ListItem, Paper, Popover, Typography } from '@mui/material';
+import { Chip, Divider, ListItem, Typography } from '@mui/material';
 import classnames from 'classnames';
 import TransactionNameEditor from 'components/Transactions/TransactionNameEditor';
-import Spending from 'models/Spending';
-import Transaction from 'models/Transaction';
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { getSpendingById } from 'shared/spending/selectors/getSpendingById';
+import TransactionSpentFromSelection from 'components/Transactions/TransactionSpentFromSelection';
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import { getTransactionById } from 'shared/transactions/selectors/getTransactionById';
-import SelectButton from 'components/SelectyBoi/SelectButton';
-import SpendingSelectionList from 'components/Spending/SpendingSelectionList';
-import updateTransaction from 'shared/transactions/actions/updateTransaction';
 
 import './styles/TransactionItem.scss';
-import { AppState } from 'store';
 
-interface PropTypes {
+interface Props {
   transactionId: number;
 }
 
-interface WithConnectionPropTypes extends PropTypes {
-  transaction: Transaction;
-  spending?: Spending;
-  updateTransaction: (transaction: Transaction) => Promise<void>;
-}
+export default function TransactionItem(props: Props): JSX.Element {
+  const transaction = useSelector(getTransactionById(props.transactionId));
 
-interface State {
-  spentFromAnchorEl: Element | null;
-  spentFromWidth: number | null;
-}
-
-export class TransactionItem extends Component<WithConnectionPropTypes, State> {
-
-  state = {
-    spentFromAnchorEl: null,
-    spentFromWidth: 0,
-  };
-
-  getSpentFromString() {
-    const { spending, transaction, updateTransaction } = this.props;
-    const { spentFromAnchorEl } = this.state;
-
-    if (transaction.getIsAddition()) {
-      return null;
-    }
-
-    const updateSpentFrom = (selection: Spending | null) => {
-      const spendingId = selection ? selection.spendingId : null;
-
-      if (spendingId === transaction.spendingId) {
-        return Promise.resolve();
-      }
-
-      const updatedTransaction = new Transaction({
-        ...transaction,
-        spendingId: spendingId,
-      });
-
-      return updateTransaction(updatedTransaction)
-        .catch(error => alert(error));
-    };
-
-    const openPopover = (event: { currentTarget: Element }) => {
-      this.setState({
-        spentFromAnchorEl: event.currentTarget,
-        spentFromWidth: event.currentTarget.clientWidth,
-      });
-    };
-
-    const closePopover = () => this.setState({
-      spentFromAnchorEl: null,
-      spentFromWidth: null,
-    });
-
-    return (
-      <Fragment>
-        <SelectButton
-          open={ Boolean(spentFromAnchorEl) }
-          onClick={ openPopover }
-        >
-          <span className="mr-1 opacity-50">
-            Spent From
-          </span>
-          <span className={ classnames('overflow-ellipsis overflow-hidden flex-nowrap whitespace-nowrap', {
-            'opacity-50': !spending,
-          }) }>
-            { spending ? spending.name : 'Safe-To-Spend' }
-          </span>
-        </SelectButton>
-        <Popover
-          id={ `transaction-spent-from-popover-${ transaction.transactionId }` }
-          open={ Boolean(spentFromAnchorEl) }
-          anchorEl={ spentFromAnchorEl }
-          onClose={ closePopover }
-          anchorOrigin={ {
-            vertical: 'bottom',
-            horizontal: 'left',
-          } }
-          transformOrigin={ {
-            vertical: 'top',
-            horizontal: 'left',
-          } }
-        >
-          <Paper
-            style={ { width: `${ this.state.spentFromWidth }px` } }
-            className="p-0 overflow-auto min-w-96 max-h-96"
+  return (
+    <Fragment>
+      <ListItem className="transactions-item h-12" role="transaction-row">
+        <div className="flex flex-row w-full">
+          <div
+            className="flex-shrink w-2/5 pr-1 font-semibold transaction-item-name place-self-center"
           >
-            <SpendingSelectionList
-              value={ transaction.spendingId }
-              onChange={ updateSpentFrom }
-            />
-          </Paper>
-        </Popover>
-      </Fragment>
-    )
-  }
+            <TransactionNameEditor transactionId={ transaction.transactionId }/>
+          </div>
 
-  render() {
-    const { transaction } = this.props;
-
-    return (
-      <Fragment>
-        <ListItem className="transactions-item h-12" role="transaction-row">
-          <div className="flex flex-row w-full">
-            <div
-              className="flex-shrink w-2/5 pr-1 font-semibold transaction-item-name place-self-center"
-            >
-              <TransactionNameEditor transactionId={ transaction.transactionId }/>
-            </div>
-
-            <p
-              className="flex-auto w-2/5 pr-1 overflow-hidden transaction-expense-name overflow-ellipsis flex-nowrap whitespace-nowrap"
-            >
-              { this.getSpentFromString() }
-            </p>
-            <div className="flex items-center flex-none w-1/5">
-              { transaction.isPending && <Chip label="Pending" className="self-center align-middle"/> }
-              <div className="flex justify-end w-full">
-                <Typography className={ classnames('amount align-middle self-center place-self-center', {
-                  'addition': transaction.getIsAddition(),
-                }) }>
-                  <b>{ transaction.getAmountString() }</b>
-                </Typography>
-              </div>
+          <p className="flex-auto w-2/5 pr-1 transaction-item-spending">
+            <TransactionSpentFromSelection transactionId={ transaction.transactionId }/>
+          </p>
+          <div className="flex items-center flex-none w-1/5">
+            { transaction.isPending && <Chip label="Pending" className="self-center align-middle"/> }
+            <div className="flex justify-end w-full">
+              <Typography className={ classnames('amount align-middle self-center place-self-center', {
+                'addition': transaction.getIsAddition(),
+              }) }>
+                <b>{ transaction.getAmountString() }</b>
+              </Typography>
             </div>
           </div>
-        </ListItem>
-        <Divider/>
-      </Fragment>
-    )
-  }
+        </div>
+      </ListItem>
+      <Divider/>
+    </Fragment>
+  )
 }
-
-export default connect(
-  (state: AppState, props: PropTypes) => {
-    const transaction = getTransactionById(props.transactionId)(state);
-
-    return {
-      transaction,
-      spending: getSpendingById(transaction.spendingId)(state),
-    }
-  },
-  {
-    updateTransaction,
-  }
-)(TransactionItem)
---
+++ ui/components/Transactions/TransactionSpentFromSelection.tsx
@@ -0,0 +1,130 @@
+import { Chip } from '@mui/material';
+import classnames from 'classnames';
+import Spending from 'models/Spending';
+import Transaction from 'models/Transaction';
+import React, { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FormatOptionLabelMeta } from 'react-select/base';
+import { getBalance } from 'shared/balances/selectors/getBalance';
+import { getSpending } from 'shared/spending/selectors/getSpending';
+import updateTransaction from 'shared/transactions/actions/updateTransaction';
+import { getTransactionById } from 'shared/transactions/selectors/getTransactionById';
+import Select, { components, OptionProps, ActionMeta, OnChangeValue, Theme } from 'react-select';
+import { Map } from 'immutable';
+import formatAmount from 'util/formatAmount';
+
+import './styles/TransactionSpentFromSelection.scss';
+
+interface Props {
+  transactionId: number;
+}
+
+interface SpendingOption {
+  readonly label: string;
+  readonly value: number | null;
+  readonly spending: Spending | null;
+}
+
+function SpendingSelectOption({ children, ...props }: OptionProps<SpendingOption>): JSX.Element {
+  return (
+    <components.Option { ...props }>
+      <div className="w-full flex items-center">
+        <span className="font-semibold">{ props.label }</span>
+        <Chip
+          className="ml-auto font-semibold"
+          label={ formatAmount(props.data.spending?.currentAmount) }
+          color="secondary"
+        />
+      </div>
+    </components.Option>
+  )
+}
+
+export default function TransactionSpentFromSelection(props: Props): JSX.Element {
+  const transaction = useSelector(getTransactionById(props.transactionId));
+  const allSpending = useSelector(getSpending);
+  const balances = useSelector(getBalance);
+  const dispatch = useDispatch();
+
+  if (transaction.getIsAddition()) {
+    return null;
+  }
+
+  function updateSpentFrom(selection: Spending | null) {
+    const spendingId = selection ? selection.spendingId : null;
+
+    if (spendingId === transaction.spendingId) {
+      return Promise.resolve();
+    }
+
+    const updatedTransaction = new Transaction({
+      ...transaction,
+      spendingId: spendingId,
+    });
+
+    return dispatch(updateTransaction(updatedTransaction));
+  }
+
+  function handleSpentFromChange(newValue: OnChangeValue<SpendingOption, false>, meta: ActionMeta<SpendingOption>) {
+    return updateSpentFrom(newValue.spending);
+  }
+
+  const safeToSpend = {
+    label: 'Safe-To-Spend',
+    value: null,
+    spending: {
+      currentAmount: balances.safe,
+    },
+  }
+  const items: Map<number, SpendingOption> = allSpending
+    .sortBy(item => item.name.toLowerCase()) // Sort without case sensitivity.
+    .map(item => ({
+      label: item.name,
+      value: item.spendingId,
+      spending: item,
+    }));
+
+  const options = [
+    safeToSpend,
+    ...items.valueSeq().toArray(),
+  ];
+
+  const selectedItem = !transaction.spendingId ? safeToSpend : items.get(transaction.spendingId);
+
+  function formatOptionsLabel(option: SpendingOption, meta: FormatOptionLabelMeta<SpendingOption>): React.ReactNode {
+    if (meta.context === 'value') {
+      return (
+        <Fragment>
+          Spent From <span className={ classnames({ 'font-bold': !!option.value }) }>
+            { option.label }
+          </span>
+        </Fragment>
+      )
+    }
+
+    return option.label;
+  }
+
+  return (
+    <Select
+      theme={ (theme: Theme): Theme => ({
+        ...theme,
+        colors: {
+          ...theme.colors,
+          primary: '#4E1AA0',
+        },
+      }) }
+      components={ {
+        Option: SpendingSelectOption,
+      } }
+      classNamePrefix="transaction-spending-select"
+      isClearable={ false }
+      isDisabled={ false }
+      isLoading={ false }
+      onChange={ handleSpentFromChange }
+      formatOptionLabel={ formatOptionsLabel }
+      options={ options }
+      value={ selectedItem }
+    />
+  );
+}
