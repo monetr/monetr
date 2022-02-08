af453bbb6681d0d5b72a1e45684e2387ef9fbb4d
elliotcourant <me@elliotcourant.dev>
2022-02-08 14:53:05 +0000

    Deploying to docs.monetr.dev from @ monetr/monetr@bac640e2b1564567aaea60b047a68ac8fb7441f9 🚀

--- docs.go
+++ docs.go
@@ -3500,8 +3500,8 @@ const docTemplate_swagger = `{
     ]
 }`
 
-// SwaggerInfo_swagger holds exported Swagger Info so clients can modify it
-var SwaggerInfo_swagger = &swag.Spec{
+// SwaggerInfo holds exported Swagger Info so clients can modify it
+var SwaggerInfo = &swag.Spec{
 	Version:          "0.0",
 	Host:             "your.monetr.app/api",
 	BasePath:         "",
@@ -3513,5 +3513,5 @@ var SwaggerInfo_swagger = &swag.Spec{
 }
 
 func init() {
-	swag.Register(SwaggerInfo_swagger.InstanceName(), SwaggerInfo_swagger)
+	swag.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
 }
