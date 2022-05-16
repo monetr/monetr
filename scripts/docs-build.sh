1bc4f12a0fbaf5b2c3f5203ee2c5a6a461b6a1f1
Elliot Courant <me@elliotcourant.dev>
2022-05-15 21:38:49 -0500

    chore: Thing

--- scripts/docs-build.sh
+++ scripts/docs-build.sh
@@ -3,7 +3,9 @@
 echo "[docs] Installing swagger plugin!"
 pip install mkdocs-render-swagger-plugin mike
 
-git config --global user.name ${GIT_USER}
-git config --global user.email ${GIT_EMAIL}
+if [[ ! -z "${CI}" ]]; then
+  git config --global user.name ${GIT_USER}
+  git config --global user.email ${GIT_EMAIL}
+fi
 
 mike deploy --config=$PWD/mkdocs.yaml $1
