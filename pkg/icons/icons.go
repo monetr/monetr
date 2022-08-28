4c35527586a427a34a4daf277b61dbd587efd2cc
Elliot Courant <me@elliotcourant.dev>
2022-08-28 15:18:57 -0500

    fix(icons): Slightly improve icon search.

--- pkg/icons/icons.go
+++ pkg/icons/icons.go
@@ -16,7 +16,9 @@ type Icon struct {
 }
 
 func SearchIcon(input string) (*Icon, error) {
-	parts := strings.Split(input, " ")
+	// Search for icon using the parts of the input string as well as the string with no spaces. This helps account for
+	// some minor mismatches in naming between our icon slugs and transaction/merchant names.
+	parts := append(strings.Split(input, " "), strings.ReplaceAll(input, " ", ""))
 	for _, part := range parts {
 		for _, index := range indexes {
 			if icon := index.Search(part); icon != nil {
