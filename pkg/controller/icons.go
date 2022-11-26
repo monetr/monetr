cc72ea1663dc18b8d9493cb0e3049ece58ebefc3
Elliot Courant <me@elliotcourant.dev>
2022-11-26 11:46:16 -0600

    feat(ui): Allow icons to be searched via POST request.
    This will make it so that the transaction names no longer show up in the
    request URL in load balancer logs.
    
    Resolves #1175

--- pkg/controller/icons.go
+++ pkg/controller/icons.go
@@ -9,12 +9,28 @@ import (
 
 func (c *Controller) iconsController(p iris.Party) {
 	if icons.GetIconsEnabled() {
+		// DEPRECATED Use the POST method.
 		p.Get("/search", c.searchIcon)
+		p.Post("/search", c.searchIcon)
 	}
 }
 
 func (c *Controller) searchIcon(ctx iris.Context) {
-	name := ctx.URLParam("name")
+	var name string
+	switch ctx.Method() {
+	case "GET":
+		name = ctx.URLParam("name")
+	case "POST":
+		var body struct {
+			Name string `json:"name"`
+		}
+		if err := ctx.ReadJSON(&body); err != nil {
+			c.invalidJson(ctx)
+			return
+		}
+		name = body.Name
+	}
+
 	if name == "" {
 		c.badRequest(ctx, "must provide a name to search icons for")
 		return
