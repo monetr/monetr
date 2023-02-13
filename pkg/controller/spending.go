bcc33aa3cc0c77c9c1210df8ead4f2d87b47d956
Elliot Courant <me@elliotcourant.dev>
2023-02-13 16:47:36 -0600

    fix(api): Fixed bug creating expense.
    When creating an expense we were evaluating the next due date
    incorrectly and always returning an error. This fixes that by correcting
    the evaluation.
    
    Resolves #1310

--- pkg/controller/spending.go
+++ pkg/controller/spending.go
@@ -128,14 +128,14 @@ func (c *Controller) postSpending(ctx *context.Context) {
 
 	switch spending.SpendingType {
 	case models.SpendingTypeExpense:
+		next = spending.NextRecurrence
+		// Once we know that the next recurrence is not in the past we can just store it here;
+		// itll be sanitized and converted to midnight below.
 		if next.Before(time.Now()) {
 			requestSpan.Status = sentry.SpanStatusInvalidArgument
 			c.badRequest(ctx, "next due date cannot be inthe past")
 			return
 		}
-		// Once we know that the next recurrence is not in the past we can just store it here;
-		// itll be sanitized and converted to midnight below.
-		next = spending.NextRecurrence
 	case models.SpendingTypeGoal:
 		// If the spending is a goal, then we don't need the rule at all.
 		next = spending.NextRecurrence
