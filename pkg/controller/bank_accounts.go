236c80486442bb35682418ef497b82d6d8fee2d5
Elliot Courant <me@elliotcourant.dev>
2023-02-13 19:01:54 -0600

    feat(api): Added support for updating bank accounts.
    Bank accounts can now be updated with a PUT request. This is work
    towards manual budgeting as well as just supporting more basic CRUD
    operations for monetr's resources.
    
    Related to #1072

--- pkg/controller/bank_accounts.go
+++ pkg/controller/bank_accounts.go
@@ -1,17 +1,17 @@
 package controller
 
 import (
-	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/context"
-	"github.com/monetr/monetr/pkg/models"
-	_ "github.com/monetr/monetr/pkg/swag"
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/kataras/iris/v12"
+	"github.com/monetr/monetr/pkg/models"
 )
 
 func (c *Controller) handleBankAccounts(p iris.Party) {
 	p.Get("/", c.getBankAccounts)
+	p.Put("/{bankAccountId:uint64}", c.putBankAccounts)
 	p.Get("/{bankAccountId:uint64}/balances", c.getBalances)
 	p.Post("/", c.postBankAccounts)
 }
@@ -27,7 +27,7 @@ func (c *Controller) handleBankAccounts(p iris.Party) {
 // @Success 200 {array} swag.BankAccountResponse
 // @Failure 402 {object} SubscriptionNotActiveError The user's subscription is not active.
 // @Failure 500 {object} ApiError Something went wrong on our end.
-func (c *Controller) getBankAccounts(ctx *context.Context) {
+func (c *Controller) getBankAccounts(ctx iris.Context) {
 	repo := c.mustGetAuthenticatedRepository(ctx)
 	bankAccounts, err := repo.GetBankAccounts(c.getContext(ctx))
 	if err != nil {
@@ -51,7 +51,7 @@ func (c *Controller) getBankAccounts(ctx *context.Context) {
 // @Failure 400 {object} InvalidBankAccountIdError Invalid Bank Account ID.
 // @Failure 402 {object} SubscriptionNotActiveError The user's subscription is not active.
 // @Failure 500 {object} ApiError Something went wrong on our end.
-func (c *Controller) getBalances(ctx *context.Context) {
+func (c *Controller) getBalances(ctx iris.Context) {
 	bankAccountId := ctx.Params().GetUint64Default("bankAccountId", 0)
 	if bankAccountId == 0 {
 		c.returnError(ctx, http.StatusBadRequest, "must specify valid bank account Id")
@@ -82,7 +82,7 @@ func (c *Controller) getBalances(ctx *context.Context) {
 // @Success 200 {object} swag.BankAccountResponse
 // @Failure 402 {object} SubscriptionNotActiveError The user's subscription is not active.
 // @Failure 500 {object} ApiError Something went wrong on our end.
-func (c *Controller) postBankAccounts(ctx *context.Context) {
+func (c *Controller) postBankAccounts(ctx iris.Context) {
 	var bankAccount models.BankAccount
 	if err := ctx.ReadJSON(&bankAccount); err != nil {
 		c.wrapAndReturnError(ctx, err, http.StatusBadRequest, "malformed JSON")
@@ -125,4 +125,53 @@ func (c *Controller) postBankAccounts(ctx *context.Context) {
 	}
 
 	ctx.JSON(bankAccount)
+}
+
+func (c *Controller) putBankAccounts(ctx iris.Context) {
+	bankAccountId := ctx.Params().GetUint64Default("bankAccountId", 0)
+	if bankAccountId == 0 {
+		c.badRequest(ctx, "must specify a valid bank account Id")
+		return
+	}
+
+	repo := c.mustGetAuthenticatedRepository(ctx)
+	existingBankAccount, err := repo.GetBankAccount(c.getContext(ctx), bankAccountId)
+	if err != nil {
+		c.wrapPgError(ctx, err, "failed to retrieve bank account")
+		return
+	}
+
+	var request struct {
+		AvailableBalance int64                    `json:"availableBalance"`
+		CurrentBalance   int64                    `json:"currentBalance"`
+		Mask             string                   `json:"mask"`
+		Name             string                   `json:"name"`
+		Status           models.BankAccountStatus `json:"status"`
+	}
+	if err = ctx.ReadJSON(&request); err != nil {
+		c.invalidJson(ctx)
+		return
+	}
+
+	// TODO Eventually we should just query the link to see if its a manual link. But for now if the bank account has a
+	//  plaid account ID then its probably safe to assume that it is a Plaid managed bank account.
+	if existingBankAccount.PlaidAccountId != "" {
+		existingBankAccount.Name = strings.TrimSpace(request.Name)
+	} else {
+		existingBankAccount.AvailableBalance = request.AvailableBalance
+		existingBankAccount.CurrentBalance = request.CurrentBalance
+		existingBankAccount.Name = strings.TrimSpace(request.Name)
+		existingBankAccount.Mask = strings.TrimSpace(request.Mask)
+		// TODO Verify the provided status string is correct.
+		existingBankAccount.Status = request.Status
+	}
+
+	// TODO This might not reflect a correct updatedAt in the resulting value. Because this is not being passed by
+	//   reference.
+	if err = repo.UpdateBankAccounts(c.getContext(ctx), *existingBankAccount); err != nil {
+		c.wrapPgError(ctx, err, "failed to update bank account")
+		return
+	}
+
+	ctx.JSON(*existingBankAccount)
 }
