4db243e1472deadb3bd60ef59c3d88daaf4577d3
Elliot Courant <me@elliotcourant.dev>
2023-02-20 17:41:51 -0600

    feat(api): Add DTSTART equivalent support to spending/funding. (#1329)
    Right now we do not store the DTSTART component of the RRule, this has
    led to some bugs already where we have to assume the DTSTART based on
    some educated guess data. This can lead to wrong calculations in the
    future. So going forward I want to support this parameter. This is the
    start of the work on that.

--- pkg/repository/expenses.go
+++ pkg/repository/expenses.go
@@ -96,6 +96,7 @@ func (r *repositoryBase) CreateSpending(ctx context.Context, spending *models.Sp
 
 	spending.AccountId = r.AccountId()
 	spending.DateCreated = time.Now().UTC()
+	spending.DateStarted = spending.NextRecurrence
 
 	if _, err := r.txn.ModelContext(span.Context(), spending).Insert(spending); err != nil {
 		span.Status = sentry.SpanStatusInternalError
