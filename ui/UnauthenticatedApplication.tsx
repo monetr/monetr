c2db539a1c5681cd167216c55f4e79371639ed2e
Elliot Courant <me@elliotcourant.dev>
2023-03-15 22:11:37 -0500

    Revert "feat(ui): Adding more lazy routes to unauthenticated components."
    This reverts commit 6beafb3c2499fa05504f8bbb93fa5933b59da521.

--- ui/UnauthenticatedApplication.tsx
+++ ui/UnauthenticatedApplication.tsx
@@ -1,15 +1,14 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { useAppConfiguration } from 'hooks/useAppConfiguration';
 import LoginPage from 'pages/login';
-
-const ForgotPasswordPage = lazy(() => import('pages/password/forgot'));
-const ResetPasswordPage = lazy(() => import('pages/password/reset'));
-const RegisterPage = lazy(() => import('pages/register'));
-const VerifyEmailPage = lazy(() => import('pages/verify/email'));
-const ResendVerificationPage = lazy(() => import('pages/verify/email/resend'));
-const TOTPView = lazy(() => import('views/Authentication/TOTPView'));
+import ForgotPasswordPage from 'pages/password/forgot';
+import ResetPasswordPage from 'pages/password/reset';
+import RegisterPage from 'pages/register';
+import VerifyEmailPage from 'pages/verify/email';
+import ResendVerificationPage from 'pages/verify/email/resend';
+import TOTPView from 'views/Authentication/TOTPView';
 
 export default function UnauthenticatedApplication(): JSX.Element {
   const {
