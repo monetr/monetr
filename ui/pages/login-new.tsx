387eba7e133daf53b777674af98960046db9c7d4
Elliot Courant <me@elliotcourant.dev>
2023-05-12 22:19:00 -0500

    chore: More

--- ui/pages/login-new.tsx
+++ ui/pages/login-new.tsx
@@ -6,6 +6,17 @@ import MLogo from 'components/MLogo';
 import MSpan from 'components/MSpan';
 import MTextField from 'components/MTextField';
 import { useAppConfiguration } from 'hooks/useAppConfiguration';
+import { Formik, FormikHelpers } from 'formik';
+
+interface LoginValues {
+  email: string;
+  password: string;
+}
+
+const initialValues: LoginValues = {
+  email: '',
+  password: '',
+};
 
 export default function LoginNew(): JSX.Element {
   const config = useAppConfiguration();
@@ -36,34 +47,44 @@ export default function LoginNew(): JSX.Element {
     );
   }
 
+  async function submit(values: LoginValues, helpers: FormikHelpers<LoginValues>) {
+    return Promise.resolve();
+  }
+
   return (
-    <form className="w-full h-full flex pt-10 md:pt-0 md:pb-10 md:justify-center items-center flex-col gap-5">
-      <div className="max-w-[128px] w-full">
-        <MLogo />
-      </div>
-      <MSpan>Sign into your monetr account</MSpan>
-      <MTextField
-        label="Email Address"
-        name='email'
-        type='email'
-        required
-        className="w-full lg:w-1/4 sm:w-1/3"
-      />
-      <MTextField
-        label="Password"
-        name='password'
-        type='password'
-        required
-        labelDecorator={ ForgotPasswordButton }
-        className="w-full lg:w-1/4 sm:w-1/3"
-      />
-      <div className="w-full lg:w-1/4 sm:w-1/3 mt-1">
-        <MButton color="primary" variant="solid">
-          Sign In
-        </MButton>
-      </div>
-      <SignUpButton />
-    </form>
+    <Formik
+      initialValues={ initialValues }
+      onSubmit={ submit }
+    >
+      <form className="w-full h-full flex pt-10 md:pt-0 md:pb-10 md:justify-center items-center flex-col gap-5">
+        <div className="max-w-[128px] w-full">
+          <MLogo />
+        </div>
+        <MSpan>Sign into your monetr account</MSpan>
+        <MTextField
+          autoFocus
+          label="Email Address"
+          name='email'
+          type='email'
+          required
+          className="w-full xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2"
+        />
+        <MTextField
+          label="Password"
+          name='password'
+          type='password'
+          required
+          labelDecorator={ ForgotPasswordButton }
+          className="w-full xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2"
+        />
+        <div className="w-full xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 mt-1">
+          <MButton color="primary" variant="solid" role="form" type="submit">
+            Sign In
+          </MButton>
+        </div>
+        <SignUpButton />
+      </form>
+    </Formik>
   );
 }
 
