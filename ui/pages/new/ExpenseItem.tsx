a7728165331de4563dac8edb6cd060c6fbe0a69a
Elliot Courant <me@elliotcourant.dev>
2023-07-03 21:15:46 -0500

    chore: More icons

--- ui/pages/new/ExpenseItem.tsx
+++ ui/pages/new/ExpenseItem.tsx
@@ -4,6 +4,7 @@ import { Avatar } from '@mui/material';
 import clsx from 'clsx';
 
 import { useFundingSchedule } from 'hooks/fundingSchedules';
+import { useIconSearch } from 'hooks/useIconSearch';
 import Spending from 'models/Spending';
 import { rrulestr } from 'rrule';
 
@@ -13,8 +14,38 @@ export interface ExpenseItemProps {
 
 export default function ExpenseItem({ spending }: ExpenseItemProps): JSX.Element {
   const fundingSchedule = useFundingSchedule(spending.fundingScheduleId);
+  const icon = useIconSearch(spending.name);
+  const IconContent = () => {
+    if (icon?.svg) {
+      // It is possible for colors to be missing for a given icon. When this happens just fall back to a black color.
+      const colorStyles = icon?.colors?.length > 0 ?
+        { backgroundColor: `#${icon.colors[0]}` } :
+        { backgroundColor: '#000000' };
 
-  const letter = spending.name.toUpperCase().charAt(0);
+      const styles = {
+        WebkitMaskImage: `url(data:image/svg+xml;base64,${icon.svg})`,
+        WebkitMaskRepeat: 'no-repeat',
+        height: '30px',
+        width: '30px',
+        ...colorStyles,
+      };
+
+      return (
+        <div className='bg-white flex items-center justify-center h-10 w-10 rounded-full'>
+          <div style={ styles } />
+        </div>
+      );
+    }
+
+    // If we have no icon to work with then create an avatar with the first character of the transaction name.
+    const letter = spending.name.toUpperCase().charAt(0);
+    return (
+      <Avatar className='bg-zinc-800 h-10 w-10'>
+        {letter}
+      </Avatar>
+    );
+  };
+
   const rule = rrulestr(spending.recurrenceRule);
 
   const amountClass = clsx([
@@ -29,9 +60,7 @@ export default function ExpenseItem({ spending }: ExpenseItemProps): JSX.Element
     <li className='w-full px-2'>
       <div className='w-full flex rounded-lg hover:bg-zinc-600 group gap-2 items-center px-2 py-1'>
         <div className='flex items-center flex-grow w-full gap-4 min-w-0'>
-          <Avatar className='bg-zinc-800 h-10 w-10'>
-            { letter }
-          </Avatar>
+          <IconContent />
           <div className='flex flex-col justify-center overflow-hidden'>
             <span
               className='text-zinc-50 font-semibold text-base overflow-hidden text-ellipsis whitespace-nowrap min-w-0'
