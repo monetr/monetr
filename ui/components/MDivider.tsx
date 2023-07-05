38feb0af0eebe242dc27c0a66e777f7e081b58fb
Elliot Courant <me@elliotcourant.dev>
2023-07-04 21:02:10 -0500

    chore: Oops

---
+++ ui/components/MDivider.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import mergeTailwind from 'util/mergeTailwind';
+
+export interface MDividerProps {
+  className?: string;
+}
+
+export default function MDivider(props: MDividerProps): JSX.Element {
+  const className = mergeTailwind(
+    'border-0 border-b-[thin] dark:border-dark-monetr-border',
+    props.className,
+  );
+
+  return (
+    <hr className={ className } />
+  );
+}
