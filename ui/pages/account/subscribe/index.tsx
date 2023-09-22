d55aa4a2d225d2aba4b8416186a40cdef0140f04
Elliot Courant <me@elliotcourant.dev>
2023-09-21 20:02:19 -0500

    fix(ui): Subscribe page now handles re-subscriptions.

--- ui/pages/account/subscribe/index.tsx
+++ ui/pages/account/subscribe/index.tsx
@@ -14,7 +14,7 @@ export default function SubscribePage(): JSX.Element {
   const {
     initialPlan,
   } = useAppConfiguration();
-  const { result: { hasSubscription } } = useAuthenticationSink();
+  const { result: { hasSubscription, activeUntil } } = useAuthenticationSink();
 
   const [loading, setLoading] = useState(false);
   const handleContinue = useCallback(() => {
@@ -41,6 +41,34 @@ export default function SubscribePage(): JSX.Element {
         });
       });
   }, [enqueueSnackbar, hasSubscription, initialPlan]);
+
+  if (activeUntil) {
+    return (
+      <div className="flex items-center justify-center w-full h-full max-h-full p-4">
+        <div className='h-full flex flex-col max-w-md gap-4 items-center justify-between'>
+          <div className='h-full flex flex-col justify-center items-center gap-4'>
+            <MLogo className='max-h-24' />
+            <MSpan size='2xl' weight='semibold' className='text-center'>
+              Your subscription is no longer active
+            </MSpan>
+            <MSpan size='lg' className='text-center'>
+              Thank you for having subscribed to monetr before! If you'd like to continue using monetr you will have to
+              resubscribe below. Click continue to proceed to our billing portal.
+            </MSpan>
+            <MBaseButton color='primary' disabled={ loading } onClick={ handleContinue }>
+              Continue
+            </MBaseButton>
+          </div>
+          { !loading &&
+          <div className='flex justify-center gap-1'>
+            <MSpan color="subtle" className='text-sm'>Not ready to continue?</MSpan>
+            <MLink to="/logout" size="sm">Logout for now</MLink>
+          </div>
+          }
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center justify-center w-full h-full max-h-full p-4">
