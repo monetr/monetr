f6223a5b3ae042a153ac3424814a2814b3e1cee2
Elliot Courant <me@elliotcourant.dev>
2023-09-30 12:38:32 -0500

    feat(ui): Added new update plaid account overlay.
    The new UI now supports reauthenticating a Plaid connection or updating
    the account selection for that connection.

--- ui/components/Layout/BankSidebarItem.tsx
+++ ui/components/Layout/BankSidebarItem.tsx
@@ -45,8 +45,8 @@ export default function BankSidebarItem({ link }: BankSidebarItemProps): JSX.Ele
 
     return (
       <span className="absolute flex h-3 w-3 right-0 bottom-0">
-        <span className="animate-ping-slow absolute inline-flex h-full w-full rounded-full bg-yellow-400"></span>
-        <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
+        <span className="animate-ping-slow absolute inline-flex h-full w-full rounded-full bg-yellow-400" />
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500" />
       </span>
     );
   };
--- ui/components/Layout/BudgetingSidebarTitle.tsx
+++ ui/components/Layout/BudgetingSidebarTitle.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable max-len */
 import React, { Fragment, useCallback, useState } from 'react';
-import { AutorenewOutlined, DeleteOutline, MoreVert, PriceChangeOutlined } from '@mui/icons-material';
+import { Autorenew, AutorenewOutlined, DeleteOutline, MoreVert, PriceChangeOutlined } from '@mui/icons-material';
 import { Popover } from '@mui/material';
 
 import MDivider from 'components/MDivider';
 import MSpan from 'components/MSpan';
+import { ReactElement } from 'components/types';
 import { useSelectedBankAccount } from 'hooks/bankAccounts';
 import { useLink } from 'hooks/links';
 import { showRemoveLinkModal } from 'modals/RemoveLinkModal';
-import Link from 'models/Link';
+import { showUpdatePlaidAccountOverlay } from 'modals/UpdatePlaidAccountOverlay';
 
 export default function BudgetingSidebarTitle(): JSX.Element {
   const { data: bankAccount, isLoading, isError } = useSelectedBankAccount();
@@ -17,10 +18,36 @@ export default function BudgetingSidebarTitle(): JSX.Element {
   const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
   const open = Boolean(anchorEl);
 
+  const closeMenu = useCallback(() => setAnchorEl(null), [setAnchorEl]);
+
   const handleClick = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
     setAnchorEl(event.currentTarget);
   }, [setAnchorEl]);
   const handleClose = useCallback(() => setAnchorEl(null), [setAnchorEl]);
+
+  const handleReauthenticateLink = useCallback(() => {
+    closeMenu();
+    showUpdatePlaidAccountOverlay({
+      link: link,
+    });
+  }, [closeMenu, link]);
+
+  const handleUpdateAccountSelection = useCallback(() => {
+    closeMenu();
+    showUpdatePlaidAccountOverlay({
+      link: link,
+      updateAccountSelection: true,
+    });
+  }, [closeMenu, link]);
+
+  const handleRemoveLink = useCallback(() => {
+    closeMenu();
+    showRemoveLinkModal({ link: link });
+  }, [closeMenu, link]);
+
+  if (!link) {
+    return null;
+  }
 
   return (
     <Fragment>
@@ -46,16 +73,14 @@ export default function BudgetingSidebarTitle(): JSX.Element {
         className='ml-[5px]'
       >
         <div className='flex flex-col dark:bg-dark-monetr-background rounded-lg border dark:border-dark-monetr-border-subtle dark:shadow-2xl' style={ { width: `${anchorEl?.offsetWidth - 10}px` } }>
-          <MSpan
-            size='md'
-            weight='medium'
-            className='p-2 cursor-pointer dark:hover:bg-dark-monetr-background-emphasis dark:hover:text-dark-monetr-content-emphasis'
-            component='a'
-            ellipsis
-          >
+          <MenuItem visible={ link.getIsPlaid() && link.getIsError() } onClick={ handleReauthenticateLink }>
+            <Autorenew className='mr-1 mb-0.5' />
+            Reauthenticate
+          </MenuItem>
+          <MenuItem visible={ link.getIsPlaid() } onClick={ handleUpdateAccountSelection }>
             <PriceChangeOutlined className='mr-1 mb-0.5' />
             Update Account Selection
-          </MSpan>
+          </MenuItem>
           <MSpan
             size='md'
             weight='medium'
@@ -67,23 +92,26 @@ export default function BudgetingSidebarTitle(): JSX.Element {
             Manually Resync
           </MSpan>
           <MDivider />
-          <RemoveLinkMenuItem link={ link } closeMenu={ handleClose } />
+          <MenuItem visible={ link.getIsPlaid() } onClick={ handleRemoveLink }>
+            <DeleteOutline className='mr-1 mb-0.5 dark:text-dark-monetr-red' />
+            Remove {link?.getName()}
+          </MenuItem>
         </div>
       </Popover>
     </Fragment>
   );
 }
 
-interface RemoveLinkMenuItemProps {
-  link: Link;
-  closeMenu: () => void;
+interface MenuItemProps {
+  visible?: boolean;
+  onClick: () => unknown;
+  children?: ReactElement;
 }
 
-function RemoveLinkMenuItem({ link, closeMenu }: RemoveLinkMenuItemProps): JSX.Element {
-  const handleRemoveLink = useCallback(() => {
-    closeMenu();
-    showRemoveLinkModal({ link: link });
-  }, [closeMenu, link]);
+function MenuItem({ visible, onClick, children }: MenuItemProps): JSX.Element {
+  if (!visible) {
+    return null;
+  }
 
   return (
     <MSpan
@@ -92,10 +120,9 @@ function RemoveLinkMenuItem({ link, closeMenu }: RemoveLinkMenuItemProps): JSX.E
       className='p-2 cursor-pointer dark:hover:bg-dark-monetr-background-emphasis dark:hover:text-dark-monetr-content-emphasis'
       component='a'
       ellipsis
-      onClick={ handleRemoveLink }
+      onClick={ onClick }
     >
-      <DeleteOutline className='mr-1 mb-0.5 dark:text-dark-monetr-red' />
-      Remove {link?.getName()}
+      { children }
     </MSpan>
   );
 }
