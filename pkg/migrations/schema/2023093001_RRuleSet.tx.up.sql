af4e57fd2b16a9e7bab2e79be7003436a4ca7f48
Elliot Courant <me@elliotcourant.dev>
2023-10-03 14:20:21 -0500

    chore: Fix migration

--- pkg/migrations/schema/2023093001_RRuleSet.tx.up.sql
+++ pkg/migrations/schema/2023093001_RRuleSet.tx.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "funding_schedules" ADD COLUMN "rruleset" TEXT;
+ALTER TABLE "funding_schedules" ADD COLUMN "ruleset" TEXT;
 
 WITH migrated_rules AS (
   SELECT
@@ -6,13 +6,13 @@ WITH migrated_rules AS (
     -- 20120201T023000Z
     'DTSTART:' || to_char(date_started AT TIME ZONE 'UTC', 'YYYYMMDD') || to_char(date_started AT TIME ZONE 'UTC', 'THH24MISSZ') || E'\n' ||
       'RRULE:' || rule AS "ruleset"
-  FROM funding_schedules;
-);
+  FROM funding_schedules
+)
 
 UPDATE "funding_schedules"
 SET "ruleset"="migrated_rules"."ruleset"
 FROM "migrated_rules"
 WHERE "funding_schedules"."funding_schedule_id"="migrated_rules"."funding_schedule_id";
 
-ALTER TABLE "funding_schedules" ALTER COLUMN "rruleset" SET NOT NULL;
+ALTER TABLE "funding_schedules" ALTER COLUMN "ruleset" SET NOT NULL;
 
