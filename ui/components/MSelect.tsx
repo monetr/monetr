dd17c1a5a931c6cb710894c1890af10590ea0625
Elliot Courant <me@elliotcourant.dev>
2023-10-05 13:39:51 -0500

    feat(ui): Added mobile friendly select component.

--- ui/components/MSelect.tsx
+++ ui/components/MSelect.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import Select, { Theme } from 'react-select';
+import React, { ReactNode } from 'react';
+import Select, { ActionMeta, GroupBase, OnChangeValue, OptionsOrGroups, Theme } from 'react-select';
+import NiceModal, { useModal } from '@ebay/nice-modal-react';
+import { CheckCircleOutline } from '@mui/icons-material';
+import { SwipeableDrawer } from '@mui/material';
 
 import MLabel, { MLabelDecorator, MLabelDecoratorProps } from './MLabel';
+import MSpan from './MSpan';
 
 import useTheme from 'hooks/useTheme';
 import mergeTailwind from 'util/mergeTailwind';
+import { ExtractProps } from 'util/typescriptEvils';
 
-export interface MSelectProps<V> extends Omit<Parameters<Select>[0], 'theme'|'styles'|'isDisabled'> {
+export interface Value<T> {
+  label: string;
+  value: T;
+}
+
+export interface MSelectProps<V extends Value<any>> extends Omit<Parameters<Select>[0], 'theme'|'styles'|'isDisabled'> {
   label?: string;
   labelDecorator?: MLabelDecorator;
   error?: string;
@@ -15,7 +25,7 @@ export interface MSelectProps<V> extends Omit<Parameters<Select>[0], 'theme'|'st
   value?: V | undefined;
 }
 
-export default function MSelect<V>(props: MSelectProps<V>): JSX.Element {
+export default function MSelect<V extends Value<any> = Value<any>>(props: MSelectProps<V>): JSX.Element {
   const theme = useTheme();
 
   function Error() {
@@ -28,13 +38,72 @@ export default function MSelect<V>(props: MSelectProps<V>): JSX.Element {
     );
   }
 
+  const { labelDecorator, className, ...otherProps } = props;
   const wrapperClassNames = mergeTailwind({
     // This will make it so the space below the input is the same when there is and isn't an error.
     'pb-[18px]': !props.error,
-  }, props.className);
+  }, className);
 
-  const { labelDecorator, ...otherProps } = props;
   const LabelDecorator = labelDecorator ?? ((_: MLabelDecoratorProps) => null);
+
+  const classNames = mergeTailwind(
+    {
+      'dark:focus:ring-dark-monetr-brand': !props.disabled && !props.error,
+      'dark:hover:ring-zinc-400': !props.disabled && !props.error,
+      'dark:ring-dark-monetr-border-string': !props.disabled && !props.error,
+      'dark:ring-red-500': !props.disabled && !!props.error,
+      'ring-gray-300': !props.disabled && !props.error,
+      'ring-red-300': !props.disabled && !!props.error,
+    },
+    {
+      'focus:ring-purple-400': !props.error,
+      'focus:ring-red-400': props.error,
+    },
+    {
+      'dark:bg-dark-monetr-background': !props.disabled,
+      'dark:text-zinc-200': !props.disabled,
+      'text-gray-900': !props.disabled,
+    },
+    { // If there is not a value, the change the text of the button to be 400 for the placeholder.
+      'dark:text-gray-400': !Boolean(props.value),
+    },
+    {
+      'dark:bg-dark-monetr-background-subtle': props.disabled,
+      'dark:ring-dark-monetr-background-emphasis': props.disabled,
+      'ring-gray-200': props.disabled,
+      'text-gray-500': props.disabled,
+    },
+    'block',
+    'md:hidden',
+    'border-0',
+    'dark:caret-zinc-50',
+    'focus:ring-2',
+    'focus:ring-inset',
+    'min-h-[38px]',
+    'px-3',
+    'py-1.5',
+    'ring-1',
+    'ring-inset',
+    'rounded-lg',
+    'shadow-sm',
+    'sm:leading-6',
+    'text-left',
+    'text-sm',
+    'w-full',
+    'relative',
+  );
+
+  function ValueContainer(): JSX.Element {
+    if (props.value?.label) {
+      return (
+        <span className="truncate">{ props?.value?.label }</span>
+      );
+    }
+
+    return (
+      <span className="truncate">{ props?.placeholder }</span>
+    );
+  }
 
   return (
     <div className={ wrapperClassNames }>
@@ -46,6 +115,20 @@ export default function MSelect<V>(props: MSelectProps<V>): JSX.Element {
       >
         <LabelDecorator name={ props.name } disabled={ props.disabled } />
       </MLabel>
+      <button
+        type='button'
+        disabled={ props.disabled }
+        className={ classNames }
+        onClick={ () => showSelectModal({
+          title: props.placeholder,
+          options: props.options,
+          value: props.value,
+          onChange: props.onChange,
+        }) }
+        role='none'
+      >
+        <ValueContainer />
+      </button>
       <Select
         theme={ (baseTheme: Theme): Theme => ({
           ...baseTheme,
@@ -67,6 +150,7 @@ export default function MSelect<V>(props: MSelectProps<V>): JSX.Element {
           },
         }) }
         { ...otherProps }
+        className='hidden md:block'
         isDisabled={ props.disabled }
         styles={ {
           placeholder: (base: object) => ({
@@ -92,4 +176,68 @@ export default function MSelect<V>(props: MSelectProps<V>): JSX.Element {
       <Error />
     </div>
   );
+}
+
+interface SelectModalProps {
+  title: ReactNode;
+  options: OptionsOrGroups<unknown, GroupBase<unknown>>;
+  value: unknown;
+  onChange: (newValue: OnChangeValue<any, false>, meta: ActionMeta<any>) => unknown;
+}
+
+function SelectModal(
+  props: SelectModalProps,
+): JSX.Element {
+  const modal = useModal();
+
+  const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const options: OptionsOrGroups<Value<unknown>, GroupBase<Value<unknown>>> =
+    props.options as OptionsOrGroups<Value<unknown>, GroupBase<Value<unknown>>>;
+
+  return (
+    <SwipeableDrawer
+      disableBackdropTransition={ !iOS } disableDiscovery={ iOS }
+      anchor="bottom"
+      open={ modal.visible }
+      onClose={ modal.hide }
+      onOpen={ () => modal.show() }
+      className='backdrop-blur-sm backdrop-brightness-50'
+    >
+      <div className='h-full flex flex-col gap-4 bg-dark-monetr-background pb-8'>
+        <MSpan weight='bold' size='xl' className='p-2'>
+          { props.title }
+        </MSpan>
+
+        <ul className='w-full flex flex-col gap-2'>
+          { options.map(item => (
+            <li
+              key={ item['value'] }
+              className='w-full flex items-center active:bg-dark-monetr-background-subtle py-2'
+              onClick={ () => {
+                props.onChange(item, undefined);
+                modal.hide();
+              }  }
+            >
+              { props.value?.['value'] === item['value'] && (
+                <CheckCircleOutline className='mx-2 w-6' />
+              )}
+              { props.value?.['value'] !== item['value'] && (
+                <div className='mx-2 w-6' />
+              )}
+              <MSpan size='lg' weight='medium'>
+                { item.label }
+              </MSpan>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </SwipeableDrawer>
+  );
+}
+
+const selectModal = NiceModal.create<SelectModalProps>(SelectModal);
+
+function showSelectModal(props: SelectModalProps): Promise<Value<unknown>> {
+  return NiceModal.show<Value<unknown>, ExtractProps<typeof selectModal>, {}>(selectModal, props);
 }
