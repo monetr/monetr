d415b5ab733edbbea2ba37a323374939ffb85c05
Elliot Courant <me@elliotcourant.dev>
2023-10-12 20:29:10 -0500

    fix(ui): Fixed transaction icons shrinking for long names.
    When a transaction would have a very long name, it would cause the
    transaction icon to shrink horizontally a bit. This was just a flexbox
    issue and is resolved by preventing the transaction icon from flexing at
    all.

--- ui/pages/new/MerchantIcon.tsx
+++ ui/pages/new/MerchantIcon.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Avatar } from '@mui/material';
 
 import { useIconSearch } from 'hooks/useIconSearch';
+import mergeTailwind from 'util/mergeTailwind';
 
 export interface MerchantIconProps {
   name?: string;
@@ -11,6 +12,17 @@ export default function MerchantIcon(props: MerchantIconProps): JSX.Element {
   const icon = useIconSearch(props?.name);
   const size = 30;
   if (icon?.svg) {
+    const classNames = mergeTailwind(
+      'dark:bg-dark-monetr-background-bright',
+      'flex',
+      'items-center',
+      'justify-center',
+      'h-10',
+      'w-10',
+      'rounded-full',
+      'flex-none',
+    );
+
     // It is possible for colors to be missing for a given icon. When this happens just fall back to a black color.
     const colorStyles = icon?.colors?.length > 0 ?
       { backgroundColor: `#${icon.colors[0]}` } :
@@ -26,7 +38,7 @@ export default function MerchantIcon(props: MerchantIconProps): JSX.Element {
     };
 
     return (
-      <div className='dark:bg-dark-monetr-background-bright flex items-center justify-center h-10 w-10 rounded-full'>
+      <div className={ classNames }>
         <div style={ styles } />
       </div>
     );
@@ -35,7 +47,7 @@ export default function MerchantIcon(props: MerchantIconProps): JSX.Element {
   // If we have no icon to work with then create an avatar with the first character of the transaction name.
   const letter = props?.name?.toUpperCase().charAt(0) || '?';
   return (
-    <Avatar className='dark:bg-dark-monetr-background-subtle h-10 w-10 dark:text-dark-monetr-content'>
+    <Avatar className='dark:bg-dark-monetr-background-subtle h-10 w-10 dark:text-dark-monetr-content flex-none'>
       {letter}
     </Avatar>
   );
