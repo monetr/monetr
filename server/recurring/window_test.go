40353eb80bef11d76e94db9dc524d83eae2b4f7a
Elliot Courant <me@elliotcourant.dev>
2023-11-19 12:16:47 -0600

    chore: Adding more testing

--- server/recurring/window_test.go
+++ server/recurring/window_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/adrg/strutil/metrics"
+	"github.com/monetr/monetr/server/internal/myownsanity"
 	"github.com/monetr/monetr/server/internal/testutils"
 	"github.com/stretchr/testify/assert"
 )
@@ -87,5 +89,58 @@ func TestWindowGetDeviation(t *testing.T) {
 			assert.EqualValues(t, -1, delta, "should have a delta of -1 for an invalid day")
 			assert.False(t, ok, "ok should be false if the provided date is outside the window")
 		}
+	})
+
+	t.Run("with real data", func(t *testing.T) {
+		timezone := testutils.Must(t, time.LoadLocation, "America/Chicago")
+		data := GetFixtures(t, "monetr_sample_data_1.json")
+		comparison := &transactionComparatorBase{
+			impl: &metrics.JaroWinkler{
+				CaseSensitive: false,
+			},
+		}
+		searcher := &TransactionSearch{
+			nameComparator:     comparison,
+			merchantComparator: comparison,
+		}
+		baseline := Transaction{
+			TransactionId: 291,
+			Amount:        1500,
+			OriginalCategories: []string{
+				"Service",
+				"Financial",
+				"Accounting and Bookkeeping",
+			},
+			Date:                 time.Date(2021, 7, 10, 0, 0, 0, 0, timezone),
+			OriginalName:         "FreshBooks. Merchant name: Freshbooks",
+			OriginalMerchantName: myownsanity.StringP("FreshBooks"),
+		}
+
+		windows := GetWindowsForDate(baseline.Date, timezone)
+
+		result := searcher.FindSimilarTransactions(baseline, data)
+		assert.NotEmpty(t, result, "should have found at least some transactions")
+
+		type Match struct {
+			Transaction Transaction
+			Window      Window
+			Delta       int
+		}
+		matches := make([]Match, 0, len(result))
+
+		for _, txn := range result {
+			for _, window := range windows {
+				delta, ok := window.GetDeviation(txn.Date)
+				if ok {
+					matches = append(matches, Match{
+						Transaction: txn,
+						Window:      window,
+						Delta:       delta,
+					})
+				}
+			}
+		}
+
+		assert.NotEmpty(t, matches)
 	})
 }
