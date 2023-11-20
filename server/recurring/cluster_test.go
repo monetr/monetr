864f720149b1246b2543060de275ee7a78765ece
Elliot Courant <me@elliotcourant.dev>
2023-11-19 19:44:34 -0600

    chore: Getting DBSCAN working

--- server/recurring/cluster_test.go
+++ server/recurring/cluster_test.go
@@ -1,6 +1,8 @@
 package recurring
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,4 +25,35 @@ func TestPreProcessor(t *testing.T) {
 	processor.PostPrepareCalculations()
 
 	assert.NotEmpty(t, processor.idf)
+
+	datums := processor.GetDatums()
+
+	// First test with 0.4 and 3 was excellent!
+	dbscan := NewDBSCAN(datums, 0.4, 3)
+	result := dbscan.Calculate()
+	assert.NotEmpty(t, result)
+	type Presentation struct {
+		ID     uint64 `json:"id"`
+		Name   string `json:"name"`
+		Amount int64  `json:"amount"`
+	}
+	output := make([][]Presentation, len(result))
+	for i, cluster := range result {
+		output[i] = make([]Presentation, 0, len(cluster.Items))
+		for _, item := range cluster.Items {
+			output[i] = append(output[i], Presentation{
+				ID:     item.Transaction.TransactionId,
+				Name:   item.Transaction.OriginalName,
+				Amount: item.Transaction.Amount,
+			})
+		}
+	}
+
+	j, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(j))
+
 }
