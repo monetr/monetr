ef22b1dac7e38ce3b4e0af555f3c748abd08d6cf
Elliot Courant <me@elliotcourant.dev>
2023-12-19 21:46:06 -0600

    chore: More test fixes

--- server/models/rule_test.go
+++ server/models/rule_test.go
@@ -8,26 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewRule(t *testing.T) {
-	t.Run("simple", func(t *testing.T) {
-		input := "FREQ=MONTHLY;BYMONTHDAY=15,-1"
-		rule, err := NewRule(input)
-		assert.NoError(t, err, "must be able to parse semi-monthly rule")
-		nextRecurrence := rule.After(time.Date(2022, 4, 5, 0, 0, 0, 0, time.UTC), false).Truncate(24 * time.Hour)
-		assert.Equal(t, time.Date(2022, 4, 15, 0, 0, 0, 0, time.UTC), nextRecurrence, "next recurrence should be equal")
-	})
-}
-
-func TestRuleString(t *testing.T) {
-	t.Run("simple", func(t *testing.T) {
-		input := "FREQ=MONTHLY;BYMONTHDAY=15,-1"
-		rule, err := NewRule(input)
-		assert.NoError(t, err, "must be able to parse semi-monthly rule")
-		assert.NotEmpty(t, rule.String(), "should produce a string")
-		assert.Equal(t, input, rule.RRule.OrigOptions.RRuleString(), "should produce the expect string rule")
-	})
-}
-
 func TestRuleSet_MarshalJSON(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
 		set, err := NewRuleSet("DTSTART:20230831T050000Z\nRRULE:FREQ=MONTHLY;INTERVAL=1;BYMONTHDAY=15,-1")
