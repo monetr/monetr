f1f39ab0bfa3de4a022c9e828ffd8b579519d647
Elliot Courant <me@elliotcourant.dev>
2024-01-02 18:26:00 -0600

    chore: Progress

--- interface/src/modals/UploadTransactionsModal.tsx
+++ interface/src/modals/UploadTransactionsModal.tsx
@@ -7,7 +7,9 @@ import { AxiosProgressEvent } from 'axios';
 import { MBaseButton } from '@monetr/interface/components/MButton';
 import MModal, { MModalRef } from '@monetr/interface/components/MModal';
 import MSpan from '@monetr/interface/components/MSpan';
+import { ReactElement } from '@monetr/interface/components/types';
 import { useSelectedBankAccountId } from '@monetr/interface/hooks/bankAccounts';
+import MonetrFile from '@monetr/interface/models/File';
 import fileSize from '@monetr/interface/util/fileSize';
 import mergeTailwind from '@monetr/interface/util/mergeTailwind';
 import request from '@monetr/interface/util/request';
@@ -15,21 +17,57 @@ import { ExtractProps } from '@monetr/interface/util/typescriptEvils';
 
 enum UploadTransactionStage {
   FileUpload = 1,
-  FieldMapping = 3,
-  Processing = 4,
-  Completed = 5,
-  Error = 6,
+  FieldMapping = 2,
+  Processing = 3,
+  Completed = 4,
+  Error = 5,
+}
+
+interface UploadContext {
+  fileToUpload: File | null;
+  stage: UploadTransactionStage;
+  file: MonetrFile | null;
+}
+
+interface UploadTransactionContextWrapperProps {
+  children: ReactElement;
+}
+
+const Context = React.createContext<[UploadContext, (update: Partial<UploadContext>) => void]>([{
+  fileToUpload: null,
+  file: null,
+  stage: UploadTransactionStage.FileUpload,
+}, () => {}]);
+
+function UploadContextWrapper(props: UploadTransactionContextWrapperProps): JSX.Element {
+  const [state, setState] = useState<UploadContext>({
+    fileToUpload: null,
+    file: null,
+    stage: UploadTransactionStage.FileUpload,
+  });
+  const setStateWrapper = useCallback((update: Partial<UploadContext>) => {
+    setState((previousState: UploadContext) => ({
+      ...previousState,
+      ...update,
+    }));
+  }, [setState]);
+  return (
+    <Context.Provider value={ [state, setStateWrapper] }>
+      { props.children }
+    </Context.Provider>
+  );
 }
 
 function UploadTransactionsModal(): JSX.Element {
   const modal = useModal();
   const ref = useRef<MModalRef>(null);
+
   const [stage, setStage] = useState<UploadTransactionStage>(UploadTransactionStage.FileUpload);
 
   function CurrentStage(): JSX.Element {
     switch (stage) {
       case UploadTransactionStage.FileUpload:
-        return <UploadFileStage setFileId={ () => {} } setStage={ setStage } />;
+        return <UploadFileStage setResult={ () => {} } setStage={ setStage } />;
       default:
         return null;
     }
@@ -44,7 +82,7 @@ function UploadTransactionsModal(): JSX.Element {
 }
 
 interface StageProps {
-  setFileId: (fileId: number) => void;
+  setResult: (file: MonetrFile) => void;
   setStage: (stage: UploadTransactionStage) => void;
 }
 
@@ -76,9 +114,8 @@ function UploadFileStage(props: StageProps) {
     setUploadProgress(0);
 
     return request()
-      .post(`/bank_accounts/${ selectedBankAccountId }/files/transactions`, formData, config)
+      .post(`/bank_accounts/${ selectedBankAccountId }/files`, formData, config)
       .then(result => {
-        props.setFileId(result?.data?.fileId);
         setTimeout(() => {
           switch (file.type) {
             case 'text/csv':
@@ -88,6 +125,7 @@ function UploadFileStage(props: StageProps) {
               props.setStage(UploadTransactionStage.Processing);
               break;
           }
+          props.setResult(new MonetrFile(result.data));
         }, 1000);
       })
       .catch(error => {
@@ -123,7 +161,6 @@ function UploadFileStage(props: StageProps) {
                 <div className='absolute top-0 bg-green-600 h-1.5 rounded-full dark:bg-green-600' style={ { width: `${uploadProgress}%` } }></div>
               </div>
             </div>
-            <Close className='mr-2 text-dark-monetr-content-subtle hover:text-dark-monetr-content' />
           </div>
         </div>
       </div>
@@ -152,7 +189,7 @@ function UploadFileStage(props: StageProps) {
               <MSpan size='lg'>{ file.name }</MSpan>
               <MSpan>{ fileSize(file.size) }</MSpan>
             </div>
-            <Close className='mr-2 text-dark-monetr-content-subtle hover:text-dark-monetr-content' />
+            <Close className='mr-2 text-dark-monetr-content-subtle hover:text-dark-monetr-content cursor-pointer' />
           </div>
         </div>
         <div className='flex justify-end gap-2 mt-2'>
