0f179a2c07cb35bacc8698f8c9272f1d26036075
Elliot Courant <me@elliotcourant.dev>
2024-01-21 13:04:16 -0600

    chore: Adding docs on new table schemas

--- server/migrations/schema/2024010700_PlaidTables.tx.up.sql
+++ server/migrations/schema/2024010700_PlaidTables.tx.up.sql
@@ -1,3 +1,218 @@
+/*
+
+This is a huge migration to decouple the common models that monetr uses from the plaid data that they are derived from.
+This is done so that adding other data sources in the future should be easier, things like manual import, finicity or 
+teller.io.
+
+Here is a general list of what the new tables will look like:
+
+## Plaid Syncs
+
+                                              Table "public.plaid_syncs"
+    Column     |           Type           | Collation | Nullable |                      Default
+---------------+--------------------------+-----------+----------+----------------------------------------------------
+ plaid_sync_id | bigint                   |           | not null | nextval('plaid_syncs_plaid_sync_id_seq'::regclass)
+ plaid_link_id | bigint                   |           | not null |
+ timestamp     | timestamp with time zone |           | not null |
+ trigger       | text                     |           | not null |
+ cursor        | text                     |           | not null |
+ added         | integer                  |           | not null |
+ modified      | integer                  |           | not null |
+ removed       | integer                  |           | not null |
+ account_id    | bigint                   |           | not null |
+Indexes:
+    "plaid_syncs_pkey" PRIMARY KEY, btree (plaid_sync_id, account_id)
+    "ix_plaid_syncs_timestamp" btree (plaid_link_id, "timestamp" DESC)
+Foreign-key constraints:
+    "fk_plaid_syncs_account" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_plaid_syncs_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+
+## Plaid Links
+
+                                                  Table "public.plaid_links"
+         Column         |           Type           | Collation | Nullable |                      Default
+------------------------+--------------------------+-----------+----------+----------------------------------------------------
+ plaid_link_id          | bigint                   |           | not null | nextval('plaid_links_plaid_link_id_seq'::regclass)
+ item_id                | text                     |           | not null |
+ products               | text[]                   |           |          |
+ webhook_url            | text                     |           |          |
+ institution_id         | text                     |           | not null |
+ institution_name       | text                     |           | not null |
+ account_id             | bigint                   |           | not null |
+ status                 | integer                  |           | not null | 0
+ error_code             | text                     |           |          |
+ expiration_date        | timestamp with time zone |           |          |
+ new_accounts_available | boolean                  |           | not null | false
+ last_manual_sync       | timestamp with time zone |           |          |
+ last_successful_update | timestamp with time zone |           |          |
+ last_attempted_update  | timestamp with time zone |           |          |
+ updated_at             | timestamp with time zone |           | not null | now()
+ created_at             | timestamp with time zone |           | not null | now()
+ created_by_user_id     | bigint                   |           | not null |
+Indexes:
+    "plaid_links_pkey" PRIMARY KEY, btree (plaid_link_id, account_id)
+    "uq_plaid_links_item_id" UNIQUE CONSTRAINT, btree (item_id)
+Foreign-key constraints:
+    "fk_plaid_links_account" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+Referenced by:
+    TABLE "links" CONSTRAINT "fk_links_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+    TABLE "plaid_bank_accounts" CONSTRAINT "fk_plaid_bank_accounts_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+    TABLE "plaid_syncs" CONSTRAINT "fk_plaid_syncs_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+
+## Plaid Bank Accounts
+
+                                                      Table "public.plaid_bank_accounts"
+        Column         |           Type           | Collation | Nullable |                              Default
+-----------------------+--------------------------+-----------+----------+--------------------------------------------------------------------
+ plaid_bank_account_id | bigint                   |           | not null | nextval('plaid_bank_accounts_plaid_bank_account_id_seq'::regclass)
+ account_id            | bigint                   |           | not null |
+ plaid_link_id         | bigint                   |           | not null |
+ plaid_id              | text                     |           | not null |
+ name                  | text                     |           | not null |
+ official_name         | text                     |           |          |
+ mask                  | text                     |           |          |
+ available_balance     | bigint                   |           | not null |
+ current_balance       | bigint                   |           | not null |
+ limit_balance         | bigint                   |           |          |
+ created_at            | timestamp with time zone |           | not null | now()
+ created_by_user_id    | bigint                   |           | not null |
+Indexes:
+    "pk_plaid_bank_accounts" PRIMARY KEY, btree (plaid_bank_account_id, account_id)
+Foreign-key constraints:
+    "fk_plaid_bank_accounts_account" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_plaid_bank_accounts_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+    "fk_plaid_bank_accounts_users_created_by_user_id" FOREIGN KEY (created_by_user_id) REFERENCES users(user_id)
+Referenced by:
+    TABLE "bank_accounts" CONSTRAINT "fk_bank_accounts_plaid_bank_accounts" FOREIGN KEY (plaid_bank_account_id, account_id) REFERENCES plaid_bank_accounts(plaid_bank_account_id, account_id)
+    TABLE "plaid_transactions" CONSTRAINT "fk_plaid_transactions_plaid_bank_account" FOREIGN KEY (plaid_bank_account_id, account_id) REFERENCES plaid_bank_accounts(plaid_bank_account_id, account_id)
+
+## Plaid Transactions
+
+                                                     Table "public.plaid_transactions"
+        Column         |           Type           | Collation | Nullable |                             Default
+-----------------------+--------------------------+-----------+----------+------------------------------------------------------------------
+ plaid_transaction_id  | bigint                   |           | not null | nextval('plaid_transactions_plaid_transaction_id_seq'::regclass)
+ account_id            | bigint                   |           | not null |
+ plaid_bank_account_id | bigint                   |           | not null |
+ plaid_id              | text                     |           | not null |
+ pending_plaid_id      | text                     |           |          |
+ categories            | text[]                   |           |          |
+ date                  | timestamp with time zone |           | not null |
+ authorized_date       | timestamp with time zone |           |          |
+ name                  | text                     |           | not null |
+ merchant_name         | text                     |           |          |
+ amount                | bigint                   |           | not null |
+ currency              | text                     |           | not null |
+ is_pending            | boolean                  |           | not null |
+ created_at            | timestamp with time zone |           | not null | now()
+ deleted_at            | timestamp with time zone |           |          |
+Indexes:
+    "pk_plaid_transactions" PRIMARY KEY, btree (plaid_transaction_id, account_id)
+Foreign-key constraints:
+    "fk_plaid_transactions_account" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_plaid_transactions_plaid_bank_account" FOREIGN KEY (plaid_bank_account_id, account_id) REFERENCES plaid_bank_accounts(plaid_bank_account_id, account_id)
+Referenced by:
+    TABLE "transactions" CONSTRAINT "fk_transactions_plaid_transactions" FOREIGN KEY (plaid_transaction_id, account_id) REFERENCES plaid_transactions(plaid_transaction_id, account_id)
+    TABLE "transactions" CONSTRAINT "fk_transactions_plaid_transactions_pending" FOREIGN KEY (pending_plaid_transaction_id, account_id) REFERENCES plaid_transactions(plaid_transaction_id, account_id)
+
+# Model Tables
+
+Several model tables have also been changed as part of this migration. Their new schema is listed below:
+
+## Links
+
+                                             Table "public.links"
+       Column       |           Type           | Collation | Nullable |                Default
+--------------------+--------------------------+-----------+----------+----------------------------------------
+ link_id            | bigint                   |           | not null | nextval('links_link_id_seq'::regclass)
+ account_id         | bigint                   |           | not null |
+ link_type          | smallint                 |           | not null |
+ plaid_link_id      | bigint                   |           |          |
+ institution_name   | text                     |           |          |
+ created_at         | timestamp with time zone |           | not null |
+ created_by_user_id | bigint                   |           | not null |
+ updated_at         | timestamp with time zone |           | not null |
+ description        | text                     |           |          |
+Indexes:
+    "pk_links" PRIMARY KEY, btree (link_id, account_id)
+Foreign-key constraints:
+    "fk_links_accounts_account_id" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_links_plaid_link" FOREIGN KEY (plaid_link_id, account_id) REFERENCES plaid_links(plaid_link_id, account_id)
+    "fk_links_users_created_by_user_id" FOREIGN KEY (created_by_user_id) REFERENCES users(user_id)
+Referenced by:
+    TABLE "bank_accounts" CONSTRAINT "fk_bank_accounts_links_link_id_account_id" FOREIGN KEY (link_id, account_id) REFERENCES links(link_id, account_id)
+
+## Bank Accounts
+
+                                                   Table "public.bank_accounts"
+        Column         |           Type           | Collation | Nullable |                        Default
+-----------------------+--------------------------+-----------+----------+--------------------------------------------------------
+ bank_account_id       | bigint                   |           | not null | nextval('bank_accounts_bank_account_id_seq'::regclass)
+ account_id            | bigint                   |           | not null |
+ link_id               | bigint                   |           | not null |
+ available_balance     | bigint                   |           | not null |
+ current_balance       | bigint                   |           | not null |
+ mask                  | text                     |           |          |
+ name                  | text                     |           | not null |
+ account_type          | text                     |           |          |
+ account_sub_type      | text                     |           |          |
+ last_updated          | timestamp with time zone |           | not null | (now() AT TIME ZONE 'UTC'::text)
+ status                | text                     |           | not null | 'active'::text
+ original_name         | text                     |           |          |
+ plaid_bank_account_id | bigint                   |           |          |
+ updated_at            | timestamp with time zone |           | not null | now()
+ created_at            | timestamp with time zone |           | not null | now()
+ created_by_user_id    | bigint                   |           | not null |
+Indexes:
+    "pk_bank_accounts" PRIMARY KEY, btree (bank_account_id, account_id)
+Foreign-key constraints:
+    "fk_bank_accounts_accounts_account_id" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_bank_accounts_links_link_id_account_id" FOREIGN KEY (link_id, account_id) REFERENCES links(link_id, account_id)
+    "fk_bank_accounts_plaid_bank_accounts" FOREIGN KEY (plaid_bank_account_id, account_id) REFERENCES plaid_bank_accounts(plaid_bank_account_id, account_id)
+Referenced by:
+    TABLE "files" CONSTRAINT "fk_files_bank_account" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+    TABLE "funding_schedules" CONSTRAINT "fk_funding_schedules_bank_accounts_bank_account_id_account_id" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+    TABLE "spending" CONSTRAINT "fk_spending_bank_accounts_bank_account_id_account_id" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+    TABLE "transaction_clusters" CONSTRAINT "fk_transaction_clusters_bank_account" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+    TABLE "transactions" CONSTRAINT "fk_transactions_bank_accounts_bank_account_id_account_id" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+
+## Transactions
+                                                      Table "public.transactions"
+            Column            |           Type           | Collation | Nullable |                       Default
+------------------------------+--------------------------+-----------+----------+------------------------------------------------------
+ transaction_id               | bigint                   |           | not null | nextval('transactions_transaction_id_seq'::regclass)
+ account_id                   | bigint                   |           | not null |
+ bank_account_id              | bigint                   |           | not null |
+ amount                       | bigint                   |           | not null |
+ spending_id                  | bigint                   |           |          |
+ spending_amount              | bigint                   |           |          |
+ categories                   | text[]                   |           |          |
+ name                         | text                     |           |          |
+ original_name                | text                     |           | not null |
+ merchant_name                | text                     |           |          |
+ original_merchant_name       | text                     |           |          |
+ is_pending                   | boolean                  |           | not null |
+ created_at                   | timestamp with time zone |           | not null | now()
+ date                         | timestamp with time zone |           | not null |
+ deleted_at                   | timestamp with time zone |           |          |
+ currency                     | text                     |           | not null | 'USD'::text
+ plaid_transaction_id         | bigint                   |           |          |
+ pending_plaid_transaction_id | bigint                   |           |          |
+Indexes:
+    "pk_transactions" PRIMARY KEY, btree (transaction_id, account_id, bank_account_id)
+    "ix_transactions_opt_order" btree (account_id, bank_account_id, date DESC, transaction_id DESC)
+    "ix_transactions_soft_delete" btree (account_id, bank_account_id, date DESC, transaction_id DESC) WHERE deleted_at IS NULL
+Foreign-key constraints:
+    "fk_transactions_accounts_account_id" FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+    "fk_transactions_bank_accounts_bank_account_id_account_id" FOREIGN KEY (bank_account_id, account_id) REFERENCES bank_accounts(bank_account_id, account_id)
+    "fk_transactions_plaid_transactions" FOREIGN KEY (plaid_transaction_id, account_id) REFERENCES plaid_transactions(plaid_transaction_id, account_id)
+    "fk_transactions_plaid_transactions_pending" FOREIGN KEY (pending_plaid_transaction_id, account_id) REFERENCES plaid_transactions(plaid_transaction_id, account_id)
+    "fk_transactions_spending" FOREIGN KEY (spending_id, account_id, bank_account_id) REFERENCES spending(spending_id, account_id, bank_account_id) ON DELETE SET NULL
+
+*/
+
+
+
 ALTER TABLE "plaid_syncs"
 ADD COLUMN "account_id" BIGINT;
 
