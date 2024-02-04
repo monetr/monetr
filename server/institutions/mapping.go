9a489750410161dec58b35731bf5eef689b0300f
Elliot Courant <me@elliotcourant.dev>
2024-02-04 13:59:17 -0600

    feat(api): Working on building an institution mapping tool

--- server/institutions/mapping.go
+++ server/institutions/mapping.go
@@ -4,7 +4,8 @@ type Mapping struct {
 	Id                  string  `json:"id"`
 	Name                string  `json:"name"`
 	Website             *string `json:"website"`
-	Logo                *string `json:"logo"`
 	PlaidInstitutionId  *string `json:"plaidInstitutionId"`
 	TellerInstitutionId *string `json:"tellerInstitutionId"`
+	PrimaryColor        *string `json:"primaryColor"`
+	MappedBy            string  `json:"mappedBy"`
 }
