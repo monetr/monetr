63c775ac47a34f6f7abfe0339451580b31d0b325
Elliot Courant <me@elliotcourant.dev>
2024-02-14 16:24:17 -0600

    chore: Fix migration

--- server/migrations/schema/2024021400_FixTransactions.tx.up.sql
+++ server/migrations/schema/2024021400_FixTransactions.tx.up.sql
@@ -8,7 +8,7 @@ DELETE FROM "transactions" WHERE "transaction_id" IN (
   INNER JOIN "links" ON "links"."link_id" = "bank_accounts"."link_id" AND 
              "links"."account_id" = "bank_accounts"."account_id" 
   WHERE "links"."teller_link_id" IS NOT NULL AND 
-        "transactions"."teller_transaction_id" IS NULL;
+        "transactions"."teller_transaction_id" IS NULL
 );
 
 DELETE FROM "teller_syncs" WHERE "timestamp" > '2024-02-06' AND "trigger" != 'initial';
