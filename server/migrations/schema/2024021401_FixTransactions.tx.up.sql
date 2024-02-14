b94ddd3cca0ec6e120a6617e975411c4bfed7dfd
Elliot Courant <me@elliotcourant.dev>
2024-02-14 16:47:33 -0600

    fix(api): Fixed typo in schema migration

--- server/migrations/schema/2024021401_FixTransactions.tx.up.sql
+++ server/migrations/schema/2024021401_FixTransactions.tx.up.sql
@@ -3,7 +3,7 @@ DELETE FROM "teller_transactions" WHERE "teller_transaction_id" IN (
   SELECT 
     "teller_transactions"."teller_transaction_id" 
   FROM "teller_transactions"
-  LEFT JOIN "transactions" ON "transactions"."teller_transaction_id" = "teller_transactions"."teller_transaction_id"
+  LEFT JOIN "transactions" ON "transactions"."teller_transaction_id" = "teller_transactions"."teller_transaction_id" AND
                               "transactions"."account_id" = "teller_transactions"."account_id"
   WHERE "transactions"."transaction_id" IS NULL
 );
