1b1d4fd8271144348a67ee7b031d1099b6d4aeab
Elliot Courant <me@elliotcourant.dev>
2024-02-14 16:42:43 -0600

    chore: Remove stray transactions

---
+++ server/migrations/schema/2024021401_FixTransactions.tx.up.sql
@@ -0,0 +1,10 @@
+-- Remove stray teller transactions.
+DELETE FROM "teller_transactions" WHERE "teller_transaction_id" IN (
+  SELECT 
+    "teller_transactions"."teller_transaction_id" 
+  FROM "teller_transactions"
+  LEFT JOIN "transactions" ON "transactions"."teller_transaction_id" = "teller_transactions"."teller_transaction_id"
+                              "transactions"."account_id" = "teller_transactions"."account_id"
+  WHERE "transactions"."transaction_id" IS NULL
+);
+
