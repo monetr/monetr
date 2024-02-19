427798d82a22529c181f3a9408fddab02ccf1a12
Elliot Courant <me@elliotcourant.dev>
2024-02-19 16:24:05 -0600

    fix(api): Fixing reset teller migration again.

--- server/migrations/schema/2024021900_ResetTeller.tx.up.sql
+++ server/migrations/schema/2024021900_ResetTeller.tx.up.sql
@@ -5,6 +5,16 @@ DELETE FROM "transaction_clusters"
        WHERE "bank_accounts"."account_id" = "transaction_clusters"."account_id" AND
              "bank_accounts"."bank_account_id" = "transaction_clusters"."bank_account_id" AND
              "bank_accounts"."teller_bank_account_id" IS NOT NULL;
+DELETE FROM "spending"
+       USING "bank_accounts"
+       WHERE "bank_accounts"."account_id" = "spending"."account_id" AND
+             "bank_accounts"."bank_account_id" = "spending"."bank_account_id" AND
+             "bank_accounts"."teller_bank_account_id" IS NOT NULL;
+DELETE FROM "funding_schedules"
+       USING "bank_accounts"
+       WHERE "bank_accounts"."account_id" = "funding_schedules"."account_id" AND
+             "bank_accounts"."bank_account_id" = "funding_schedules"."bank_account_id" AND
+             "bank_accounts"."teller_bank_account_id" IS NOT NULL;
 DELETE FROM "bank_accounts" WHERE "teller_bank_account_id" IS NOT NULL;
 DELETE FROM "teller_syncs";
 DELETE FROM "teller_transactions";
