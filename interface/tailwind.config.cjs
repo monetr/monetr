f797185173ea7f11199ff8f3dbdef3531baa4027
Elliot Courant <me@elliotcourant.dev>
2024-03-25 18:33:19 -0500

    chore(ui): Working on UI stuff.
    Trying to get shadcn stuff working.

--- interface/tailwind.config.cjs
+++ interface/tailwind.config.cjs
@@ -12,9 +12,65 @@ module.exports = {
     './src/**/*.jsx',
     './src/**/*.js',
   ],
+  plugins: [require('tailwindcss-animate')],
   theme: {
     extend: {
+      // borderRadius: {
+      //   lg: 'var(--radius)',
+      //   md: 'calc(var(--radius) - 2px)',
+      //   sm: 'calc(var(--radius) - 4px)',
+      // },
+      // fontFamily: {
+      //   sans: ['var(--font-sans)', ...fontFamily.sans],
+      // },
+      // keyframes: {
+      //   'accordion-down': {
+      //     from: { height: '0' },
+      //     to: { height: 'var(--radix-accordion-content-height)' },
+      //   },
+      //   'accordion-up': {
+      //     from: { height: 'var(--radix-accordion-content-height)' },
+      //     to: { height: '0' },
+      //   },
+      // },
+      // animation: {
+      //   'accordion-down': 'accordion-down 0.2s ease-out',
+      //   'accordion-up': 'accordion-up 0.2s ease-out',
+      // },
       colors: {
+        // border: 'hsl(var(--border))',
+        // input: 'hsl(var(--input))',
+        // ring: 'hsl(var(--ring))',
+        // background: 'hsl(var(--background))',
+        // foreground: 'hsl(var(--foreground))',
+        // primary: {
+        //   DEFAULT: 'hsl(var(--primary))',
+        //   foreground: 'hsl(var(--primary-foreground))',
+        // },
+        // secondary: {
+        //   DEFAULT: 'hsl(var(--secondary))',
+        //   foreground: 'hsl(var(--secondary-foreground))',
+        // },
+        // destructive: {
+        //   DEFAULT: 'hsl(var(--destructive))',
+        //   foreground: 'hsl(var(--destructive-foreground))',
+        // },
+        // muted: {
+        //   DEFAULT: 'hsl(var(--muted))',
+        //   foreground: 'hsl(var(--muted-foreground))',
+        // },
+        // accent: {
+        //   DEFAULT: 'hsl(var(--accent))',
+        //   foreground: 'hsl(var(--accent-foreground))',
+        // },
+        // popover: {
+        //   DEFAULT: 'hsl(var(--popover))',
+        //   foreground: 'hsl(var(--popover-foreground))',
+        // },
+        // card: {
+        //   DEFAULT: 'hsl(var(--card))',
+        //   foreground: 'hsl(var(--card-foreground))',
+        // },
         'monetr': {
           brand: {
             DEFAULT: '#4E1AA0',
