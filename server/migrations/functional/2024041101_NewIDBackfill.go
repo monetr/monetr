e029fe9347a8559a97081748b9fc4f90f37e739a
Elliot Courant <me@elliotcourant.dev>
2024-04-18 18:07:27 -0500

    chore: Progress

--- server/migrations/functional/2024041101_NewIDBackfill.go
+++ server/migrations/functional/2024041101_NewIDBackfill.go
@@ -146,6 +146,24 @@ func init() {
 				}
 			}
 
+			{ // Transaction clusters
+				var rows []struct {
+					tableName string `pg:"transaction_clusters"`
+
+					IdOld uint64        `pg:"transaction_cluster_id,pk"`
+					IdNew identifier.ID `pg:"transaction_cluster_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.TransactionClusterKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
 			{ // Spending
 				var rows []struct {
 					tableName string `pg:"spending"`
@@ -182,6 +200,42 @@ func init() {
 				}
 			}
 
+			{ // Files
+				var rows []struct {
+					tableName string `pg:"files"`
+
+					IdOld uint64        `pg:"file_id,pk"`
+					IdNew identifier.ID `pg:"file_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.FileKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Jobs
+				var rows []struct {
+					tableName string `pg:"jobs"`
+
+					IdOld uint64        `pg:"job_id,pk"`
+					IdNew identifier.ID `pg:"job_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.JobKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
 			{ // Plaid Links
 				var rows []struct {
 					tableName string `pg:"plaid_links"`
@@ -194,6 +248,132 @@ func init() {
 				}
 				for i := range rows {
 					rows[i].IdNew = identifier.New(identifier.PlaidLinkKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Plaid Syncs
+				var rows []struct {
+					tableName string `pg:"plaid_syncs"`
+
+					IdOld uint64        `pg:"plaid_sync_id,pk"`
+					IdNew identifier.ID `pg:"plaid_sync_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.PlaidSyncKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Plaid Bank Accounts
+				var rows []struct {
+					tableName string `pg:"plaid_bank_accounts"`
+
+					IdOld uint64        `pg:"plaid_bank_account_id,pk"`
+					IdNew identifier.ID `pg:"plaid_bank_account_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.PlaidBankAccountKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Plaid Transactions
+				var rows []struct {
+					tableName string `pg:"plaid_transactions"`
+
+					IdOld uint64        `pg:"plaid_transaction_id,pk"`
+					IdNew identifier.ID `pg:"plaid_transaction_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.PlaidTransactionKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Teller Links
+				var rows []struct {
+					tableName string `pg:"teller_links"`
+
+					IdOld uint64        `pg:"teller_link_id,pk"`
+					IdNew identifier.ID `pg:"teller_link_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.TellerLinkKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Teller Bank Accounts
+				var rows []struct {
+					tableName string `pg:"teller_bank_accounts"`
+
+					IdOld uint64        `pg:"teller_bank_account_id,pk"`
+					IdNew identifier.ID `pg:"teller_bank_account_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.TellerBankAccountKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Teller Syncs
+				var rows []struct {
+					tableName string `pg:"teller_syncs"`
+
+					IdOld uint64        `pg:"teller_sync_id,pk"`
+					IdNew identifier.ID `pg:"teller_sync_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.TellerSyncKind)
+				}
+				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+			}
+
+			{ // Teller Tranasactions
+				var rows []struct {
+					tableName string `pg:"teller_transacations"`
+
+					IdOld uint64        `pg:"teller_transacation_id,pk"`
+					IdNew identifier.ID `pg:"teller_transacation_id_new"`
+				}
+				if err := db.Model(&rows).Select(&rows); err != nil {
+					return errors.Wrap(err, fmt.Sprintf("failed to query %s", db.Model(&rows).TableModel().Table().SQLName))
+				}
+				for i := range rows {
+					rows[i].IdNew = identifier.New(identifier.TellerTransactionKind)
 				}
 				if _, err := db.Model(&rows).WherePK().Update(&rows); err != nil {
 					return errors.Wrap(err, fmt.Sprintf("failed to update %s", db.Model(&rows).TableModel().Table().SQLName))
--- server/migrations/schema/2024041100_NewID.tx.up.sql
+++ server/migrations/schema/2024041100_NewID.tx.up.sql
@@ -36,6 +36,11 @@ ADD COLUMN "pending_plaid_transaction_id_new" VARCHAR(64),
 ADD COLUMN "teller_transaction_id_new" VARCHAR(64),
 ADD COLUMN "spending_id_new" VARCHAR(64);
 
+ALTER TABLE "transaction_clusters"
+ADD COLUMN "transaction_cluster_id_new" VARCHAR(64),
+ADD COLUMN "account_id_new" VARCHAR(64),
+ADD COLUMN "bank_account_id_new" VARCHAR(64);
+
 ALTER TABLE "spending"
 ADD COLUMN "spending_id_new" VARCHAR(64),
 ADD COLUMN "account_id_new" VARCHAR(64),
@@ -46,6 +51,14 @@ ALTER TABLE "funding_schedules"
 ADD COLUMN "funding_schedule_id_new" VARCHAR(64),
 ADD COLUMN "account_id_new" VARCHAR(64),
 ADD COLUMN "bank_account_id_new" VARCHAR(64);
+
+ALTER TABLE "files"
+ADD COLUMN "file_id_new" VARCHAR(64),
+ADD COLUMN "account_id_new" VARCHAR(64),
+ADD COLUMN "bank_account_id_new" VARCHAR(64);
+
+ALTER TABLE "jobs"
+ADD COLUMN "job_id_new" VARCHAR(64);
 
 ALTER TABLE "plaid_links"
 ADD COLUMN "plaid_link_id_new" VARCHAR(64),
@@ -89,3 +102,4 @@ ALTER TABLE "teller_transactions"
 ADD COLUMN "teller_transaction_id_new" VARCHAR(64),
 ADD COLUMN "account_id_new" VARCHAR(64),
 ADD COLUMN "teller_bank_account_id_new" VARCHAR(64);
+
