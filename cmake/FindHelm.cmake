2f404d2eb38b88dd0cc802bddbb495e660af38bb
Elliot Courant <me@elliotcourant.dev>
2024-05-19 16:06:09 -0500

    chore: Pin helm version, dont use a container

--- cmake/FindHelm.cmake
+++ cmake/FindHelm.cmake
@@ -1,7 +1,7 @@
 include(GolangUtils)
 
 set(HELM_EXECUTABLE ${GO_BIN_DIR}/helm CACHE INTERNAL "Path to the local helm tooling")
-SET(HELM_VERSION latest)
+SET(HELM_VERSION "v3.15.0")
 go_install(
   OUTPUT ${HELM_EXECUTABLE}
   PACKAGE "helm.sh/helm/v3/cmd/helm"
