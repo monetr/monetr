394ceda879e8e2275854ee3148490affd20dab71
Elliot Courant <me@elliotcourant.dev>
2024-06-30 09:14:44 -0500

    fix(ui): Don't show manual on setup page if its not enabled

--- interface/src/pages/link/create.tsx
+++ interface/src/pages/link/create.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { PowerOutlined } from '@mui/icons-material';
 
 import MTopNavigation from '@monetr/interface/components/MTopNavigation';
+import { useAppConfigurationSink } from '@monetr/interface/hooks/useAppConfiguration';
 import SetupPage from '@monetr/interface/pages/setup';
 
 export default function LinkCreatePage(): JSX.Element {
+  const { result: config } = useAppConfigurationSink();
+
   return (
     <div className='flex flex-col w-full'>
       <MTopNavigation
         icon={ PowerOutlined }
         title='Add another connection'
       />
-      <SetupPage alreadyOnboarded manualEnabled />
+      <SetupPage alreadyOnboarded manualEnabled={ config?.manualEnabled } />
     </div>
   );
 }
