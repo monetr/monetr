2faf1d0faa403dbf7c1e58e1e44aac4835dad6ef
Elliot Courant <me@elliotcourant.dev>
2024-08-04 14:26:48 -0500

    chore: Messy progress

--- server/backup/backup.go
+++ server/backup/backup.go
@@ -23,22 +23,22 @@ type BackupManager struct {
 	destination BackupDestination
 }
 
-// BackupDestination represents an implementation of the destination of a monetr
-// backup. This could be a local filesystem or an object store. But it must
-// consume from a reader that is passed to it. The backup destination
-// implementation cannot know or rely on knowing how large the backup will be.
-// But must be able to read from the stream of data to it in chunks.
-type BackupDestination interface {
-	// Start begins the backup writer, it consumes a reader and should read from
-	// that reader in a chunk size that is up to the implementation. As it reads
-	// chunks from the reader it should flush that data to whatever storage system
-	// this destination represents.
-	Start(ctx context.Context, reader io.Reader) error
-	// Close cleans up any temporary files from the backup destination
-	// implementation and finalizes anything needed for the backup. If the backup
-	// is incomplete or was canceled then this should not remove any files written
-	// (other than temp files).
-	Close() error
+func NewBackupManager(
+	log *logrus.Entry,
+	configuration config.Configuration,
+	db pg.DBI,
+	kms secrets.KeyManagement,
+	objects storage.Storage,
+	destination BackupDestination,
+) *BackupManager {
+	return &BackupManager{
+		log:         log,
+		conf:        configuration,
+		db:          db,
+		kms:         kms,
+		objects:     objects,
+		destination: destination,
+	}
 }
 
 func (b *BackupManager) Backup(ctx context.Context) error {
@@ -64,7 +64,7 @@ func (b *BackupManager) Backup(ctx context.Context) error {
 		return errors.New("PostgreSQL did not create a snapshot for backup")
 	}
 
-	postgresBackup := newPostgresBackupSource(b.log, b.conf)
+	postgresBackup := newPostgresBackupSource(b.log, b.conf, txn)
 
 	reader, writer := io.Pipe()
 	gzipWriter := gzip.NewWriter(writer)
