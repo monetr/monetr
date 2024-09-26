d4d978e0ead6c40199aea14c94b8bfd5cf45942e
Elliot Courant <me@elliotcourant.dev>
2024-09-25 20:23:46 -0500

    feat(ui): Adding an error boundary
    This might need to be tweaked as time goes on, but its better than what
    monetr currently does which is just die to a purple screen.
    
    Resolves #1531

--- interface/src/Root.tsx
+++ interface/src/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NiceModal from '@ebay/nice-modal-react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import * as Sentry from '@sentry/react';
 
 import MQueryClient from '@monetr/interface/components/MQueryClient';
 import MSnackbarProvider from '@monetr/interface/components/MSnackbarProvider';
@@ -10,20 +11,21 @@ import Monetr from '@monetr/interface/monetr';
 import { newTheme } from '@monetr/interface/theme';
 
 export default function Root(): JSX.Element {
-  // <Sentry.ErrorBoundary>
   return (
     <BrowserRouter>
-      <MQueryClient>
-        <ThemeProvider theme={ newTheme }>
-          <MSnackbarProvider>
-            <NiceModal.Provider>
-              <CssBaseline />
-              <PullToRefresh />
-              <Monetr />
-            </NiceModal.Provider>
-          </MSnackbarProvider>
-        </ThemeProvider>
-      </MQueryClient>
+      <Sentry.ErrorBoundary>
+        <MQueryClient>
+          <ThemeProvider theme={ newTheme }>
+            <MSnackbarProvider>
+              <NiceModal.Provider>
+                <CssBaseline />
+                <PullToRefresh />
+                <Monetr />
+              </NiceModal.Provider>
+            </MSnackbarProvider>
+          </ThemeProvider>
+        </MQueryClient>
+      </Sentry.ErrorBoundary>
     </BrowserRouter>
   );
 }
