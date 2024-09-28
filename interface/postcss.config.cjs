4dc59282a36b1fb3af9e914cac5d7bfd3e77cb15
Elliot Courant <me@elliotcourant.dev>
2024-09-28 11:57:48 -0500

    feat(ui): Minify CSS files

--- interface/postcss.config.cjs
+++ interface/postcss.config.cjs
@@ -1,7 +1,15 @@
+/** @type {import('postcss').Postcss} */
 module.exports = {
   plugins: [
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require('cssnano')({
+      preset: ['default', {
+        discardComments: {
+          removeAll: true,
+        },
+      }],
+    }),
   ],
 };
