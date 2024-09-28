5805b1a6727afa0558fef510eb3ba1d4d4ad4e81
Elliot Courant <me@elliotcourant.dev>
2024-09-28 13:36:59 -0500

    fix(docs): Fixing docs build

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,15 +1,18 @@
 /** @type {import('postcss').Postcss} */
 module.exports = {
   plugins: [
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: ['default', {
-        discardComments: {
-          removeAll: true,
-        },
-      }],
-    }),
+    'tailwindcss/nesting',
+    'tailwindcss',
+    'autoprefixer',
+    [
+      'cssnano',
+      {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true,
+          },
+        }],
+      },
+    ],
   ],
 };
