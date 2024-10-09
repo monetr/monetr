3cfbcd8a22631b40aef2036e5d4d274751b9caad
Elliot Courant <me@elliotcourant.dev>
2024-10-08 19:23:51 -0500

    chore: Migration to fix existing spending bois

---
+++ server/migrations/schema/202100800_SpendingDST.tx.up.sql
@@ -0,0 +1,4 @@
+-- Any spending object that happens after the daylight savings time transition,
+-- reset it to the previous recurrence. The process spending job will then
+-- correct this by recalculating the next recurrence within 30 minutes.
+UPDATE "spending" SET "next_recurrence" = "last_recurrence" WHERE "next_recurrence" > '2024-11-02 00:00:00.000000+00'::TIMESTAMP;
