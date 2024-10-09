7482b15cfbd0bbe5781663dd41b7736a1c02a55a
Elliot Courant <me@elliotcourant.dev>
2024-10-09 18:42:20 -0500

    chore: TWeaks

--- server/migrations/schema/2024100800_SpendingDST.tx.up.sql
+++ server/migrations/schema/2024100800_SpendingDST.tx.up.sql
@@ -3,4 +3,6 @@
 -- correct this by recalculating the next recurrence within 30 minutes.
 UPDATE "spending"
 SET "next_recurrence" = now()
-WHERE "spending_type" = 0 AND "next_recurrence" > '2024-11-02 00:00:00.000000+00'::TIMESTAMP AND "next_recurrence" < '2025-03-10 00:00:00.000000+00'::TIMESTAMP;
+WHERE "spending_type" = 0 AND 
+      "next_recurrence" > '2024-11-02 00:00:00.000000+00'::TIMESTAMP AND 
+      "next_recurrence" < '2025-03-10 00:00:00.000000+00'::TIMESTAMP;
