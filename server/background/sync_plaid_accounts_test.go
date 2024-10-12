5126369ddc08f747ff2e76bd633718a25fe13dc6
Elliot Courant <me@elliotcourant.dev>
2024-10-12 10:37:56 -0500

    chore: Adding more tests

--- server/background/sync_plaid_accounts_test.go
+++ server/background/sync_plaid_accounts_test.go
@@ -11,7 +11,10 @@ import (
 	"github.com/monetr/monetr/server/internal/myownsanity"
 	"github.com/monetr/monetr/server/internal/testutils"
 	"github.com/monetr/monetr/server/models"
+	"github.com/monetr/monetr/server/platypus"
+	"github.com/monetr/monetr/server/repository"
 	"github.com/monetr/monetr/server/secrets"
+	"github.com/plaid/plaid-go/v26/plaid"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -119,3 +122,190 @@ func TestSyncPlaidAccountsHandler_EnqueueTriggeredJob(t *testing.T) {
 	})
 }
 
+func TestSyncPlaidAccountsJob_Run(t *testing.T) {
+	t.Run("deactivate an account", func(t *testing.T) {
+		clock := clock.NewMock()
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		log := testutils.GetLog(t)
+		db := testutils.GetPgDatabase(t, testutils.IsolatedDatabase)
+		kms := secrets.NewPlaintextKMS()
+
+		user, _ := fixtures.GivenIHaveABasicAccount(t, clock)
+		plaidLink := fixtures.GivenIHaveAPlaidLink(t, clock, user)
+		checkingAccount := fixtures.GivenIHaveAPlaidBankAccount(
+			t,
+			clock,
+			&plaidLink,
+			models.DepositoryBankAccountType,
+			models.CheckingBankAccountSubType,
+		)
+
+		savingsAccount := fixtures.GivenIHaveAPlaidBankAccount(
+			t,
+			clock,
+			&plaidLink,
+			models.DepositoryBankAccountType,
+			models.SavingsBankAccountSubType,
+		)
+
+		plaidPlatypus := mockgen.NewMockPlatypus(ctrl)
+		plaidClient := mockgen.NewMockClient(ctrl)
+
+		plaidPlatypus.EXPECT().
+			NewClient(
+				gomock.Any(),
+				gomock.AssignableToTypeOf(new(models.Link)),
+				gomock.Any(),
+				gomock.Eq(plaidLink.PlaidLink.PlaidId),
+			).
+			Return(plaidClient, nil).
+			AnyTimes()
+
+		plaidClient.EXPECT().
+			GetAccounts(
+				gomock.Any(),
+				gomock.Len(0), // No account IDs should be passed
+			).
+			Return(
+				[]platypus.BankAccount{
+					// Since we only return the checking account, the savings account should
+					// get deactivated.
+					testutils.Must(t, platypus.NewPlaidBankAccount, plaid.AccountBase{
+						AccountId: checkingAccount.PlaidBankAccount.PlaidId,
+					}),
+				},
+				nil,
+			).
+			Times(1)
+
+		job, err := background.NewSyncPlaidAccountsJob(
+			log,
+			repository.NewRepositoryFromSession(clock, "user_system", user.AccountId, db),
+			clock,
+			repository.NewSecretsRepository(log, clock, db, kms, user.AccountId),
+			plaidPlatypus,
+			background.SyncPlaidAccountsArguments{
+				AccountId: user.AccountId,
+				LinkId:    plaidLink.LinkId,
+			},
+		)
+		assert.NoError(t, err)
+		assert.NotNil(t, job)
+
+		{ // Checking account and savings account should be active before the job
+			checkingAccountBefore := testutils.MustRetrieve(t, checkingAccount)
+			assert.Equal(t, checkingAccountBefore.Status, models.ActiveBankAccountStatus)
+			savingsAccountBefore := testutils.MustRetrieve(t, savingsAccount)
+			assert.Equal(t, savingsAccountBefore.Status, models.ActiveBankAccountStatus)
+		}
+
+		// Then we run the job
+		err = job.Run(context.Background())
+		assert.NoError(t, err)
+
+		{ // Now the checking is active but the savings is inactive
+			checkingAccountBefore := testutils.MustRetrieve(t, checkingAccount)
+			assert.Equal(t, checkingAccountBefore.Status, models.ActiveBankAccountStatus)
+			savingsAccountBefore := testutils.MustRetrieve(t, savingsAccount)
+			assert.Equal(t, savingsAccountBefore.Status, models.InactiveBankAccountStatus)
+		}
+	})
+
+	t.Run("should reactivate an account", func(t *testing.T) {
+		clock := clock.NewMock()
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		log := testutils.GetLog(t)
+		db := testutils.GetPgDatabase(t, testutils.IsolatedDatabase)
+		kms := secrets.NewPlaintextKMS()
+
+		user, _ := fixtures.GivenIHaveABasicAccount(t, clock)
+		plaidLink := fixtures.GivenIHaveAPlaidLink(t, clock, user)
+		checkingAccount := fixtures.GivenIHaveAPlaidBankAccount(
+			t,
+			clock,
+			&plaidLink,
+			models.DepositoryBankAccountType,
+			models.CheckingBankAccountSubType,
+		)
+
+		savingsAccount := fixtures.GivenIHaveAPlaidBankAccount(
+			t,
+			clock,
+			&plaidLink,
+			models.DepositoryBankAccountType,
+			models.SavingsBankAccountSubType,
+		)
+
+		{ // Update the savings account to have an inactive status.
+			savingsAccount.Status = models.InactiveBankAccountStatus
+			testutils.MustDBUpdate(t, &savingsAccount)
+		}
+
+		plaidPlatypus := mockgen.NewMockPlatypus(ctrl)
+		plaidClient := mockgen.NewMockClient(ctrl)
+
+		plaidPlatypus.EXPECT().
+			NewClient(
+				gomock.Any(),
+				gomock.AssignableToTypeOf(new(models.Link)),
+				gomock.Any(),
+				gomock.Eq(plaidLink.PlaidLink.PlaidId),
+			).
+			Return(plaidClient, nil).
+			AnyTimes()
+
+		plaidClient.EXPECT().
+			GetAccounts(
+				gomock.Any(),
+				gomock.Len(0), // No account IDs should be passed
+			).
+			Return(
+				[]platypus.BankAccount{
+					testutils.Must(t, platypus.NewPlaidBankAccount, plaid.AccountBase{
+						AccountId: checkingAccount.PlaidBankAccount.PlaidId,
+					}),
+					testutils.Must(t, platypus.NewPlaidBankAccount, plaid.AccountBase{
+						AccountId: savingsAccount.PlaidBankAccount.PlaidId,
+					}),
+				},
+				nil,
+			).
+			Times(1)
+
+		job, err := background.NewSyncPlaidAccountsJob(
+			log,
+			repository.NewRepositoryFromSession(clock, "user_system", user.AccountId, db),
+			clock,
+			repository.NewSecretsRepository(log, clock, db, kms, user.AccountId),
+			plaidPlatypus,
+			background.SyncPlaidAccountsArguments{
+				AccountId: user.AccountId,
+				LinkId:    plaidLink.LinkId,
+			},
+		)
+		assert.NoError(t, err)
+		assert.NotNil(t, job)
+
+		{ // Only checking should be active befre the job runs
+			checkingAccountBefore := testutils.MustRetrieve(t, checkingAccount)
+			assert.Equal(t, checkingAccountBefore.Status, models.ActiveBankAccountStatus)
+			savingsAccountBefore := testutils.MustRetrieve(t, savingsAccount)
+			assert.Equal(t, savingsAccountBefore.Status, models.InactiveBankAccountStatus)
+		}
+
+		// Then we run the job
+		err = job.Run(context.Background())
+		assert.NoError(t, err)
+
+		{ // Now both accounts should be active
+			checkingAccountBefore := testutils.MustRetrieve(t, checkingAccount)
+			assert.Equal(t, checkingAccountBefore.Status, models.ActiveBankAccountStatus)
+			savingsAccountBefore := testutils.MustRetrieve(t, savingsAccount)
+			assert.Equal(t, savingsAccountBefore.Status, models.ActiveBankAccountStatus)
+		}
+	})
+}
