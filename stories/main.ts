c456fd033102b3b59ec76fd51ea3cc8d13230b6b
Elliot Courant <me@elliotcourant.dev>
2024-10-26 17:14:59 -0500

    fix(docs): Get storybook builds working

--- stories/main.ts
+++ stories/main.ts
@@ -1,12 +1,4 @@
-import { resolve } from 'path';
 import type { StorybookConfig } from 'storybook-react-rsbuild';
-
-const envName = process.env.NODE_ENV;
-const isDevelopment = envName !== 'production';
-
-const root = resolve(__dirname, '../');
-const uiDir = resolve(root, 'interface/src');
-const emailDir = resolve(root, 'emails/src');
 
 const marketingStoryOnly = process.env.MARKETING_STORY_ONLY === 'true';
 let stories = [
@@ -31,71 +23,19 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-viewport',
     'storybook-addon-react-router-v6',
-    // {
-    //   name: '@storybook/addon-styling',
-    //   options: {
-    //     // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-    //     // For more details on this addon's options.
-    //     postCss: true,
-    //   },
-    // },
+    // TODO Add storycap back in later for screenshots of stories.
     // 'storycap',
   ],
   framework: 'storybook-react-rsbuild',
-  // webpackFinal: async config => {
-  //   config.mode = isDevelopment ? 'development' : 'production';
-  //   if (isDevelopment) {
-  //     //@ts-ignore
-  //     config.devServer = {
-  //       //@ts-ignore
-  //       ...config.devServer,
-  //       hot: true,
-  //     };
-  //
-  //     config.plugins = [
-  //       ...config.plugins,
-  //       new ReactRefreshWebpackPlugin(),
-  //     ];
-  //   }
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     '@monetr/interface': uiDir,
-  //   };
-  //   config.resolve.extensions = [
-  //     ...config.resolve.extensions,
-  //     '.svg',
-  //     '.scss',
-  //     '.css',
-  //   ];
-  //   config.resolve.modules = [
-  //     ...config.resolve.modules,
-  //     uiDir,
-  //     emailDir,
-  //     'node_modules',
-  //   ];
-  //   config.module.rules = [
-  //     ...config.module.rules,
-  //     {
-  //       test: /\.?scss$/,
-  //       use: [
-  //         'style-loader',
-  //         'css-loader',
-  //         'postcss-loader',
-  //         'sass-loader',
-  //       ],
-  //     },
-  //     {
-  //       test: /\.svg$/,
-  //       parser: {
-  //         dataUrlCondition: {
-  //           maxSize: 1 * 1024 * 1024, // 1MB
-  //         },
-  //       },
-  //     },
-  //   ];
-  //
-  //   return config;
-  // },
+  rsbuildFinal: config => {
+    if (marketingStoryOnly) {
+      // Marketing story only merges the storybook dist with the next.js dist from docs.
+      // as a result, the subpath for storybook needs to be /_storybook in order to be served properly.
+      config.output ??= { };
+      config.output.assetPrefix = '/_storybook/';
+    }
+    return config;
+  },
   docs: {
     autodocs: 'tag',
   },
