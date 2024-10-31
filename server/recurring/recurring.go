592d71a43f85af19ad545acc4062889cd48336bf
Elliot Courant <me@elliotcourant.dev>
2024-10-30 19:34:37 -0500

    chore: Progress

--- server/recurring/recurring.go
+++ server/recurring/recurring.go
@@ -21,11 +21,21 @@ var (
 
 type Frequency struct {
 	StartDate time.Time
-	Period    int
+	Frequency int
 	Rule      models.RuleSet
 }
 
+type FrequencyScore struct {
+	Frequency      int
+	EstimatedIndex float64
+	Index          float64
+	Conclusion     float64
+	Confidence     float64
+}
+
 type RecurringTransactionResult struct {
+	Best    *Frequency
+	Results []FrequencyScore
 }
 
 func DetectRecurringTransactions(
@@ -74,6 +84,7 @@ func DetectRecurringTransactions(
 		"count":   len(transactions),
 	})
 
+	// Build our time series of transaction items.
 	series := make([]complex128, size)
 	for i := range transactions {
 		txn := transactions[i]
@@ -91,7 +102,73 @@ func DetectRecurringTransactions(
 		series[index] += complex(1, 0)
 	}
 
-	// result := calc.FastFourierTransform(series)
+	// Frequencies represents the number of days between each transaction, we will
+	// evaluate the resulting frequency spectrum from the fourier transform for
+	// these specific frequencies. If a group of transactions clearly show a
+	// specific frequency then that will be the end result. Some frequencies are
+	// allowed to overlap or tie for "first". Like 14,15,16.
+	frequencies := []int{
+		7,
+		14,
+		15,
+		16,
+		30,
+		60,
+		90,
+	}
+
+	result := calc.FastFourierTransform(series)
+
+	scores := make([]FrequencyScore, len(frequencies))
+	for f := range frequencies {
+		frequency := frequencies[f]
+		// Period is the frequency adjusted to the scale of our current time series.
+		period := ((time.Duration(frequency) * 24 * time.Hour).Seconds()) / segment
+		// Estimated index is a floating point number which indicates where in the
+		// resulting frequency spectrum this frequency would be located.
+		estimatedIndex := (1 / period) * float64(size)
+		// Index is the actual index we will use for this frequency, this index
+		// might be the same for multiple frequencies depending on the number of
+		// transactions in the time series. We round so that we favor the higher or
+		// lower index depending on the decimal of the estimated index that we
+		// calculated above.
+		index := math.Round(estimatedIndex)
+		score := FrequencyScore{
+			Frequency:      f,
+			EstimatedIndex: estimatedIndex,
+			Index:          index,
+			Conclusion:     0,
+			Confidence:     0,
+		}
+
+		// We will only have valid magnitudes up to N + 1 where N is the number of
+		// transactions we provided to the time series. This way we eliminate a ton
+		// of extra data that might be misleading. Also if our index ends up being 0
+		// then this frequency is definitely not valid as 0 is always a useless
+		// frequency on the spectrum.
+		if index > float64(len(transactions))+1 || index == 0 {
+			scores[f] = score
+			continue
+		}
+
+		value := result[int(index)]
+		real := real(value)
+		imaginary := imag(value)
+		magnitude := math.Sqrt((real * real) + (imaginary * imaginary))
+		score.Conclusion = magnitude
+		// TODO Add some form of confidence calculation
+		scores[f] = score
+	}
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].Conclusion > scores[j].Conclusion
+	})
+
+	// TODO Determine if the top score is actually the best, or if it is tied with
+	// other scores. If its tied but its a compatible score (such as 14, 15 and
+	// 16) then use the top score. Otherwise return no recurrence detected.
+
+	// TODO Try to determine which transactions are part of the recurrence as well
+	// as what the start date for the actual recurrence is.
 
 	return nil, nil
 }
