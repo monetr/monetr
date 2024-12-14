e882c7970255a6b694c6dfc2a53e74150acb053c
Elliot Courant <me@elliotcourant.dev>
2024-12-14 13:30:28 -0600

    chore(ui): Cleaning up old unused files

--- interface/src/pages/accounts.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import AllAccountsView from '@monetr/interface/components/BankAccounts/AllAccountsView/AllAccountsView';
-
-export default function AccountsPage(): JSX.Element {
-  return <AllAccountsView />;
-}
