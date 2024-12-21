fe56bc0acc5a101ff4b3c48fd07bc11e1038bda3
Elliot Courant <me@elliotcourant.dev>
2024-12-21 17:03:58 -0600

    fix(docs): Fix frontmatter and head tags

--- docs/src/components/Head.tsx
+++ docs/src/components/Head.tsx
@@ -8,15 +8,20 @@ export default function Head(): JSX.Element {
   const { frontMatter } = useConfig();
   const { asPath } = useRouter();
 
+  const url = `https://monetr.app${asPath}`;
   let suffix = '';
-
   if (asPath !== '/') {
     suffix = '- monetr';
   }
 
   let title = 'monetr';
-  if (frontMatter?.title) {
+  if (frontMatter?.title?.length > 0) {
     title = `${frontMatter.title} ${suffix}`;
+  }
+
+  let description = 'Take control of your finances, paycheck by paycheck, with monetr. Put aside what you need, spend what you want, and confidently manage your money with ease. Always know you’ll have enough for your bills and what’s left to save or spend.';
+  if (frontMatter?.description?.length > 0) {
+    description = frontMatter.description;
   }
 
   return (
@@ -25,10 +30,11 @@ export default function Head(): JSX.Element {
 
       <title>{ title }</title>
       <meta property='og:title' content={ frontMatter.title || title } />
+      <meta property='og:url' content={ url } />
       <meta name='title' content={ frontMatter.title || title } />
-
-      <meta property='og:description' content={ frontMatter.description || 'Take control of your finances, paycheck by paycheck, with monetr. Put aside what you need, spend what you want, and confidently manage your money with ease. Always know you’ll have enough for your bills and what’s left to save or spend.' } />
-      <meta name='description' content={ frontMatter.description || 'monetr' } />
+      <meta httpEquiv='Content-Language' content='en' />
+      <meta property='og:description' content={ description } />
+      <meta name='description' content={ description } />
 
       { process.env.NODE_ENV != 'development' && 
         <script defer src='https://a.monetr.app/script.js' data-website-id='ccbdfaf9-683f-4487-b97f-5516e1353715' /> 
