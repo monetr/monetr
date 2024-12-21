7be07de2d188b1d072e1969263a9a5e9b0f4c618
Elliot Courant <me@elliotcourant.dev>
2024-12-21 17:07:48 -0600

    fix(docs): Fix google search indexing complaing about redirects

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://monetr.app',
+  trailingSlash: true,
   generateRobotsTxt: true, // (optional)
   exclude: ['*/_meta'],
   output: 'export',
