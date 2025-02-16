d6adccd7626e5e725afabb69a65b86d616de1cb5
Elliot Courant <me@elliotcourant.dev>
2025-02-16 09:51:45 -0600

    feat(api): Lift balances query out of SQL view.
    This query was previously stored in a view in the database, making it
    diffficult to maintain and update over time. The query has been lifted
    directly into a repository function now to make it easier to maintain
    going forward. It also has 2 tests proving _some_ behavior.

--- server/repository/balances.go
+++ server/repository/balances.go
@@ -10,8 +10,6 @@ import (
 )
 
 type Balances struct {
-	tableName string `pg:"balances"`
-
 	BankAccountId ID[BankAccount] `json:"bankAccountId" pg:"bank_account_id"`
 	AccountId     ID[Account]     `json:"-" pg:"account_id"`
 	Currency      string          `json:"currency" pg:"currency"`
@@ -28,9 +26,45 @@ func (r *repositoryBase) GetBalances(ctx context.Context, bankAccountId ID[BankA
 	defer span.Finish()
 
 	var balance Balances
-	err := r.txn.ModelContext(span.Context(), &balance).
-		Where(`"balances"."account_id" = ?`, r.AccountId()).
-		Where(`"balances"."bank_account_id" = ?`, bankAccountId).
+
+	expenseQuery := r.txn.ModelContext(span.Context(), &Spending{}).
+		ColumnExpr(`"spending"."bank_account_id"`).
+		ColumnExpr(`"spending"."account_id"`).
+		ColumnExpr(`SUM("spending"."current_amount") AS "current_amount"`).
+		Where(`"spending"."spending_type" = ?`, SpendingTypeExpense).
+		GroupExpr(`"spending"."bank_account_id"`).
+		GroupExpr(`"spending"."account_id"`)
+
+	goalQuery := r.txn.ModelContext(span.Context(), &Spending{}).
+		ColumnExpr(`"spending"."bank_account_id"`).
+		ColumnExpr(`"spending"."account_id"`).
+		ColumnExpr(`SUM("spending"."current_amount") AS "current_amount"`).
+		Where(`"spending"."spending_type" = ?`, SpendingTypeGoal).
+		GroupExpr(`"spending"."bank_account_id"`).
+		GroupExpr(`"spending"."account_id"`)
+
+	err := r.txn.ModelContext(span.Context(), &BankAccount{}).
+		With(`expense`, expenseQuery).
+		With(`goal`, goalQuery).
+		ColumnExpr(`"bank_account"."bank_account_id"`).
+		ColumnExpr(`"bank_account"."account_id"`).
+		ColumnExpr(`"bank_account"."currency"`).
+		ColumnExpr(`"bank_account"."current_balance" AS "current"`).
+		ColumnExpr(`"bank_account"."available_balance" AS "available"`).
+		ColumnExpr(`"bank_account"."limit_balance" AS "limit"`).
+		ColumnExpr(`"bank_account"."available_balance" - SUM(COALESCE("expense"."current_amount", 0)) - SUM(COALESCE("goal"."current_amount", 0)) AS "free"`).
+		ColumnExpr(`SUM(COALESCE("expense"."current_amount", 0)) AS "expenses"`).
+		ColumnExpr(`SUM(COALESCE("goal"."current_amount", 0)) AS "goals"`).
+		Join(`LEFT JOIN "expense"`).
+		JoinOn(`"expense"."bank_account_id" = "bank_account"."bank_account_id"`).
+		JoinOn(`"expense"."account_id" = "bank_account"."account_id"`).
+		Join(`LEFT JOIN "goal"`).
+		JoinOn(`"goal"."bank_account_id" = "bank_account"."bank_account_id"`).
+		JoinOn(`"goal"."account_id" = "bank_account"."account_id"`).
+		Where(`"bank_account"."account_id" = ?`, r.AccountId()).
+		Where(`"bank_account"."bank_account_id" = ?`, bankAccountId).
+		GroupExpr(`"bank_account"."bank_account_id"`).
+		GroupExpr(`"bank_account"."account_id"`).
 		Limit(1).
 		Select(&balance)
 	if err != nil {
