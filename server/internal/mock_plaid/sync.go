ed8afdef516c4d76d5b4e4d500ea74a38039a0be
Elliot Courant <me@elliotcourant.dev>
2025-02-17 15:13:37 -0600

    fix(plaid): Fix Plaid pagination error handling
    When we get an error from Plaid indicating that the sync must be retried
    from scratch we now throw an error causing the transaction to be rolled
    back. We then retry the job from scratch up to a few attempts to correct
    the issue. If we get any other error then we still return it normally.
    
    Resolves #2453

---
+++ server/internal/mock_plaid/sync.go
@@ -0,0 +1,71 @@
+package mock_plaid
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/monetr/monetr/server/internal/mock_http_helper"
+	"github.com/monetr/monetr/server/internal/myownsanity"
+	"github.com/plaid/plaid-go/v30/plaid"
+	"github.com/stretchr/testify/require"
+)
+
+func MockSync(t *testing.T, transactions []plaid.Transaction) {
+	mock_http_helper.NewHttpMockJsonResponder(
+		t,
+		"POST", Path(t, "/transactions/sync"),
+		func(t *testing.T, request *http.Request) (interface{}, int) {
+			ValidatePlaidAuthentication(t, request, RequireAccessToken)
+			var syncTransactionsRequest struct {
+				Cursor string `json:"cursor"`
+				Count  int    `json:"count"`
+			}
+			require.NoError(t, json.NewDecoder(request.Body).Decode(&syncTransactionsRequest), "must decode request")
+			count := syncTransactionsRequest.Count
+
+			filteredTransactions := make([]plaid.Transaction, 0, len(transactions))
+			for _, transaction := range transactions {
+				filteredTransactions = append(filteredTransactions, transaction)
+			}
+
+			endingOffset := myownsanity.Min(len(filteredTransactions), count)
+			data := filteredTransactions[0:endingOffset]
+
+			return plaid.TransactionsSyncResponse{
+				Accounts:             nil, // Add some basic reporting here too
+				Added:                data,
+				HasMore:              false,
+				RequestId:            gofakeit.UUID(),
+				AdditionalProperties: nil,
+			}, http.StatusOK
+		},
+		PlaidHeaders,
+	)
+}
+
+func MockSyncError(t *testing.T, error plaid.PlaidError) {
+	mock_http_helper.NewHttpMockJsonResponder(
+		t,
+		"POST", Path(t, "/transactions/sync"),
+		func(t *testing.T, request *http.Request) (interface{}, int) {
+			ValidatePlaidAuthentication(t, request, RequireAccessToken)
+			var syncTransactionsRequest struct {
+				Cursor string `json:"cursor"`
+				Count  int    `json:"count"`
+			}
+			require.NoError(t, json.NewDecoder(request.Body).Decode(&syncTransactionsRequest), "must decode request")
+
+			var status int
+			if s := error.Status.Get(); s != nil {
+				status = int(*s)
+			} else {
+				status = http.StatusInternalServerError
+			}
+
+			return error, status
+		},
+		PlaidHeaders,
+	)
+}
