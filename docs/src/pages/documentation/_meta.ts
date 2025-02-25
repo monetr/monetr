852b994077132957f80e6aad6288002d52aa6624
Elliot Courant <me@elliotcourant.dev>
2025-02-25 17:29:36 -0600

    chore: Tweaks

--- docs/src/pages/documentation/_meta.ts
+++ docs/src/pages/documentation/_meta.ts
@@ -12,7 +12,6 @@ export default {
   },
   'install': '',
   'configure': '',
-  'guides': 'Guides',
 
   '-- Contributing': {
     'type': 'separator',
--- docs/src/pages/documentation/guides/_meta.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  'plaid': 'Plaid',
-};
