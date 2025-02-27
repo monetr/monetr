1ff4b20af8fb448c90b7d0765548f09f1ed0bd41
Elliot Courant <me@elliotcourant.dev>
2025-02-27 17:52:34 -0600

    fix(ui): Add estimated deposit and exclude weekends to new funding modal
    When creating a new funding schedule, allow the user to specify whether
    or not the funding schedule excludes weekends or what the estimated
    deposit is.
    
    Resolves #2323

--- interface/src/modals/NewFundingModal.tsx
+++ interface/src/modals/NewFundingModal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { Fragment, useRef } from 'react';
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
 import { AxiosError } from 'axios';
 import { startOfDay, startOfTomorrow } from 'date-fns';
@@ -6,14 +6,17 @@ import { FormikHelpers } from 'formik';
 import { useSnackbar } from 'notistack';
 
 import FormButton from '@monetr/interface/components/FormButton';
+import MAmountField from '@monetr/interface/components/MAmountField';
 import MDatePicker from '@monetr/interface/components/MDatePicker';
 import MForm from '@monetr/interface/components/MForm';
 import MModal, { MModalRef } from '@monetr/interface/components/MModal';
 import MSelectFrequency from '@monetr/interface/components/MSelectFrequency';
 import MSpan from '@monetr/interface/components/MSpan';
 import MTextField from '@monetr/interface/components/MTextField';
+import { Switch } from '@monetr/interface/components/Switch';
 import { useSelectedBankAccountId } from '@monetr/interface/hooks/bankAccounts';
 import { useCreateFundingSchedule } from '@monetr/interface/hooks/fundingSchedules';
+import useLocaleCurrency from '@monetr/interface/hooks/useLocaleCurrency';
 import FundingSchedule from '@monetr/interface/models/FundingSchedule';
 import { ExtractProps } from '@monetr/interface/util/typescriptEvils';
 
@@ -39,6 +42,7 @@ function NewFundingModal(): JSX.Element {
   const { enqueueSnackbar } = useSnackbar();
   const selectedBankAccountId = useSelectedBankAccountId();
   const createFundingSchedule = useCreateFundingSchedule();
+  const { data: { friendlyToAmount } } = useLocaleCurrency();
 
   async function submit(values: NewFundingValues, helpers: FormikHelpers<NewFundingValues>): Promise<void> {
     helpers.setSubmitting(true);
@@ -47,8 +51,8 @@ function NewFundingModal(): JSX.Element {
       name: values.name,
       nextRecurrence: startOfDay(new Date(values.nextOccurrence)),
       ruleset: values.ruleset,
-      estimatedDeposit: null,
-      excludeWeekends: false,
+      estimatedDeposit: values.estimatedDeposit > 0 ? friendlyToAmount(values.estimatedDeposit) : null,
+      excludeWeekends: values.excludeWeekends,
     });
 
     return createFundingSchedule(newFundingSchedule)
@@ -62,52 +66,76 @@ function NewFundingModal(): JSX.Element {
   }
 
   return (
-    <MModal open={ modal.visible } ref={ ref } className='md:max-w-sm'>
+    <MModal open={ modal.visible } ref={ ref } className='md:max-w-md'>
       <MForm
         initialValues={ initialValues }
         onSubmit={ submit }
         className='h-full flex flex-col gap-2 p-2 justify-between' data-testid='new-funding-modal'
       >
-        <div className='flex flex-col'>
-          <MSpan className='font-bold text-xl mb-2'>
-            Create A New Funding Schedule
-          </MSpan>
-          <MTextField
-            autoFocus
-            id='funding-name-search' // Keep's 1Pass from hijacking normal name fields.
-            name='name'
-            label='What do you want to call your funding schedule?'
-            required
-            autoComplete='off'
-            placeholder='Example: Payday...'
-          />
-          <MDatePicker
-            name='nextOccurrence'
-            label='When do you get paid next?'
-            required
-            min={ startOfTomorrow() }
-          />
-          <MSelectFrequency
-            dateFrom='nextOccurrence'
-            menuPosition='fixed'
-            menuShouldScrollIntoView={ false }
-            menuShouldBlockScroll={ true }
-            menuPortalTarget={ document.body }
-            menuPlacement='bottom'
-            label='How often do you get paid?'
-            placeholder='Select a funding frequency...'
-            required
-            name='ruleset'
-          />
-        </div>
-        <div className='flex justify-end gap-2'>
-          <FormButton variant='destructive' onClick={ modal.remove } data-testid='close-new-funding-modal'>
-            Cancel
-          </FormButton>
-          <FormButton variant='primary' type='submit'>
-            Create
-          </FormButton>
-        </div>
+        { ({ setFieldValue, values }) => (
+          <Fragment>
+            <div className='flex flex-col'>
+              <MSpan className='font-bold text-xl mb-2'>
+                Create A New Funding Schedule
+              </MSpan>
+              <MTextField
+                autoFocus
+                id='funding-name-search' // Keep's 1Pass from hijacking normal name fields.
+                name='name'
+                label='What do you want to call your funding schedule?'
+                required
+                autoComplete='off'
+                placeholder='Example: Payday...'
+              />
+              <MDatePicker
+                name='nextOccurrence'
+                label='When do you get paid next?'
+                required
+                min={ startOfTomorrow() }
+              />
+              <MSelectFrequency
+                dateFrom='nextOccurrence'
+                menuPosition='fixed'
+                menuShouldScrollIntoView={ false }
+                menuShouldBlockScroll={ true }
+                menuPortalTarget={ document.body }
+                menuPlacement='bottom'
+                label='How often do you get paid?'
+                placeholder='Select a funding frequency...'
+                required
+                name='ruleset'
+              />
+              <MAmountField
+                allowNegative={ false }
+                label='Estimated Deposit'
+                name='estimatedDeposit'
+                placeholder='Example: $ 1,000.00'
+              />
+              <div className='flex flex-row items-center justify-between rounded-lg ring-1 p-2 ring-dark-monetr-border-string mb-4'>
+                <div className='space-y-0.5'>
+                  <label className='text-sm font-medium text-dark-monetr-content-emphasis cursor-pointer'>
+                    Exclude Weekends
+                  </label>
+                  <p className='text-sm text-dark-monetr-content'>
+                    If it were to land on a weekend, it is adjusted to the previous weekday instead.
+                  </p>
+                </div>
+                <Switch
+                  checked={ values['excludeWeekends'] }
+                  onCheckedChange={ () => setFieldValue('excludeWeekends', !values['excludeWeekends']) }
+                />
+              </div>
+            </div>
+            <div className='flex justify-end gap-2'>
+              <FormButton variant='destructive' onClick={ modal.remove } data-testid='close-new-funding-modal'>
+                Cancel
+              </FormButton>
+              <FormButton variant='primary' type='submit'>
+                Create
+              </FormButton>
+            </div>
+          </Fragment>
+        ) }
       </MForm>
     </MModal>
   );
