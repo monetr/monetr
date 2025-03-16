274524a1fe4b6f3e3d73693b46d1a9cf496913f8
Elliot Courant <me@elliotcourant.dev>
2025-03-16 15:30:48 -0500

    feat: Improvements on import flow
    Imports will be on a per link basis, once the import is created its
    kicked into a background job to be processed. The frontend can then
    initiate a `.../progress` request to interact with the import. Once the
    import goes into a confirming status the frontend can send data over the
    socket to tweak the import before it is persisted. This would be like
    excluding a bank account from the import or canceling it. Once they
    proceed with the import the websocket will provide status updates.

--- server/models/transaction_import.go
+++ server/models/transaction_import.go
@@ -8,30 +8,36 @@ import (
 	"github.com/go-pg/pg/v10"
 )
 
+// TransactionImportStatus represents the status and "stage" of the transaction
+// import workflow. When an import is created it will be created in a pending
+// status. The import is then kicked into a background job which will parse the
+// file and establish which accounts will be affected or if the file is even
+// valid. If the file is not valid it moves to failed immediately. If the file
+// is valid then it moves to a confirming status. This status indicates that the
+// user must provide some input to continue the file processing. At this stage
+// the user must confirm the data, things like the items in the import before
+// continuing. Once they have confirmed the import will move into a processing
+// state. If the user does not confirm, eventually the import will move to
+// expired as the file for the import is automatically removed after 1 hour. If
+// any of the import items fail during processing, the entire import is moved to
+// a failed status. If all import items succeed then the import is moved to a
+// complete status.
+type TransactionImportStatus string
+
+const (
+	TransactionImportStatusPending    = "pending"
+	TransactionImportStatusConfirming = "confirming"
+	TransactionImportStatusProcessing = "processing"
+	TransactionImportStatusFailed     = "failed"
+	TransactionImportStatusComplete   = "complete"
+	TransactionImportStatusExpired    = "expired"
+)
+
 var (
 	_ pg.BeforeInsertHook = (*TransactionImport)(nil)
 	_ Identifiable        = TransactionImport{}
 	_ Uploadable          = TransactionImport{}
 )
-
-type TransactionImportAffectedBankAccounts struct {
-	// UploadIdentifier is the hashed value of the account Id field from the camt
-	// file. This is typically an account number which is why it is hashed.
-	UploadIdentifier string `json:"uploadIdentifier"`
-	// UploadIdentifierType is taken from the camt `Acct/Id/...` field. It
-	// designates which element was used to uniquely identify the account.
-	UploadIdentifierType string `json:"uploadIdentifierType"`
-	// Name is the account name from the file and is taken from the camt field
-	// `Acct/Nm`.
-	Name string `json:"name"`
-	// Currency is the currency code taken from the account in the file. It may
-	// not be a currency supported by monetr.
-	Currency string `json:"currency"`
-	// BankAccountId is the ID of the account that this part of the import maps
-	// to. If the bank account ID is nil then this is an account that would be
-	// created.
-	BankAccountId *ID[BankAccount] `json:"bankAccountId"`
-}
 
 // TransactionImport is different from TraansctionUpload, uploads are specific
 // to a single bank account. Where as imports are a link level import. Imports
@@ -40,17 +46,18 @@ type TransactionImportAffectedBankAccounts struct {
 type TransactionImport struct {
 	tableName string `pg:"transaction_imports"`
 
-	TransactionImportId  ID[TransactionImport]                   `json:"transactionImportId" pg:"transaction_import_id,notnull,pk"`
-	AccountId            ID[Account]                             `json:"-" pg:"account_id,notnull,pk"`
-	Account              *Account                                `json:"-" pg:"rel:has-one"`
-	LinkId               ID[Link]                                `json:"linkId" pg:"link_id,notnull"`
-	Link                 *Link                                   `json:"-,omitempty" pg:"rel:has-one"`
-	FileId               ID[File]                                `json:"fileId" pg:"file_id,notnull"`
-	File                 *File                                   `json:"file,omitempty" pg:"rel:has-one"`
-	AffectedBankAccounts []TransactionImportAffectedBankAccounts `json:"affectedBankAccounts" pg:"affected_bank_accounts"`
-	CreatedAt            time.Time                               `json:"createdAt" pg:"created_at,notnull"`
-	CreatedBy            ID[User]                                `json:"createdBy" pg:"created_by,notnull"`
-	CreatedByUser        *User                                   `json:"-" pg:"rel:has-one,fk:created_by"`
+	TransactionImportId ID[TransactionImport]   `json:"transactionImportId" pg:"transaction_import_id,notnull,pk"`
+	AccountId           ID[Account]             `json:"-" pg:"account_id,notnull,pk"`
+	Account             *Account                `json:"-" pg:"rel:has-one"`
+	LinkId              ID[Link]                `json:"linkId" pg:"link_id,notnull"`
+	Link                *Link                   `json:"-,omitempty" pg:"rel:has-one"`
+	FileId              ID[File]                `json:"fileId" pg:"file_id,notnull"`
+	File                *File                   `json:"file,omitempty" pg:"rel:has-one"`
+	Items               []TransactionImportItem `json:"items" pg:"rel:has-many"`
+	ExpiresAt           time.Time               `json:"expiresAt" pg:"expires_at,notnull"`
+	CreatedAt           time.Time               `json:"createdAt" pg:"created_at,notnull"`
+	CreatedBy           ID[User]                `json:"createdBy" pg:"created_by,notnull"`
+	CreatedByUser       *User                   `json:"-" pg:"rel:has-one,fk:created_by"`
 }
 
 func (TransactionImport) IdentityPrefix() string {
@@ -80,4 +87,3 @@ func (o *TransactionImport) BeforeInsert(ctx context.Context) (context.Context, 
 
 	return ctx, nil
 }
-
