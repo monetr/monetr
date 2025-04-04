b87214fea3ac9ee99ca637e1080e7ea3ec308900
Elliot Courant <me@elliotcourant.dev>
2025-04-04 08:31:02 -0500

    fix(log): Added trace and span IDs to log messages
    This will make it easier to tie a specific trace (especially for errors
    or bugs) to the log messages associated with it.

--- server/internal/ctxkeys/keys.go
+++ server/internal/ctxkeys/keys.go
@@ -3,6 +3,7 @@ package ctxkeys
 import (
 	"context"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/sirupsen/logrus"
 )
 
@@ -26,9 +27,13 @@ var (
 	}
 )
 
-// LogrusFieldsFromContext will extract known fields from the Go context in order to better support logging. This allows
-// monetr to more easily provide useful context for all of its log entries for every action that occurs.
-func LogrusFieldsFromContext(ctx context.Context, existingFields logrus.Fields) logrus.Fields {
+// LogrusFieldsFromContext will extract known fields from the Go context in
+// order to better support logging. This allows monetr to more easily provide
+// useful context for all of its log entries for every action that occurs.
+func LogrusFieldsFromContext(
+	ctx context.Context,
+	existingFields logrus.Fields,
+) logrus.Fields {
 	fields := logrus.Fields{}
 	for _, key := range keys {
 		// If the field is already on the log entry then do not overwrite it.
@@ -38,6 +43,16 @@ func LogrusFieldsFromContext(ctx context.Context, existingFields logrus.Fields) 
 
 		if value := ctx.Value(key); value != nil {
 			fields[string(key)] = value
+		}
+	}
+
+	// Add tracing details to our log messages to make it easier to go from a
+	// trace in Sentry to logs elsewhere.
+	if span := sentry.SpanFromContext(ctx); span != nil {
+		fields["sentry"] = logrus.Fields{
+			"traceId":      span.TraceID,
+			"spanId":       span.SpanID,
+			"parentSpanId": span.ParentSpanID,
 		}
 	}
 
