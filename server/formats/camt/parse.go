d174ca93e4d18211eff993b944ac881d814b2019
Elliot Courant <me@elliotcourant.dev>
2025-09-27 21:21:53 -0500

    feat: Adding a function to parse CAMT.053 amounts

--- server/formats/camt/parse.go
+++ server/formats/camt/parse.go
@@ -3,7 +3,10 @@ package camt
 import (
 	"encoding/xml"
 	"io"
+	"strings"
 
+	"github.com/monetr/monetr/server/currency"
+	"github.com/monetr/monetr/server/internal/myownsanity"
 	"github.com/pkg/errors"
 )
 
@@ -23,4 +26,30 @@ func Parse(reader io.Reader) (*CamtDocument, error) {
 	}
 
 	return &camt, nil
+}
+
+// ParseTransactionAmount takes a CAMT.053 transaction entry and extracts the
+// amount field as well as the currency code for the amount (if present?) and
+// parses the amount in that field. Even though CAMT.053 files deserialize as
+// float64, this function will safely and properly convert that value into an
+// int64 for monetr's own storage; as well as changing the sign of the number
+// depending on the direction of the transaction.
+func ParseTransactionAmount(transaction ReportEntry15) (int64, error) {
+	amount, err := currency.ParseFloatToAmount(
+		transaction.Amt.Value,
+		transaction.Amt.CcyAttr,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	switch {
+	case strings.EqualFold(transaction.CdtDbtInd, "DBIT"):
+		return myownsanity.Abs(amount), nil
+	case strings.EqualFold(transaction.CdtDbtInd, "CRDT"):
+		// Credits are represented as negative in monetr.
+		return myownsanity.Abs(amount) * -1, nil
+	default:
+		return amount, nil
+	}
 }
