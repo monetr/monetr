3c4fd8233d8f2def1e0e884e530ef8e04991d114
Elliot Courant <me@elliotcourant.dev>
2025-09-29 18:15:34 -0500

    chore: Fixing tests

--- interface/src/hooks/useIconSearch.ts
+++ interface/src/hooks/useIconSearch.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { QueryMethod } from '@monetr/interface/components/MQueryClient';
 import { useAppConfiguration } from '@monetr/interface/hooks/useAppConfiguration';
 
 export interface Icon {
@@ -12,6 +13,9 @@ export function useIconSearch(name: string): Icon | null {
   const { data } = useQuery<Icon>({
     queryKey: ['/icons/search', { name }],
     enabled: Boolean(configuration?.iconsEnabled) && Boolean(name) && name?.length > 0,
+    meta: {
+      method: QueryMethod.UseBody,
+    },
     staleTime: 60 * 60 * 1000, // 60 minutes in milliseconds
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
