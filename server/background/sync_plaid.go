270bd78b66fa00896996aaa9822dc06ed294478b
Elliot Courant <me@elliotcourant.dev>
2025-10-01 08:35:41 -0500

    fix(sync): Fixing how errors are reported for plaid sync

--- server/background/sync_plaid.go
+++ server/background/sync_plaid.go
@@ -179,17 +179,19 @@ RetrySync:
 	})
 	{ // Allow the plaid sync job to be retried under some circumstances
 		attempts++
-		if attempts < maxAttempts {
-			switch plaidError := errors.Cause(err).(type) {
-			case *platypus.PlatypusError:
-				if plaidError.ErrorCode == "TRANSACTIONS_SYNC_MUTATION_DURING_PAGINATION" {
+		switch plaidError := errors.Cause(err).(type) {
+		case *platypus.PlatypusError:
+			if plaidError.ErrorCode == "TRANSACTIONS_SYNC_MUTATION_DURING_PAGINATION" {
+				// So we don't report this error to sentry when its not necessary.
+				err = nil
+				if attempts < maxAttempts {
 					log.WithError(err).Warn("plaid sync failed with mutation error, job will be retried in a few seconds")
-					// So we don't report this error to sentry when its not necessary.
-					err = nil
 					// TODO Very evil, would be better to just build in an actual backoff
 					// with the job system. But this will do something at least.
 					time.Sleep(time.Duration(attempts) * 2 * time.Second)
 					goto RetrySync
+				} else {
+					log.WithError(err).Warn("plaid sync failed with mutation error, job will no longer be retried")
 				}
 			}
 		}
