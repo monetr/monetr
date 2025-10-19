2ad11ebdee6e3f6165d1cc1118c46da23fc058b6
Elliot Courant <me@elliotcourant.dev>
2025-10-18 20:15:19 -0500

    chore: Linting?

--- stories/stories/colors.stories.tsx
+++ stories/stories/colors.stories.tsx
@@ -19,21 +19,20 @@ export const Pallete: StoryObj<typeof Component> = {
   render: () => {
     const theme = realTailwindConfig.theme;
 
-    const colors = Object.entries(theme.colors)
-      .flatMap(([name, variations]) => {
-        const items = Object.entries(variations)
-          .filter(([_, color]) => !!color)
-          .map(([weight, color]) => (
-            <div key={`${name}-${weight}`}>
-              <span>
-                {name}-{weight}
-              </span>
-              <div className='p-4 col-span-1' style={{ backgroundColor: color as any }} />
-            </div>
-          ));
+    const colors = Object.entries(theme.colors).flatMap(([name, variations]) => {
+      const items = Object.entries(variations)
+        .filter(([_, color]) => !!color)
+        .map(([weight, color]) => (
+          <div key={`${name}-${weight}`}>
+            <span>
+              {name}-{weight}
+            </span>
+            <div className='p-4 col-span-1' style={{ backgroundColor: color as any }} />
+          </div>
+        ));
 
-        return items;
-      });
+      return items;
+    });
 
     return (
       <div className='bg-black'>
