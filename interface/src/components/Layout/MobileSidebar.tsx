55fe490840ef143b3fdd1435ad0df12c34305d57
Elliot Courant <me@elliotcourant.dev>
2025-10-19 19:47:25 -0500

    fix(ui): Fix mobile sidebar not auto-closing

--- interface/src/components/Layout/MobileSidebar.tsx
+++ interface/src/components/Layout/MobileSidebar.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useMatch } from 'react-router-dom';
+import { useLocation, useMatch } from 'react-router-dom';
 
 import BankSidebar from '@monetr/interface/components/Layout/BankSidebar';
 import BudgetingSidebar from '@monetr/interface/components/Layout/BudgetingSidebar';
@@ -10,14 +10,19 @@ export default function MobileSidebar(): JSX.Element {
   const match = useMatch('/bank/:bankId/*');
   const isBankRoute = Boolean(match?.params?.bankId || null);
   const { setMobileSidebarOpen, mobileSidebarOpen } = useStore();
+  const { pathname } = useLocation();
 
   // When we navigate away from the current page, if the sidebar is open; close it.
   // This achieves the behavior of; if they click a navigation item in the sidebar we automatically
   // close the sidebar. Without us having to have some kind of magic that listens for clicks or anything
   // like that.
   useEffect(() => {
-    setMobileSidebarOpen(false);
-  }, [setMobileSidebarOpen]);
+    // Make sure that the pathname doesn't get autoremoved by the linter.
+    if (pathname) {
+      // Whenever the pathname changes close the sidebar
+      setMobileSidebarOpen(false);
+    }
+  }, [pathname, setMobileSidebarOpen]);
 
   // Keeps the entire sidebar from re-rendering when things change. This way stuff like the drawer animation works
   // properly.
