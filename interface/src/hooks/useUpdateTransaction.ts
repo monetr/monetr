8a525c26f738ae14201436f2912de9c8b7e037fe
Elliot Courant <me@elliotcourant.dev>
2025-10-24 09:15:21 -0500

    fix(ui): Fixed broken transaction updates
    Fixed an issue where transaction updates would say they failed when they
    really didn't.
    
    Resolves #2774

--- interface/src/hooks/useUpdateTransaction.ts
+++ interface/src/hooks/useUpdateTransaction.ts
@@ -1,4 +1,4 @@
-import { type InfiniteData, useMutation, useQueryClient } from '@tanstack/react-query';
+import { type InfiniteData, useMutation } from '@tanstack/react-query';
 
 import Balance from '@monetr/interface/models/Balance';
 import type Spending from '@monetr/interface/models/Spending';
@@ -12,54 +12,55 @@ export interface TransactionUpdateResponse {
 }
 
 export function useUpdateTransaction(): (_transaction: Transaction) => Promise<TransactionUpdateResponse> {
-  const queryClient = useQueryClient();
-
-  async function updateTransaction(transaction: Transaction): Promise<TransactionUpdateResponse> {
-    return request()
-      .put<TransactionUpdateResponse>(
-        `/bank_accounts/${transaction.bankAccountId}/transactions/${transaction.transactionId}`,
-        transaction,
-      )
-      .then(result => result.data);
-  }
-
   const { mutateAsync } = useMutation({
-    mutationFn: updateTransaction,
-    onSuccess: ({ transaction, spending, balance }: TransactionUpdateResponse) =>
+    async mutationFn(transaction: Transaction): Promise<TransactionUpdateResponse> {
+      return request()
+        .put<TransactionUpdateResponse>(
+          `/bank_accounts/${transaction.bankAccountId}/transactions/${transaction.transactionId}`,
+          transaction,
+        )
+        .then(result => result.data);
+    },
+    onSuccess: ({ transaction, spending, balance }: TransactionUpdateResponse, _input, _, { client: queryClient }) =>
       Promise.all([
-        queryClient.setQueryData(
+        queryClient.setQueryData<InfiniteData<Array<Partial<Transaction>>>>(
           [`/bank_accounts/${transaction.bankAccountId}/transactions`],
-          (previous: InfiniteData<Array<Transaction>>) =>
+          previous =>
             // If previous does not exist then do nothing, otherwise this will break the page.
             previous && {
               ...previous,
-              pages: previous?.pages?.map(page =>
+              // Map over all of the pages
+              pages: previous.pages?.map(page =>
+                // And over all the items in the page, looking for transactions with the same ID as the one we are
+                // updating, if we find one then return the updated transaction instead of the one that was there.
                 page.map(item => (item.transactionId === transaction.transactionId ? transaction : item)),
               ),
             },
+          {},
         ),
         queryClient.setQueryData(
           [`/bank_accounts/${transaction.bankAccountId}/transactions/${transaction.transactionId}`],
           transaction,
         ),
-        queryClient.setQueryData(
+        queryClient.setQueryData<Array<Partial<Spending>>>(
           [`/bank_accounts/${transaction.bankAccountId}/spending`],
-          (previous: Array<Partial<Spending>>) =>
-            previous.map(item => (spending || []).find(updated => updated.spendingId === item.spendingId) || item),
+          previous =>
+            // Since there could be multiple spending objects updated here, we need to take map over all of the existing
+            // spendinng objects and then check to see if that spending object is in the array of updated spending
+            // objects. If it is, replace it.
+            previous?.map(item => (spending || []).find(updated => updated.spendingId === item.spendingId) || item),
         ),
+        // For all of the spending objects that were updated we need to make sure to update their individual items if
+        // they have been requested.
         (spending || []).map(spending =>
           queryClient.setQueryData(
             [`/bank_accounts/${transaction.bankAccountId}/spending/${spending.spendingId}`],
             spending,
           ),
         ),
-        queryClient.setQueryData(
+        queryClient.setQueryData<Partial<Balance>>(
           [`/bank_accounts/${transaction.bankAccountId}/balances`],
-          (previous: Partial<Balance>) =>
-            new Balance({
-              ...previous,
-              ...balance,
-            }),
+          previous => new Balance({ ...previous, ...balance }),
         ),
         queryClient.invalidateQueries({
           queryKey: [`/bank_accounts/${transaction.bankAccountId}/forecast`],
