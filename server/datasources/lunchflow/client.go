f4a1932c3b1a12e4594bbcd6e4da006a9e3b7024
Elliot Courant <me@elliotcourant.dev>
2025-12-11 08:13:09 -0600

    feat(api): Adding Lunch Flow as a datasource
    Lunch Flow provides a way for users who are self hosting their own
    budgeting apps to sync their data with institutions that are in the
    EU/UK as well as other countries as well. This was previously very
    difficult to do as an individual.
    
    This is adding support for Lunch Flow as a datasource for monetr, but it
    will only be available for self hosted instances.
    
    Related to #2427

---
+++ server/datasources/lunchflow/client.go
@@ -0,0 +1,39 @@
+package lunchflow
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type AccountId string
+
+type Account struct {
+	Id              AccountId `json:"id"`
+	Name            string    `json:"name"`
+	InstitutionName string    `json:"institution_name"`
+	InstitutionLogo *string   `json:"institution_logo"`
+	Provider        string    `json:"provider"`
+	Currency        string    `json:"currency"`
+	Status          string    `json:"status"`
+}
+
+type Balance struct {
+	Amount   json.Number `json:"amount"`
+	Currency string      `json:"currency"`
+}
+
+type Transaction struct {
+	Id          string      `json:"id"`
+	AccountId   AccountId   `json:"accountId"`
+	Amount      json.Number `json:"amount"`
+	Currency    string      `json:"currency"`
+	Date        string      `json:"date"`
+	Merchant    *string     `json:"merchant"`
+	Description *string     `json:"description"`
+}
+
+type Client interface {
+	GetAccounts(ctx context.Context) ([]Account, error)
+	GetBalance(ctx context.Context, accountId AccountId) (*Balance, error)
+	GetTransactions(ctx context.Context, accountId AccountId) ([]Transaction, error)
+}
