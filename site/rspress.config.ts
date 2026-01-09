c9d27023c85d6b9f152cd949c620890a34695a44
Elliot Courant <me@elliotcourant.dev>
2026-01-09 10:42:26 -0600

    docs: Migrate docs site to rspress

---
+++ site/rspress.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from '@rspress/core';
+
+export default defineConfig({
+  root: 'src',
+  lang: 'en',
+  logo: 'https://monetr.app/_next/static/media/logo.77f6bc96.svg',
+  logoText: 'monetr',
+  themeConfig: {
+    darkMode: false,
+  },
+  locales: [
+    {
+      lang: 'en',
+      label: 'English',
+      title: 'monetr',
+      description: 'monetr',
+    },
+  ],
+  route: {
+    cleanUrls: true,
+    excludeConvention: ['**/public/*'],
+  },
+  multiVersion: {
+    default: 'v1',
+    versions: ['v1'],
+  },
+  search: {
+    versioned: true,
+  },
+  builderConfig: {
+    html: {
+      tags: [
+        {
+          tag: 'script',
+          // Specify the default theme mode, which can be `dark` or `light`
+          children: "window.RSPRESS_THEME = 'dark';",
+        },
+      ],
+    },
+  },
+});
---
+++ site/src/v1/en/pricing.mdx
@@ -0,0 +1,5 @@
+---
+title: Pricing
+---
+
+# Pricing
