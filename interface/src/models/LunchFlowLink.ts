389c6ef1d7259b57b4e22b29d40d1fa8da11e078
Elliot Courant <me@elliotcourant.dev>
2026-01-18 12:06:03 -0600

    chore: Starting on some intial frontend

--- interface/src/models/LunchFlowLink.ts
+++ interface/src/models/LunchFlowLink.ts
@@ -1,39 +1,33 @@
 import parseDate from '@monetr/interface/util/parseDate';
 
-export enum PlaidLinkStatus {
-  Unknown = 0,
-  Pending = 1,
-  Setup = 2,
-  Error = 3,
-  PendingExpiration = 4,
-  Revoked = 5,
+export enum LunchFlowLinkStatus {
+  Active = 'active',
+  Deactivated = 'deactivated',
+  Error = 'error',
 }
 
-export default class PlaidLink {
-  products: Array<string>;
-  status: PlaidLinkStatus;
-  errorCode?: string;
-  expirationDate?: Date;
-  newAccountsAvailable: boolean;
-  institutionId: string;
-  institutionName: string;
+export default class LunchFlowLink {
+  lunchFlowLinkId: string;
+  apiUrl: string;
+  status: LunchFlowLinkStatus;
   lastManualSync?: Date;
   lastSuccessfulUpdate?: Date;
   lastAttemptedUpdate?: Date;
   updatedAt: Date;
   createdAt: Date;
+  deletedAt?: Date;
   createdBy: string;
 
-  constructor(data?: Partial<PlaidLink>) {
+  constructor(data?: Partial<LunchFlowLink>) {
     if (data) {
       Object.assign(this, {
         ...data,
-        expirationDate: parseDate(data?.expirationDate),
         lastManualSync: parseDate(data?.lastManualSync),
         lastSuccessfulUpdate: parseDate(data?.lastSuccessfulUpdate),
         lastAttemptedUpdate: parseDate(data?.lastAttemptedUpdate),
         updatedAt: parseDate(data?.updatedAt),
         createdAt: parseDate(data?.createdAt),
+        deletedAt: parseDate(data?.deletedAt),
       });
     }
   }
