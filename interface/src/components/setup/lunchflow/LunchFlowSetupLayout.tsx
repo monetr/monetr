053b4ae829e742cabe1726d6665b147ee3124c5e
Elliot Courant <me@elliotcourant.dev>
2026-01-25 13:55:02 -0600

    chore: Logo Improvements

--- interface/src/components/setup/lunchflow/LunchFlowSetupLayout.tsx
+++ interface/src/components/setup/lunchflow/LunchFlowSetupLayout.tsx
@@ -1,15 +1,12 @@
 import type { ReactNode } from 'react';
+import { Plus } from 'lucide-react';
 
+import LunchFlowLogo from '@monetr/interface/components/Logo/LunchFlowLogo';
 import MLogo from '@monetr/interface/components/MLogo';
 import MStepper from '@monetr/interface/components/MStepper';
 import { LunchFlowSetupSteps } from '@monetr/interface/components/setup/lunchflow/LunchFlowSetupSteps';
 
 import styles from './LunchFlowSetupLayout.module.scss';
-import { useViewContext } from '@monetr/interface/components/ViewManager';
-import {
-  LunchFlowSetupForm,
-  LunchFlowSetupMetadata,
-} from '@monetr/interface/components/setup/lunchflow/LunchFlowSetup';
 
 interface LunchFlowSetupLayoutProps {
   step?: LunchFlowSetupSteps;
@@ -24,8 +21,12 @@ export default function LunchFlowSetupLayout(props: LunchFlowSetupLayoutProps): 
       <div className='p-0 md:p-8 w-full'>
         <MStepper activeIndex={step} steps={['Intro', 'Accounts', 'Sync']} />
       </div>
-      <div className='flex flex-col md:justify-center items-center max-w-sm'>
-        <MLogo className='w-24 h-24' />
+      <div className='flex flex-col md:justify-center items-center max-w-sm gap-2'>
+        <div className='flex gap-2'>
+          <MLogo className='w-16 h-16' />
+          <Plus className='h-16' />
+          <LunchFlowLogo className='h-16' iconOnly />
+        </div>
         {props.children}
       </div>
       <div />
