44a177472f424fd2c68f31fbc4822d4f8846074a
Elliot Courant <me@elliotcourant.dev>
2026-03-03 20:00:50 -0600

    refactor(ui): More migrations to scss modules

--- interface/src/components/transactions/TransactionItem.tsx
+++ interface/src/components/transactions/TransactionItem.tsx
@@ -1,6 +1,6 @@
+import { ChevronRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
-import ArrowLink from '@monetr/interface/components/ArrowLink';
 import Flex from '@monetr/interface/components/Flex';
 import Typography from '@monetr/interface/components/Typography';
 import TransactionAmount from '@monetr/interface/components/transactions/TransactionAmount';
@@ -10,7 +10,8 @@ import { useSpending } from '@monetr/interface/hooks/useSpending';
 import type Transaction from '@monetr/interface/models/Transaction';
 import mergeTailwind from '@monetr/interface/util/mergeTailwind';
 
-import styles from './TransactionItemSelectSpending.module.scss';
+import itemStyles from './TransactionItem.module.scss';
+import selectSpendingStyles from './TransactionItemSelectSpending.module.scss';
 
 export interface TransactionItemProps {
   transaction: Transaction;
@@ -21,34 +22,33 @@ export default function TransactionItem({ transaction }: TransactionItemProps): 
 
   return (
     <li
-      className={mergeTailwind('group relative w-full px-1 md:px-2', styles.transactionItemRoot)}
+      className={mergeTailwind(itemStyles.root, selectSpendingStyles.transactionItemRoot)}
       data-testid={transaction.transactionId}
       id={transaction.transactionId}
     >
-      <Link
-        className='absolute left-0 top-0 flex h-full w-full cursor-pointer md:hidden md:cursor-auto'
-        to={detailsUrl}
-      />
-      <div className='group flex h-full gap-1 rounded-lg px-2 py-1 group-hover:bg-zinc-600 md:gap-4'>
-        <div className='flex w-full min-w-0 flex-1 flex-row items-center gap-4 md:w-1/2'>
+      <Link className={itemStyles.mobileLink} to={detailsUrl} />
+      <div className={itemStyles.inner}>
+        <div className={itemStyles.leftSection}>
           <TransactionMerchantIcon name={transaction.getName()} pending={transaction.isPending} />
           <Flex flex='shrink' gap='none' orientation='column'>
             <Typography color='emphasis' ellipsis size='md' weight='semibold'>
               {transaction.getName()}
             </Typography>
-            <Typography className='hidden md:block' ellipsis size='sm' weight='medium'>
+            <Typography className={itemStyles.categoryLabel} ellipsis size='sm' weight='medium'>
               {transaction.getMainCategory()}
             </Typography>
-            <BudgetingInfo className='flex w-full text-sm md:hidden' transaction={transaction} />
+            <BudgetingInfo className={itemStyles.budgetingMobile} transaction={transaction} />
           </Flex>
         </div>
         {!transaction.getIsAddition() && <TransactionItemSelectSpending transaction={transaction} />}
         {transaction.getIsAddition() && (
-          <BudgetingInfo className='hidden md:flex w-1/2 flex-1 items-center pl-6' transaction={transaction} />
+          <BudgetingInfo className={itemStyles.budgetingAddition} transaction={transaction} />
         )}
-        <div className='flex shrink-0 items-center justify-end gap-2 md:min-w-[8em]'>
+        <div className={itemStyles.amountSection}>
           <TransactionAmount transaction={transaction} />
-          <ArrowLink to={detailsUrl} />
+          <Link className={itemStyles.arrowLink} tabIndex={-1} to={detailsUrl}>
+            <ChevronRight />
+          </Link>
         </div>
       </div>
     </li>
@@ -60,42 +60,24 @@ interface BudgetingInfoProps {
   transaction: Transaction;
 }
 
-function BudgetingInfo({ transaction, ...props }: BudgetingInfoProps): JSX.Element {
+function BudgetingInfo({ transaction, className }: BudgetingInfoProps): JSX.Element {
   const { data: spending } = useSpending(transaction.spendingId);
-  const className = mergeTailwind('overflow-hidden', 'text-ellipsis', 'whitespace-nowrap', 'min-w-0', props.className);
-
-  const spentFromClasses = mergeTailwind(
-    {
-      // Transaction does have spending
-      'font-bold': Boolean(transaction.spendingId),
-      'dark:text-dark-monetr-content-emphasis': Boolean(transaction.spendingId),
-      // No spending for the transaction
-      'font-medium': !transaction.spendingId,
-      'dark:text-dark-monetr-content': !transaction.spendingId,
-    },
-    'md:text-base',
-    'min-w-0',
-    'overflow-hidden',
-    'text-ellipsis',
-    'text-sm',
-    'whitespace-nowrap',
-  );
 
   if (transaction.getIsAddition()) {
     return (
-      <span className={className}>
-        <span className='min-w-0 flex-none truncate font-medium dark:text-dark-monetr-content-subtle'>
-          Contribution
-        </span>
+      <span className={mergeTailwind(itemStyles.budgetingInfo, className)}>
+        <span className={itemStyles.contributionLabel}>Contribution</span>
       </span>
     );
   }
 
   return (
-    <span className={className}>
-      <span className='min-w-0 flex-none truncate font-medium dark:text-content'>Spent from</span>
+    <span className={mergeTailwind(itemStyles.budgetingInfo, className)}>
+      <span className={itemStyles.spentFromLabel}>Spent from</span>
       &nbsp;
-      <span className={spentFromClasses}>{spending?.name || 'Free-To-Use'}</span>
+      <span className={itemStyles.spentFromValue} data-hasspending={String(Boolean(transaction.spendingId))}>
+        {spending?.name || 'Free-To-Use'}
+      </span>
     </span>
   );
 }
