08e5cd805ebc54491afd43252344c3d6c80943a6
Elliot Courant <me@elliotcourant.dev>
2026-03-05 08:09:57 -0600

    chore: Progress on BankSidebarSubscriptionItem

--- interface/src/components/Layout/BankSidebarSubscriptionItem.tsx
+++ interface/src/components/Layout/BankSidebarSubscriptionItem.tsx
@@ -6,6 +6,8 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@monetr/interface/compo
 import { useAppConfiguration } from '@monetr/interface/hooks/useAppConfiguration';
 import { useAuthentication } from '@monetr/interface/hooks/useAuthentication';
 
+import styles from './BankSidebarSubscriptionItem.module.scss';
+
 export default function BankSidebarSubscriptionItem(): JSX.Element {
   const { data: config } = useAppConfiguration();
   const { data } = useAuthentication();
@@ -19,11 +21,11 @@ export default function BankSidebarSubscriptionItem(): JSX.Element {
     return (
       <Tooltip delayDuration={100}>
         <TooltipTrigger>
-          <Link className='relative group' data-testid='bank-sidebar-subscription' to={path}>
-            <CreditCard className='dark:group-hover:text-dark-monetr-content-emphasis dark:text-dark-monetr-content-subtle cursor-pointer mt-1.5' />
-            <span className='absolute flex h-2 w-2 right-0 bottom-0'>
-              <span className='animate-ping-slow absolute inline-flex h-full w-full rounded-full bg-yellow-400' />
-              <span className='relative inline-flex rounded-full h-2 w-2 bg-yellow-500' />
+          <Link className={styles.link} data-testid='bank-sidebar-subscription' to={path}>
+            <CreditCard className={`${styles.icon} ${styles.iconTrialing}`} />
+            <span className={styles.statusIndicator}>
+              <span className={styles.statusPing} />
+              <span className={styles.statusDot} />
             </span>
           </Link>
         </TooltipTrigger>
@@ -35,8 +37,8 @@ export default function BankSidebarSubscriptionItem(): JSX.Element {
   }
 
   return (
-    <Link data-testid='bank-sidebar-subscription' to={path}>
-      <CreditCard className='dark:hover:text-dark-monetr-content-emphasis dark:text-dark-monetr-content-subtle cursor-pointer' />
+    <Link className={styles.link} data-testid='bank-sidebar-subscription' to={path}>
+      <CreditCard className={styles.icon} />
     </Link>
   );
 }
