b8e5786c541642906ad483918d46224ea0132a70
Elliot Courant <me@elliotcourant.dev>
2026-03-07 21:42:54 -0600

    fix(api): Fixing transaction cluster bug (#3005)
    * fix(api): Fixing transaction cluster bug
    
    With the new changes where transaction clusters are now persisted and
    updated over time. This made bugs evident in the transaction cluster
    algorithm that made it so that persistent clusters were not being built
    properly.
    
    This resolves those bugs. Improving the accuracy of the centroid
    tracking.
    
    * chore: Logging
    
    On trace level logging, log the clusters that were deleted and the ones
    that weree modified

--- server/repository/transaction_cluster.go
+++ server/repository/transaction_cluster.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-pg/pg/v10"
 	"github.com/go-pg/pg/v10/types"
 	"github.com/monetr/monetr/server/crumbs"
+	"github.com/monetr/monetr/server/logging"
 	. "github.com/monetr/monetr/server/models"
 	"github.com/pkg/errors"
 )
@@ -14,12 +15,13 @@ func (r *repositoryBase) WriteTransactionClusters(
 	ctx context.Context,
 	bankAccountId ID[BankAccount],
 	clusters []TransactionCluster,
-) error {
+) (updated []TransactionCluster, err error) {
 	span := crumbs.StartFnTrace(ctx)
 	defer span.Finish()
 
+	// Build an array of signature + centroid pairs that we want to keep. We will
+	// delete verything that isn't in this dataset.
 	keysToKeep := []types.ValueAppender{}
-
 	for i := range clusters {
 		cluster := clusters[i]
 		cluster.AccountId = r.AccountId()
@@ -34,6 +36,7 @@ func (r *repositoryBase) WriteTransactionClusters(
 		clusters[i] = cluster
 	}
 
+	var deleted []TransactionCluster
 	// Because transaction clusters are calculated using the entire bank accounts
 	// transaction dataset. If any clusters exist right now that don't have the
 	// same signature/centroid pair then we can safely remove them. Either those
@@ -41,33 +44,58 @@ func (r *repositoryBase) WriteTransactionClusters(
 	// merged into a newer larger cluster based on updated information. Either way
 	// the old cluster needs to be removed. This might be volatile for accounts
 	// with less data, but becomes very stable the more data the accounts have.
-	_, err := r.txn.ModelContext(span.Context(), &TransactionCluster{}).
+	cleanResult, err := r.txn.ModelContext(span.Context(), &TransactionCluster{}).
 		Where(`"account_id" = ?`, r.AccountId()).
 		Where(`"bank_account_id" = ?`, bankAccountId).
 		WhereIn(`("signature", "centroid") NOT IN (?)`, keysToKeep).
-		Delete()
+		Returning("*").
+		Delete(&deleted)
 	if err != nil {
-		return errors.Wrap(err, "failed to clean up outdated transaction clusters")
+		return nil, errors.Wrap(err, "failed to clean up outdated transaction clusters")
 	}
 
 	// Then we can insert all the transaction clusters we have calculated. But if
 	// we get a conflict on our unique index then we will instead just update the
 	// existing cluster.
-	_, err = r.txn.ModelContext(span.Context(), &clusters).
+	result, err := r.txn.ModelContext(span.Context(), &clusters).
 		OnConflict(`("account_id", "bank_account_id", "signature", "centroid") DO UPDATE`).
-		Set(`members = EXCLUDED.members`).
-		Set(`debug = EXCLUDED.debug`).
+		Set(`"members" = EXCLUDED.members`).
+		Set(`"debug" = EXCLUDED.debug`).
+		Set(`"merchant" = EXCLUDED.merchant`).
 		// TODO It is possible for a cluster to be recalculated with no changes
 		// whatsoever. When this happens it does not exactly make sense to update
 		// the updated_at timestamp here. But it would involve pulling that clusters
 		// data from the database and comparing it in someway.
-		Set(`updated_at = now()`).
+		Set(`"updated_at" = now()`).
+		Where(`"transaction_cluster"."members" != EXCLUDED.members`).
+		// Return rows that we touched. This will only return rows that were either
+		// freshly inserted or were updated by this upsert. Rows that are identical
+		// to existing data are not returned here.
+		Returning(`"transaction_cluster".*`).
 		Insert(&clusters)
 	if err != nil {
-		return errors.Wrap(err, "failed to insert the new transaction clusters")
+		return nil, errors.Wrap(err, "failed to insert the new transaction clusters")
 	}
 
-	return nil
+	if len(deleted) > 0 {
+		r.log.Log(
+			span.Context(),
+			logging.LevelTrace,
+			"cleaned outdated transaction clusters",
+			"removed", deleted,
+			"upserted", clusters,
+		)
+	}
+
+	r.log.DebugContext(
+		span.Context(),
+		"upserted transaction clusters",
+		"cleaned", cleanResult.RowsAffected(),
+		"returned", result.RowsReturned(),
+		"affected", result.RowsAffected(),
+	)
+
+	return clusters, nil
 }
 
 func (r *repositoryBase) GetTransactionClusterByMember(
