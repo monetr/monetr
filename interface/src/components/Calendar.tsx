afd2cb516e862f3d6cf10275def5ba2ce4f22fa0
Elliot Courant <me@elliotcourant.dev>
2026-04-17 22:47:30 -0500

    deps(ui): Upgrading to react-day-picker@v9

--- interface/src/components/Calendar.tsx
+++ interface/src/components/Calendar.tsx
@@ -1,6 +1,12 @@
-import { addYears, format } from 'date-fns';
+import { addYears, format, type Locale } from 'date-fns';
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
-import { DayPicker, type DayPickerSingleProps, useNavigation } from 'react-day-picker';
+import {
+  DayPicker,
+  type MonthCaptionProps,
+  type PropsBase,
+  type PropsSingle,
+  useDayPicker,
+} from 'react-day-picker';
 
 import { Button, buttonVariants } from '@monetr/interface/components/Button';
 import Typography from '@monetr/interface/components/Typography';
@@ -8,7 +14,7 @@ import mergeTailwind from '@monetr/interface/util/mergeTailwind';
 
 import styles from './Calendar.module.scss';
 
-export type CalendarProps = DayPickerSingleProps & {
+export type CalendarProps = PropsBase & PropsSingle & {
   enableYearNavigation?: boolean;
 };
 
@@ -26,54 +32,59 @@ function Calendar({
       classNames={{
         months: styles.months,
         month: styles.month,
-        caption: styles.captionRow,
-        caption_label: styles.captionLabel,
+        month_caption: styles.captionRow,
         nav: styles.nav,
-        nav_button: buttonVariants({ variant: 'calendar' }),
-        nav_button_previous: styles.navButtonPrevious,
-        nav_button_next: styles.navButtonNext,
-        table: styles.table,
-        head_row: styles.headRow,
-        head_cell: styles.headCell,
-        row: styles.row,
-        cell: styles.cell,
-        day: styles.day,
-        day_range_end: 'day-range-end',
-        day_selected: styles.daySelected,
-        day_today: styles.dayToday,
-        day_outside: styles.dayOutside,
-        day_disabled: styles.dayDisabled,
-        day_range_middle: styles.dayRangeMiddle,
-        day_hidden: styles.dayHidden,
+        button_previous: mergeTailwind(buttonVariants({ variant: 'calendar' }), styles.navButtonPrevious),
+        button_next: mergeTailwind(buttonVariants({ variant: 'calendar' }), styles.navButtonNext),
+        month_grid: styles.table,
+        weekdays: styles.headRow,
+        weekday: styles.headCell,
+        week: styles.row,
+        day: styles.cell,
+        day_button: styles.day,
+        selected: styles.daySelected,
+        today: styles.dayToday,
+        outside: styles.dayOutside,
+        disabled: styles.dayDisabled,
+        hidden: styles.dayHidden,
         ...classNames,
       }}
       components={{
         // biome-ignore lint/correctness/noNestedComponentDefinitions: Easier to structure it this way.
-        Caption: ({ ...props }) => {
-          const { goToMonth, nextMonth, previousMonth, currentMonth } = useNavigation();
+        MonthCaption: (captionProps: MonthCaptionProps) => {
+          const { goToMonth, nextMonth, previousMonth } = useDayPicker();
+          const displayMonth = captionProps.calendarMonth.date;
           return (
-            <div className={styles.caption} {...props}>
+            <div className={styles.caption}>
               <div className={styles.navGroup}>
                 {enableYearNavigation && (
-                  <Button onClick={() => goToMonth(addYears(currentMonth, -1))} variant='calendar'>
+                  <Button onClick={() => goToMonth(addYears(displayMonth, -1))} variant='calendar'>
                     <ChevronsLeft />
                   </Button>
                 )}
-                <Button onClick={() => goToMonth(previousMonth)} variant='calendar'>
+                <Button
+                  disabled={!previousMonth}
+                  onClick={() => previousMonth && goToMonth(previousMonth)}
+                  variant='calendar'
+                >
                   <ChevronLeft />
                 </Button>
               </div>
 
               <Typography className={styles.monthLabel} color='emphasis' size='sm'>
-                {format(props.displayMonth, 'LLLL yyy', { locale })}
+                {format(displayMonth, 'LLLL yyy', { locale: locale as Locale })}
               </Typography>
 
               <div className={styles.navGroup}>
-                <Button onClick={() => goToMonth(nextMonth)} variant='calendar'>
+                <Button
+                  disabled={!nextMonth}
+                  onClick={() => nextMonth && goToMonth(nextMonth)}
+                  variant='calendar'
+                >
                   <ChevronRight />
                 </Button>
                 {enableYearNavigation && (
-                  <Button onClick={() => goToMonth(addYears(currentMonth, 1))} variant='calendar'>
+                  <Button onClick={() => goToMonth(addYears(displayMonth, 1))} variant='calendar'>
                     <ChevronsRight />
                   </Button>
                 )}
