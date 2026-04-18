1a41f02d5a0ab23aa38408329e69eb862d257a2d
Elliot Courant <me@elliotcourant.dev>
2026-04-18 18:26:18 -0500

    chore: Adding transaction schemas

---
+++ server/schema/transaction.go
@@ -0,0 +1,43 @@
+package schema
+
+import (
+	z "github.com/Oudwins/zog"
+	"github.com/monetr/monetr/server/models"
+)
+
+var (
+	CreateManualTransaction = z.Struct(z.Shape{
+		"amount":       z.Int64().Required(),
+		"spendingId":   z.Ptr(ID[models.Spending]().Optional()),
+		"name":         Name().Required(),
+		"merchantName": Name().Optional(),
+		"date":         Date().Required(),
+		"isPending":    z.Bool().Default(false).Required(),
+	})
+
+	AdjustsBalance = z.Struct(z.Shape{
+		// Meta fields, these fields affect things besides the transaction when the
+		// transaction is created.
+		"adjustsBalance": z.Bool().Default(false).Required(),
+	})
+
+	// PatchTransaction is the schema of fields that can be updated on any
+	// transaction in monetr, this is a safe subset of fields that applies to any
+	// datasource monetr supports.
+	PatchTransaction = z.Struct(z.Shape{
+		"spendingId": z.Ptr(ID[models.Spending]().Optional()),
+		"name":       Name().Optional(),
+	})
+
+	// PatchManualTransaction is the schema of fields that can be updated for
+	// manual links. This allows more fields to be changed after the creation of
+	// the transaction in order to allow for easier budgeting.
+	PatchManualTransaction = z.Struct(z.Shape{
+		"amount":       z.Int64().Optional(),
+		"spendingId":   z.Ptr(ID[models.Spending]().Optional()),
+		"name":         Name().Optional(),
+		"merchantName": Name().Optional(),
+		"date":         Date().Optional(),
+		"isPending":    z.Bool().Optional(),
+	})
+)
