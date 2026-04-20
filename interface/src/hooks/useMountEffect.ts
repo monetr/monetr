4af06909bbe494ea913ad21a926c55b37bf86299
Elliot Courant <me@elliotcourant.dev>
2026-04-19 19:06:29 -0500

    chore: Tweaks

--- interface/src/hooks/useMountEffect.ts
+++ interface/src/hooks/useMountEffect.ts
@@ -1,5 +1,5 @@
 import { type EffectCallback, useEffect } from 'react';
 
 export default function useMountEffect(callback: EffectCallback) {
-  useEffect(callback, []);
+  useEffect(callback, [callback]);
 }
