e1819dd4e1ab86a9e85effc7c16ece7bb7595317
Elliot Courant <me@elliotcourant.dev>
2026-05-13 08:19:08 -0500

    refactor(ui): Migrating dialog to be scss modules

--- interface/src/components/Dialog.tsx
+++ interface/src/components/Dialog.tsx
@@ -4,6 +4,8 @@ import { X } from 'lucide-react';
 
 import mergeTailwind from '@monetr/interface/util/mergeTailwind';
 
+import styles from './Dialog.module.scss';
+
 const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
@@ -16,14 +18,7 @@ const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    className={mergeTailwind(
-      'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
+  <DialogPrimitive.Overlay className={mergeTailwind(styles.dialogOverlay, className)} ref={ref} {...props} />
 ));
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
@@ -33,18 +28,11 @@ const DialogContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
-    <DialogPrimitive.Content
-      className={mergeTailwind(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    >
+    <DialogPrimitive.Content className={mergeTailwind(styles.dialogContent, className)} ref={ref} {...props}>
       {children}
-      <DialogPrimitive.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-dark-monetr-content-subtle'>
-        <X className='h-4 w-4' />
-        <span className='sr-only'>Close</span>
+      <DialogPrimitive.Close className={styles.dialogClose}>
+        <X className={styles.dialogCloseIcon} />
+        <span className={styles.dialogSrOnly}>Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
@@ -52,15 +40,12 @@ const DialogContent = React.forwardRef<
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={mergeTailwind('flex flex-col space-y-1.5 text-center sm:text-left', className)} {...props} />
+  <div className={mergeTailwind(styles.dialogHeader, className)} {...props} />
 );
 DialogHeader.displayName = 'DialogHeader';
 
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={mergeTailwind('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)}
-    {...props}
-  />
+  <div className={mergeTailwind(styles.dialogFooter, className)} {...props} />
 );
 DialogFooter.displayName = 'DialogFooter';
 
@@ -68,11 +53,7 @@ const DialogTitle = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Title
-    className={mergeTailwind('text-lg font-semibold leading-none tracking-tight', className)}
-    ref={ref}
-    {...props}
-  />
+  <DialogPrimitive.Title className={mergeTailwind(styles.dialogTitle, className)} ref={ref} {...props} />
 ));
 DialogTitle.displayName = DialogPrimitive.Title.displayName;
 
@@ -80,11 +61,7 @@ const DialogDescription = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Description
-    className={mergeTailwind('text-sm text-dark-monetr-content-subtle', className)}
-    ref={ref}
-    {...props}
-  />
+  <DialogPrimitive.Description className={mergeTailwind(styles.dialogDescription, className)} ref={ref} {...props} />
 ));
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
