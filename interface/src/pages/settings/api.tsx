36c076052296614b6ed5c3cce5599bf611250353
Elliot Courant <me@elliotcourant.dev>
2026-07-19 17:13:33 -0500

    fix: Fixed sorting and some item styles

--- interface/src/pages/settings/api.tsx
+++ interface/src/pages/settings/api.tsx
@@ -1,24 +1,30 @@
 import { useCallback } from 'react';
-import { Book, KeyRound, Plus, RefreshCcw, ServerCrash } from 'lucide-react';
+import { Book, KeyRound, Plus, RefreshCcw, ServerCrash, Trash } from 'lucide-react';
 
 import { Button } from '@monetr/interface/components/Button';
 import Card from '@monetr/interface/components/Card';
+import Code from '@monetr/interface/components/Code';
 import SettingsAPIHeader from '@monetr/interface/components/settings/SettingsAPI/Header';
 import Typography from '@monetr/interface/components/Typography';
 import useApiKeys from '@monetr/interface/hooks/useApiKeys';
+import { useLocale } from '@monetr/interface/hooks/useLocale';
+import useTimezone from '@monetr/interface/hooks/useTimezone';
+import { DateLength, formatDate } from '@monetr/interface/util/formatDate';
 
 import styles from './api.module.scss';
 
 export default function SettingsAPIKeys(): React.JSX.Element {
-  const { data: keys, isLoading, isError, refetch, isFetching } = useApiKeys();
+  const { data: keys, isLoading, isError, refetch, isFetching, isSuccess } = useApiKeys();
+  const { inTimezone } = useTimezone();
+  const { data: locale, isLoading: localeIsLoading } = useLocale();
 
   const refresh = useCallback(() => refetch(), [refetch]);
 
-  if (isLoading) {
+  if (isLoading || localeIsLoading) {
     return <div>Loading placeholder </div>;
   }
 
-  if (isError) {
+  if (isError || !isSuccess) {
     return (
       <div className={styles.root}>
         <SettingsAPIHeader />
@@ -44,34 +50,68 @@ export default function SettingsAPIKeys(): React.JSX.Element {
     );
   }
 
+  if (keys.length === 0) {
+    return (
+      <div className={styles.root}>
+        <SettingsAPIHeader />
+        <Card className={styles.cardRoot}>
+          <div className={styles.keyLogos}>
+            <KeyRound className={styles.keyLogosBack} />
+            <KeyRound className={styles.keyLogosFront} />
+          </div>
+          <div className={styles.cardEmptyText}>
+            <Typography size='lg' weight='bold'>
+              No API Keys yet
+            </Typography>
+            <Typography color='subtle' size='lg' weight='normal'>
+              API Keys let scripts and tools read your monetr data and manage it on your behalf. You'll only see the
+              secret once at creation time...
+            </Typography>
+          </div>
+          <div className={styles.cardEmptyActions}>
+            <Button variant='outlined'>
+              <Book />
+              Read the Docs
+            </Button>
+            <Button>
+              <Plus />
+              Create your first API Key
+            </Button>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.root}>
       <SettingsAPIHeader />
-      <Card className={styles.cardRoot}>
-        <div className={styles.keyLogos}>
-          <KeyRound className={styles.keyLogosBack} />
-          <KeyRound className={styles.keyLogosFront} />
-        </div>
-        <div className={styles.cardEmptyText}>
-          <Typography size='lg' weight='bold'>
-            No API Keys yet
-          </Typography>
-          <Typography color='subtle' size='lg' weight='normal'>
-            API Keys let scripts and tools read your monetr data and manage it on your behalf. You'll only see the
-            secret once at creation time...
-          </Typography>
-        </div>
-        <div className={styles.cardEmptyActions}>
-          <Button variant='outlined'>
-            <Book />
-            Read the Docs
-          </Button>
-          <Button>
-            <Plus />
-            Create your first API Key
-          </Button>
-        </div>
-      </Card>
+      {keys.map(item => (
+        <Card className={styles.item} key={item.apiKeyId}>
+          <div className={styles.itemContent}>
+            <Typography size='lg' weight='bold'>
+              {item.name}
+            </Typography>
+            <Code className={styles.itemContentKeyId} icon={KeyRound} label='Key ID'>
+              {item.apiKeyId}
+            </Code>
+            <div className={styles.itemContentMetadata}>
+              <Typography component='p' ellipsis size='sm'>
+                Created By: <b>[PLACEHOLDER TODO]</b>
+              </Typography>
+              <Typography component='p' ellipsis size='sm'>
+                Created On: <b>{formatDate(item.createdAt, inTimezone, locale!, DateLength.Full)}</b>
+              </Typography>
+            </div>
+          </div>
+          <div>
+            <Button variant='destructive'>
+              <Trash />
+              Revoke
+            </Button>
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
