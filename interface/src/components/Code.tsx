1e6c1b49e0fe34cf6e25dd75a407401a561dfa55
Elliot Courant <me@elliotcourant.dev>
2026-07-22 20:05:12 -0500

    chore: Getting copying working

--- interface/src/components/Code.tsx
+++ interface/src/components/Code.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { Fragment, useCallback, useId, useState } from 'react';
 import { Copy } from 'lucide-react';
 
 import { Button } from '@monetr/interface/components/Button';
@@ -6,6 +6,7 @@ import ErrorText from '@monetr/interface/components/ErrorText';
 import Label, { type LabelDecorator } from '@monetr/interface/components/Label';
 import { Skeleton } from '@monetr/interface/components/Skeleton';
 import mergeClasses from '@monetr/interface/util/mergeClasses';
+import { useSnackbar } from '@monetr/notify';
 
 import styles from './Code.module.scss';
 import errorTextStyles from './ErrorText.module.scss';
@@ -25,10 +26,29 @@ export default function Code(props: CodeProps): React.JSX.Element {
   props = {
     id,
     ...props,
+    // Even if the prop is set, if the browser doesn't have the capabilty for some reason then don't even show the
+    // button.
+    copy: props.copy && typeof navigator.clipboard?.writeText === 'function' ? props.copy : undefined,
   };
   const { labelDecorator, icon, className, ...otherProps } = props;
   const LabelDecorator = labelDecorator ?? (() => null);
   const Icon = icon ?? (() => null);
+  const [copied, setCopied] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const onCopy = useCallback(async () => {
+    if (props.copy) {
+      return await navigator.clipboard
+        .writeText(props.copy)
+        .then(() => setCopied(true))
+        .catch(() =>
+          enqueueSnackbar('Failed to copy to the clipboard.', {
+            variant: 'error',
+            disableWindowBlurListener: true,
+          }),
+        );
+    }
+  }, [enqueueSnackbar, props.copy]);
 
   if (props.isLoading) {
     return (
@@ -58,8 +78,8 @@ export default function Code(props: CodeProps): React.JSX.Element {
             {props.children}
           </code>
           {Boolean(props.copy) && (
-            <Button aria-label='Copy' variant='outlined'>
-              <Copy />
+            <Button aria-label='Copy' onClick={onCopy} variant={copied ? 'primary' : 'outlined'}>
+              {copied ? <Fragment>Copied!</Fragment> : <Copy />}
             </Button>
           )}
         </div>
