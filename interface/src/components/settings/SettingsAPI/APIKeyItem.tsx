14e7cb5d1167cc25c0db5d486f5a771f06463686
Elliot Courant <me@elliotcourant.dev>
2026-07-26 09:30:39 -0500

    chore: Adding api key items

---
+++ interface/src/components/settings/SettingsAPI/APIKeyItem.tsx
@@ -0,0 +1,56 @@
+import { Fragment } from 'react';
+import { KeyRound, Trash } from 'lucide-react';
+
+import { Button } from '@monetr/interface/components/Button';
+import Card from '@monetr/interface/components/Card';
+import Code from '@monetr/interface/components/Code';
+import { showRevokeAPIKeyModal } from '@monetr/interface/components/settings/SettingsAPI/RevokeAPIKeyModal';
+import Typography from '@monetr/interface/components/Typography';
+import { useLocale } from '@monetr/interface/hooks/useLocale';
+import useTimezone from '@monetr/interface/hooks/useTimezone';
+import { useUser } from '@monetr/interface/hooks/useUser';
+import type ApiKey from '@monetr/interface/models/ApiKey';
+import { DateLength, formatDate } from '@monetr/interface/util/formatDate';
+
+import styles from './APIKeyItem.module.scss';
+
+interface APIKeyItemProps {
+  apiKey: ApiKey;
+}
+
+export default function APIKeyItem(props: APIKeyItemProps): React.JSX.Element {
+  const { inTimezone } = useTimezone();
+  const { data: locale, isLoading: localeIsLoading } = useLocale();
+  const { data: createdByUser } = useUser(props.apiKey.createdBy);
+
+  if (localeIsLoading) {
+    return <Fragment />;
+  }
+
+  return (
+    <Card className={styles.item} key={props.apiKey.apiKeyId}>
+      <div className={styles.itemContent}>
+        <Typography size='lg' weight='bold'>
+          {props.apiKey.name}
+        </Typography>
+        <Code className={styles.itemContentKeyId} icon={KeyRound} label='Key ID'>
+          {props.apiKey.apiKeyId}
+        </Code>
+        <div className={styles.itemContentMetadata}>
+          <Typography component='p' ellipsis size='sm'>
+            Created By: <b>{createdByUser?.name() ?? '...'}</b>
+          </Typography>
+          <Typography component='p' ellipsis size='sm'>
+            Created On: <b>{formatDate(props.apiKey.createdAt, inTimezone, locale!, DateLength.Full)}</b>
+          </Typography>
+        </div>
+      </div>
+      <div>
+        <Button onClick={() => showRevokeAPIKeyModal({ apiKey: props.apiKey })} variant='destructive'>
+          <Trash />
+          Revoke
+        </Button>
+      </div>
+    </Card>
+  );
+}
--- interface/src/pages/settings/api.tsx
+++ interface/src/pages/settings/api.tsx
@@ -1,27 +1,21 @@
 import { useCallback } from 'react';
-import { Book, KeyRound, Plus, RefreshCcw, ServerCrash, Trash } from 'lucide-react';
+import { Book, KeyRound, Plus, RefreshCcw, ServerCrash } from 'lucide-react';
 
 import { Button } from '@monetr/interface/components/Button';
 import Card from '@monetr/interface/components/Card';
-import Code from '@monetr/interface/components/Code';
+import APIKeyItem from '@monetr/interface/components/settings/SettingsAPI/APIKeyItem';
 import SettingsAPIHeader from '@monetr/interface/components/settings/SettingsAPI/Header';
-import { showRevokeAPIKeyModal } from '@monetr/interface/components/settings/SettingsAPI/RevokeAPIKeyModal';
 import Typography from '@monetr/interface/components/Typography';
 import useApiKeys from '@monetr/interface/hooks/useApiKeys';
-import { useLocale } from '@monetr/interface/hooks/useLocale';
-import useTimezone from '@monetr/interface/hooks/useTimezone';
-import { DateLength, formatDate } from '@monetr/interface/util/formatDate';
 
 import styles from './api.module.scss';
 
 export default function SettingsAPIKeys(): React.JSX.Element {
   const { data: keys, isLoading, isError, refetch, isFetching, isSuccess } = useApiKeys();
-  const { inTimezone } = useTimezone();
-  const { data: locale, isLoading: localeIsLoading } = useLocale();
 
   const refresh = useCallback(() => refetch(), [refetch]);
 
-  if (isLoading || localeIsLoading) {
+  if (isLoading) {
     return <div>Loading placeholder </div>;
   }
 
@@ -88,30 +82,7 @@ export default function SettingsAPIKeys(): React.JSX.Element {
     <div className={styles.root}>
       <SettingsAPIHeader />
       {keys.map(item => (
-        <Card className={styles.item} key={item.apiKeyId}>
-          <div className={styles.itemContent}>
-            <Typography size='lg' weight='bold'>
-              {item.name}
-            </Typography>
-            <Code className={styles.itemContentKeyId} icon={KeyRound} label='Key ID'>
-              {item.apiKeyId}
-            </Code>
-            <div className={styles.itemContentMetadata}>
-              <Typography component='p' ellipsis size='sm'>
-                Created By: <b>[PLACEHOLDER TODO]</b>
-              </Typography>
-              <Typography component='p' ellipsis size='sm'>
-                Created On: <b>{formatDate(item.createdAt, inTimezone, locale!, DateLength.Full)}</b>
-              </Typography>
-            </div>
-          </div>
-          <div>
-            <Button onClick={() => showRevokeAPIKeyModal({ apiKey: item })} variant='destructive'>
-              <Trash />
-              Revoke
-            </Button>
-          </div>
-        </Card>
+        <APIKeyItem apiKey={item} key={item.apiKeyId} />
       ))}
     </div>
   );
