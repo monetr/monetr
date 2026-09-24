ab91bad6bdf8d7d9d62d0b9d85769fc179e06780
Elliot Courant <me@elliotcourant.dev>
2026-09-24 16:46:45 -0500

    fix(ui): Adding tooltip to pending transactions

--- interface/src/components/transactions/TransactionMerchantIcon.tsx
+++ interface/src/components/transactions/TransactionMerchantIcon.tsx
@@ -1,4 +1,5 @@
 import MerchantIcon, { type MerchantIconProps } from '@monetr/interface/components/MerchantIcon';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@monetr/interface/components/Tooltip';
 
 import styles from './TransactionMerchantIcon.module.scss';
 
@@ -13,10 +14,15 @@ export default function TransactionMerchantIcon(props: TransactionMerchantIconPr
     return (
       <div className={styles.root}>
         <MerchantIcon {...merchantIconProps} />
-        <span className={styles.pendingIndicator}>
-          <span className={styles.pendingPing} />
-          <span className={styles.pendingDot} />
-        </span>
+        <Tooltip delayDuration={100}>
+          <TooltipTrigger asChild>
+            <span aria-label='Transaction is pending' className={styles.pendingIndicator} role='img'>
+              <span className={styles.pendingPing} />
+              <span className={styles.pendingDot} />
+            </span>
+          </TooltipTrigger>
+          <TooltipContent>Transaction is pending</TooltipContent>
+        </Tooltip>
       </div>
     );
   }
